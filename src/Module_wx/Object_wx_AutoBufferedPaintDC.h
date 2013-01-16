b89fa355695ffbb3df831f3e3c498156d95aee06
Yutaka SAITO <pxn11432@nifty.com>
2013-01-16 15:54:05 +0900

    AutoBufferedPaintDC inherits from DC

--- src/Module_wx/Object_wx_AutoBufferedPaintDC.h
+++ src/Module_wx/Object_wx_AutoBufferedPaintDC.h
@@ -15,14 +15,14 @@ Gura_DeclareUserClass(wx_AutoBufferedPaintDC);
 //----------------------------------------------------------------------------
 // Object declaration for wxAutoBufferedPaintDC
 //----------------------------------------------------------------------------
-class Object_wx_AutoBufferedPaintDC : public Object_wx_BufferedPaintDC {
+class Object_wx_AutoBufferedPaintDC : public Object_wx_DC {
 public:
 	Gura_DeclareObjectAccessor(wx_AutoBufferedPaintDC)
 public:
 	inline Object_wx_AutoBufferedPaintDC(wxAutoBufferedPaintDC *pEntity, GuraObjectObserver *pObserver, bool ownerFlag) :
-				Object_wx_BufferedPaintDC(Gura_UserClass(wx_AutoBufferedPaintDC), pEntity, pObserver, ownerFlag) {}
+				Object_wx_DC(Gura_UserClass(wx_AutoBufferedPaintDC), pEntity, pObserver, ownerFlag) {}
 	inline Object_wx_AutoBufferedPaintDC(Class *pClass, wxAutoBufferedPaintDC *pEntity, GuraObjectObserver *pObserver, bool ownerFlag) :
-				Object_wx_BufferedPaintDC(pClass, pEntity, pObserver, ownerFlag) {}
+				Object_wx_DC(pClass, pEntity, pObserver, ownerFlag) {}
 	virtual ~Object_wx_AutoBufferedPaintDC();
 	virtual Object *Clone() const;
 	virtual String ToString(Signal sig, bool exprFlag);
