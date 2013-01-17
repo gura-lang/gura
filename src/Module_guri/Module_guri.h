f805cf1e06cf847508cd56b1c1daea07218acd71
Yutaka SAITO <pxn11432@nifty.com>
2013-01-17 15:31:41 +0900

    adopted for Linux

--- src/Module_guri/Module_guri.h
+++ src/Module_guri/Module_guri.h
@@ -4,6 +4,17 @@
 #ifndef __MODULE_GURI_H__
 #define __MODULE_GURI_H__
 #include <gura.h>
+#if defined(HAVE_WINDOWS_H)
+typedef int socklen_t;
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+inline void closesocket(int sock) { close(sock); }
+#endif
 
 Gura_BeginModule(guri)
 }}
