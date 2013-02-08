6ffc7e6d458ae243d52cb3e4c962da59d96b9a68
Yutaka SAITO <pxn11432@nifty.com>
2013-02-08 11:22:06 +0900

    implementing cairo module

--- src/Module_cairo/Module_cairo.cpp
+++ src/Module_cairo/Module_cairo.cpp
@@ -3619,6 +3619,93 @@ Gura_ImplementUserClass(recording_surface)
 //-----------------------------------------------------------------------------
 // Gura interfaces for win32_surface
 //-----------------------------------------------------------------------------
+#if defined(HAVE_WINDOWS_H)
+// cairo.win32_surface.emf_create(filename:string, width_in_points:number, height_in_points:number) {block?}
+Gura_DeclareClassMethod(win32_surface, create_emf)
+{
+	SetMode(RSLTMODE_Normal, FLAG_None);
+	DeclareArg(env, "filename", VTYPE_string);
+	DeclareArg(env, "width_in_points", VTYPE_number);
+	DeclareArg(env, "height_in_points", VTYPE_number);
+	DeclareBlock(OCCUR_ZeroOrOnce);
+}
+
+Gura_ImplementClassMethod(win32_surface, create_emf)
+{
+	double width = args.GetDouble(1);
+	double height = args.GetDouble(2);
+	RECT rc;
+	rc.left = 0, rc.top = 0;
+	rc.right = static_cast<long>(width * 20);	// unit: 1/20pt
+	rc.bottom = static_cast<long>(height * 20);	// unit: 1/20pt
+	HDC hdc = ::CreateEnhMetaFile(NULL, args.GetString(0), &rc, "EMF by Gura\0");
+	::StartPage(hdc);
+	//::SetMapMode(hdc, MM_TWIPS);				// 1 unit = 1/20pt
+	::SetMapMode(hdc, MM_HIMETRIC);				// 1 unit = 0.01mm
+	//::SetWindowOrgEx(hdc, 0, height * 20, NULL);
+	//::MoveToEx(hdc, 0, 0, NULL);
+	//::LineTo(hdc, 1000, 1000);
+	//::MoveToEx(hdc, 0, 0, NULL);
+	//::LineTo(hdc, 1000, -1000);
+	Writer_EnhMetaFile *pWriter = new Writer_EnhMetaFile(sig, width, height, hdc);
+	cairo_surface_t *surface = ::cairo_win32_printing_surface_create(hdc);
+	Object_surface *pObjSurface = new Object_win32_surface(surface, pWriter);
+	return ReturnValue(env, sig, args, Value(pObjSurface));
+}
+
+// cairo.win32_surface.create_printing(printer_name:string, width_in_points:number, height_in_points:number) {block?}
+Gura_DeclareClassMethod(win32_surface, create_printing)
+{
+	SetMode(RSLTMODE_Normal, FLAG_None);
+	DeclareArg(env, "printer_name", VTYPE_string);
+	DeclareArg(env, "width_in_points", VTYPE_number);
+	DeclareArg(env, "height_in_points", VTYPE_number);
+	DeclareBlock(OCCUR_ZeroOrOnce);
+}
+
+Gura_ImplementClassMethod(win32_surface, create_printing)
+{
+	const char *driverName = "WINSPOOL\0";
+	const char *printerName = args.GetString(0);
+	char printerNameDefault[MAX_PATH];
+	BYTE printerInfoBuff[16384];
+	PRINTER_INFO_2 *pPrinterInfo2 = reinterpret_cast<PRINTER_INFO_2 *>(printerInfoBuff);
+	if (::strcmp(printerName, "") == 0) {
+		DWORD cch;
+		if (!::GetDefaultPrinter(printerNameDefault, &cch)) {
+			sig.SetError(ERR_IOError, "failed to get a default printer");
+			return Value::Null;
+		}
+		printerName = printerNameDefault;
+	}
+	do {
+		HANDLE hPrinter;
+		if (!::OpenPrinter(const_cast<char *>(printerName), &hPrinter, NULL)) {
+			sig.SetError(ERR_IOError, "failed to open printer handler");
+			return Value::Null;
+		}
+		DWORD cbNeeded;
+		if (!::GetPrinter(hPrinter, 2, printerInfoBuff, sizeof(printerInfoBuff), &cbNeeded)) {
+			sig.SetError(ERR_IOError, "failed to get printer information");
+			return Value::Null;
+		}
+		::ClosePrinter(hPrinter);
+	} while (0);
+	double width = args.GetDouble(1), height = args.GetDouble(2);
+	RECT rc;
+	rc.left = 0, rc.top = 0;
+	rc.right = static_cast<long>(width * 20);	// unit: 1/20pt
+	rc.bottom = static_cast<long>(height * 20);	// unit: 1/20pt
+	HDC hdc = ::CreateDC(driverName, printerName, pPrinterInfo2->pPortName, NULL);
+	::SetMapMode(hdc, MM_TWIPS);				// 1 unit = 1/20pt
+	//::SetMapMode(hdc, MM_HIMETRIC);			// 1 unit = 0.01mm
+	Writer_WindowsDC *pWriter = new Writer_WindowsDC(sig, width, height, hdc);
+	cairo_surface_t *surface = ::cairo_win32_printing_surface_create(hdc);
+	Object_surface *pObjSurface = new Object_win32_surface(surface, pWriter);
+	return ReturnValue(env, sig, args, Value(pObjSurface));
+}
+#endif
+
 //#cairo_surface_t *cairo_win32_surface_create(HDC hdc);
 //#cairo_surface_t *cairo_win32_surface_create_with_dib(cairo_format_t format, int width, int height);
 //#cairo_surface_t *cairo_win32_surface_create_with_ddb(HDC hdc, cairo_format_t format, int width, int height);
@@ -3628,6 +3715,10 @@ Gura_ImplementUserClass(recording_surface)
 
 Gura_ImplementUserClass(win32_surface)
 {
+#if defined(HAVE_WINDOWS_H)
+	Gura_AssignMethod(win32_surface, create_emf);
+	Gura_AssignMethod(win32_surface, create_printing);
+#endif
 }
 
 //-----------------------------------------------------------------------------
@@ -4355,97 +4446,6 @@ Gura_ImplementFunction(create)
 		Value(new Object_context(cr, Object_surface::Reference(pObjSurface))));
 }
 
-#if defined(HAVE_WINDOWS_H)
-// cairo.emf_create(filename:string, width_in_points:number, height_in_points:number) {block?}
-Gura_DeclareFunction(emf_create)
-{
-	SetMode(RSLTMODE_Normal, FLAG_None);
-	DeclareArg(env, "filename", VTYPE_string);
-	DeclareArg(env, "width_in_points", VTYPE_number);
-	DeclareArg(env, "height_in_points", VTYPE_number);
-	DeclareBlock(OCCUR_ZeroOrOnce);
-}
-
-Gura_ImplementFunction(emf_create)
-{
-	double width = args.GetDouble(1), height = args.GetDouble(2);
-	RECT rc;
-	rc.left = 0, rc.top = 0;
-	rc.right = static_cast<long>(width * 20);	// unit: 1/20pt
-	rc.bottom = static_cast<long>(height * 20);	// unit: 1/20pt
-	HDC hdc = ::CreateEnhMetaFile(NULL, args.GetString(0), &rc, "EMF by Gura\0");
-	::StartPage(hdc);
-	//::SetMapMode(hdc, MM_TWIPS);				// 1 unit = 1/20pt
-	::SetMapMode(hdc, MM_HIMETRIC);				// 1 unit = 0.01mm
-	//::SetWindowOrgEx(hdc, 0, height * 20, NULL);
-	//::MoveToEx(hdc, 0, 0, NULL);
-	//::LineTo(hdc, 1000, 1000);
-	//::MoveToEx(hdc, 0, 0, NULL);
-	//::LineTo(hdc, 1000, -1000);
-	Writer_EnhMetaFile *pWriter = new Writer_EnhMetaFile(sig, width, height, hdc);
-	cairo_surface_t *surface = ::cairo_win32_surface_create(hdc);
-	//cairo_surface_t *surface = ::cairo_win32_printing_surface_create(hdc);
-	Object_surface *pObjSurface = new Object_win32_surface(surface, pWriter);
-	cairo_t *cr = ::cairo_create(surface);
-	Value result(new Object_context(cr, pObjSurface));
-	return ReturnValue(env, sig, args, result);
-}
-
-// cairo.printer_create(printer_name:string, width_in_points:number, height_in_points:number) {block?}
-Gura_DeclareFunction(printer_create)
-{
-	SetMode(RSLTMODE_Normal, FLAG_None);
-	DeclareArg(env, "printer_name", VTYPE_string);
-	DeclareArg(env, "width_in_points", VTYPE_number);
-	DeclareArg(env, "height_in_points", VTYPE_number);
-	DeclareBlock(OCCUR_ZeroOrOnce);
-}
-
-Gura_ImplementFunction(printer_create)
-{
-	const char *driverName = "WINSPOOL\0";
-	const char *printerName = args.GetString(0);
-	char printerNameDefault[MAX_PATH];
-	BYTE printerInfoBuff[16384];
-	PRINTER_INFO_2 *pPrinterInfo2 = reinterpret_cast<PRINTER_INFO_2 *>(printerInfoBuff);
-	if (::strcmp(printerName, "") == 0) {
-		DWORD cch;
-		if (!::GetDefaultPrinter(printerNameDefault, &cch)) {
-			sig.SetError(ERR_IOError, "failed to get a default printer");
-			return Value::Null;
-		}
-		printerName = printerNameDefault;
-	}
-	do {
-		HANDLE hPrinter;
-		if (!::OpenPrinter(const_cast<char *>(printerName), &hPrinter, NULL)) {
-			sig.SetError(ERR_IOError, "failed to open printer handler");
-			return Value::Null;
-		}
-		DWORD cbNeeded;
-		if (!::GetPrinter(hPrinter, 2, printerInfoBuff, sizeof(printerInfoBuff), &cbNeeded)) {
-			sig.SetError(ERR_IOError, "failed to get printer information");
-			return Value::Null;
-		}
-		::ClosePrinter(hPrinter);
-	} while (0);
-	double width = args.GetDouble(1), height = args.GetDouble(2);
-	RECT rc;
-	rc.left = 0, rc.top = 0;
-	rc.right = static_cast<long>(width * 20);	// unit: 1/20pt
-	rc.bottom = static_cast<long>(height * 20);	// unit: 1/20pt
-	HDC hdc = ::CreateDC(driverName, printerName, pPrinterInfo2->pPortName, NULL);
-	::SetMapMode(hdc, MM_TWIPS);				// 1 unit = 1/20pt
-	//::SetMapMode(hdc, MM_HIMETRIC);			// 1 unit = 0.01mm
-	Writer_WindowsDC *pWriter = new Writer_WindowsDC(sig, width, height, hdc);
-	cairo_surface_t *surface = ::cairo_win32_printing_surface_create(hdc);
-	Object_surface *pObjSurface = new Object_win32_surface(surface, pWriter);
-	cairo_t *cr = ::cairo_create(surface);
-	Value result(new Object_context(cr, pObjSurface));
-	return ReturnValue(env, sig, args, result);
-}
-#endif
-
 //#int cairo_format_stride_for_width(cairo_format_t format, int width);
 //#cairo_surface_t *cairo_image_surface_create(cairo_format_t format, int width, int height);
 //#const char *cairo_status_to_string(cairo_status_t status);
@@ -4580,10 +4580,6 @@ Gura_ModuleEntry()
 	Gura_AssignValue(context,				Value(Gura_UserClass(context)));
 	// function assignment
 	Gura_AssignFunction(create);
-#if defined(HAVE_WINDOWS_H)
-	Gura_AssignFunction(emf_create);
-	Gura_AssignFunction(printer_create);
-#endif
 	Gura_AssignFunction(test);
 	// cairo_path_data_type_t
 	Gura_AssignCairoValue(PATH_MOVE_TO);
