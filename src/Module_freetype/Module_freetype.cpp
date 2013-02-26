f6863e392108321d58a1a88e4a899d2ce0303d86
Yutaka SAITO <pxn11432@nifty.com>
2013-02-26 15:40:50 +0900

    updated

--- src/Module_freetype/Module_freetype.cpp
+++ src/Module_freetype/Module_freetype.cpp
@@ -61,66 +61,6 @@ FT_ULong Handler::ReadStub(FT_Stream streamFT,
 }
 
 //-----------------------------------------------------------------------------
-// Object_Vector implementation
-//-----------------------------------------------------------------------------
-Object *Object_Vector::Clone() const
-{
-	return NULL;
-}
-
-String Object_Vector::ToString(Signal sig, bool exprFlag)
-{
-	char buff[80];
-	::sprintf(buff, "<sdl.Vector:x=%d,y=%d>", _vector.x, _vector.y);
-	return String(buff);
-}
-
-bool Object_Vector::DoDirProp(Signal sig, SymbolSet &symbols)
-{
-	if (!Object::DoDirProp(sig, symbols)) return false;
-	symbols.insert(Gura_Symbol(x));
-	symbols.insert(Gura_Symbol(y));
-	return true;
-}
-
-Value Object_Vector::DoGetProp(Signal sig, const Symbol *pSymbol,
-						const SymbolSet &attrs, bool &evaluatedFlag)
-{
-	evaluatedFlag = true;
-	if (pSymbol->IsIdentical(Gura_Symbol(x))) {
-		return Value(_vector.x);
-	} else if (pSymbol->IsIdentical(Gura_Symbol(y))) {
-		return Value(_vector.y);
-	}
-	evaluatedFlag = false;
-	return Value::Null;
-}
-
-Value Object_Vector::DoSetProp(Signal sig, const Symbol *pSymbol, const Value &value,
-							const SymbolSet &attrs, bool &evaluatedFlag)
-{
-	evaluatedFlag = true;
-	if (pSymbol->IsIdentical(Gura_Symbol(x))) {
-		if (!value.MustBeNumber(sig)) return Value::Null;
-		_vector.x = static_cast<FT_Pos>(value.GetLong());
-		return Value(_vector.x);
-	} else if (pSymbol->IsIdentical(Gura_Symbol(y))) {
-		if (!value.MustBeNumber(sig)) return Value::Null;
-		_vector.y = static_cast<FT_Pos>(value.GetLong());
-		return Value(_vector.y);
-	}
-	evaluatedFlag = false;
-	return Value::Null;
-}
-
-//-----------------------------------------------------------------------------
-// Class implementation for freetype.Vector
-//-----------------------------------------------------------------------------
-Gura_ImplementUserClass(Vector)
-{
-}
-
-//-----------------------------------------------------------------------------
 // Object_BBox implementation
 //-----------------------------------------------------------------------------
 Object *Object_BBox::Clone() const
--- src/Module_freetype/Module_freetype.h
+++ src/Module_freetype/Module_freetype.h
@@ -84,32 +84,6 @@ private:
 };
 
 //-----------------------------------------------------------------------------
-// Object_Vector declaration
-//-----------------------------------------------------------------------------
-Gura_DeclareUserClass(Vector);
-
-class Object_Vector : public Object {
-public:
-	Gura_DeclareObjectAccessor(Vector)
-private:
-	FT_Vector _vector;
-public:
-	inline Object_Vector(const FT_Vector &vector) :
-			Object(Gura_UserClass(Vector)), _vector(vector) {}
-	inline Object_Vector(const Object_Vector &obj) :
-			Object(obj), _vector(obj._vector) {}
-	virtual Object *Clone() const;
-	virtual String ToString(Signal sig, bool exprFlag);
-	virtual bool DoDirProp(Signal sig, SymbolSet &symbols);
-	virtual Value DoGetProp(Signal sig, const Symbol *pSymbol,
-							const SymbolSet &attrs, bool &evaluatedFlag);
-	virtual Value DoSetProp(Signal sig, const Symbol *pSymbol, const Value &value,
-							const SymbolSet &attrs, bool &evaluatedFlag);
-	inline FT_Vector &GetEntity() { return _vector; }
-	inline const FT_Vector &GetEntity() const { return _vector; }
-};
-
-//-----------------------------------------------------------------------------
 // Object_BBox declaration
 //-----------------------------------------------------------------------------
 Gura_DeclareUserClass(BBox);
