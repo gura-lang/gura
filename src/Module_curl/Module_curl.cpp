c963bbbd1c525288f9416aea0f0515a3d21d97ae
Yutaka Saito <ypsitau@nifty.com>
2013-04-20 11:21:59 +0900

    addec curl module

---
+++ src/Module_curl/Module_curl.cpp
@@ -0,0 +1,40 @@
+//-----------------------------------------------------------------------------
+// Gura curl module
+//-----------------------------------------------------------------------------
+#include "Module_curl.h"
+
+Gura_BeginModule(curl)
+
+//-----------------------------------------------------------------------------
+// Gura module functions: curl
+//-----------------------------------------------------------------------------
+// curl.test(num1:number, num2:number)
+Gura_DeclareFunction(test)
+{
+	SetMode(RSLTMODE_Normal, FLAG_None);
+	DeclareArg(env, "num1", VTYPE_number);
+	DeclareArg(env, "num2", VTYPE_number);
+	AddHelp(Gura_Symbol(en), "adds two numbers and returns the result.");
+}
+
+Gura_ImplementFunction(test)
+{
+	return Value(args.GetNumber(0) + args.GetNumber(1));
+}
+
+//-----------------------------------------------------------------------------
+// Module entry
+//-----------------------------------------------------------------------------
+Gura_ModuleEntry()
+{
+	// function assignment
+	Gura_AssignFunction(test);
+}
+
+Gura_ModuleTerminate()
+{
+}
+
+Gura_EndModule(curl, curl)
+
+Gura_RegisterModule(curl)
