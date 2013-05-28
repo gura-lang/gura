c136ec25532f3115817d9fd30478962f77c13634
Yutaka Saito <ypsitau@nifty.com>
2013-05-28 11:50:30 +0900

    implementing midi module

---
+++ src/Module_midi/MIDIHandle.h
@@ -0,0 +1,52 @@
+#ifndef __MIDIHANDLE_H__
+#define __MIDIHANDLE_H__
+
+class MIDIHandle {
+private:
+	HMIDIOUT _hMIDI;
+public:
+	inline MIDIHandle() : _hMIDI(NULL) {}
+	inline ~MIDIHandle() {
+		Reset();
+		Close();
+	}
+	static inline int GetNumDevs() {
+		return ::midiOutGetNumDevs();
+	}
+	inline bool Open(int id) {
+		if (_hMIDI != NULL) Close();
+		return ::midiOutOpen(&_hMIDI, id, 0, 0, CALLBACK_NULL) == MMSYSERR_NOERROR;
+	}
+	inline void Close() {
+		if (_hMIDI != NULL) ::midiOutClose(_hMIDI);
+		_hMIDI = NULL;
+	}
+	inline void Reset() {
+		if (_hMIDI != NULL) ::midiOutReset(_hMIDI);
+	}
+	inline void RawWrite(unsigned char msg1) {
+		::midiOutShortMsg(_hMIDI,
+					(static_cast<DWORD>(msg1) << 0));
+	}
+	inline void RawWrite(unsigned char msg1, unsigned char msg2) {
+		::midiOutShortMsg(_hMIDI,
+					(static_cast<DWORD>(msg1) << 0) +
+					(static_cast<DWORD>(msg2) << 8));
+	}
+	inline void RawWrite(unsigned char msg1, unsigned char msg2, unsigned char msg3) {
+		::midiOutShortMsg(_hMIDI,
+					(static_cast<DWORD>(msg1) << 0) +
+					(static_cast<DWORD>(msg2) << 8) +
+					(static_cast<DWORD>(msg3) << 16));
+	}
+	inline void RawWrite(unsigned char msg1, unsigned char msg2, unsigned char msg3,
+					unsigned char msg4) {
+		::midiOutShortMsg(_hMIDI,
+					(static_cast<DWORD>(msg1) << 0) +
+					(static_cast<DWORD>(msg2) << 8) +
+					(static_cast<DWORD>(msg3) << 16) +
+					(static_cast<DWORD>(msg4) << 24));
+	}
+};
+
+#endif
--- src/Module_midi/Module_midi.cpp
+++ src/Module_midi/Module_midi.cpp
@@ -2,12 +2,130 @@
 // Gura midi module
 //-----------------------------------------------------------------------------
 #include "Module_midi.h"
+#include "MIDIHandle.h"
 
 Gura_BeginModule(midi)
 
 //-----------------------------------------------------------------------------
+// Class declaration for midi.device
+//-----------------------------------------------------------------------------
+Gura_DeclareUserClass(device);
+
+class Object_device : public Object {
+public:
+	Gura_DeclareObjectAccessor(device)
+private:
+	MIDIHandle _handle;
+public:
+	inline Object_device(Environment &env) : Object(Gura_UserClass(device)) {}
+	virtual Object *Clone() const;
+	virtual bool DoDirProp(Environment &env, Signal sig, SymbolSet &symbols);
+	virtual Value DoGetProp(Environment &env, Signal sig, const Symbol *pSymbol,
+								const SymbolSet &attrs, bool &evaluatedFlag);
+	virtual String ToString(Signal sig, bool exprFlag);
+	inline MIDIHandle &GetHandle() { return _handle; }
+};
+
+//-----------------------------------------------------------------------------
+// Object_device
+//-----------------------------------------------------------------------------
+Object *Object_device::Clone() const
+{
+	return NULL;
+}
+
+bool Object_device::DoDirProp(Environment &env, Signal sig, SymbolSet &symbols)
+{
+	if (!Object::DoDirProp(env, sig, symbols)) return false;
+	//symbols.insert(Gura_Symbol(string));
+	return true;
+}
+
+Value Object_device::DoGetProp(Environment &env, Signal sig, const Symbol *pSymbol,
+							const SymbolSet &attrs, bool &evaluatedFlag)
+{
+#if 0
+	evaluatedFlag = true;
+	if (pSymbol->IsIdentical(Gura_Symbol(string))) {
+		return Value(env, _str);
+	}
+#endif
+	evaluatedFlag = false;
+	return Value::Null;
+}
+
+String Object_device::ToString(Signal sig, bool exprFlag)
+{
+	String rtn;
+	rtn += "<device:";
+	rtn += ">";
+	return rtn;
+}
+
+//-----------------------------------------------------------------------------
+// Gura interfaces for midi.device
+//-----------------------------------------------------------------------------
+// midi.device#rawwrite(msg+:number):map
+Gura_DeclareMethod(device, rawwrite)
+{
+	SetMode(RSLTMODE_Void, FLAG_Map);
+	DeclareArg(env, "msg", VTYPE_number, OCCUR_OnceOrMore);
+}
+
+Gura_ImplementMethod(device, rawwrite)
+{
+	Object_device *pThis = Object_device::GetThisObj(args);
+	const ValueList &valList = args.GetList(0);
+	size_t nArgs = valList.size();
+	if (nArgs == 1) {
+		pThis->GetHandle().RawWrite(valList[0].GetUChar());
+	} else if (nArgs == 2) {
+		pThis->GetHandle().RawWrite(valList[0].GetUChar(), valList[1].GetUChar());
+	} else if (nArgs == 3) {
+		pThis->GetHandle().RawWrite(valList[0].GetUChar(), valList[1].GetUChar(),
+														valList[2].GetUChar());
+	} else if (nArgs == 4) {
+		pThis->GetHandle().RawWrite(valList[0].GetUChar(), valList[1].GetUChar(),
+							valList[2].GetUChar(), valList[3].GetUChar());
+	} else {
+		sig.SetError(ERR_ArgumentError, "too many arguments");
+		return Value::Null;
+	}
+	return Value::Null;
+}
+
+//-----------------------------------------------------------------------------
+// Class implementation for midi.device
+//-----------------------------------------------------------------------------
+Gura_ImplementUserClass(device)
+{
+	Gura_AssignMethod(device, rawwrite);
+}
+
+//-----------------------------------------------------------------------------
 // Gura module functions: midi
 //-----------------------------------------------------------------------------
+// midi.device(id?:number) {block?}
+Gura_DeclareFunction(device)
+{
+	SetMode(RSLTMODE_Normal, FLAG_None);
+	DeclareArg(env, "id", VTYPE_number, OCCUR_ZeroOrOnce);
+	DeclareBlock(OCCUR_ZeroOrOnce);
+	SetClassToConstruct(Gura_UserClass(device));
+	AddHelp(Gura_Symbol(en), "open a MIDI device and returns a handle to it.");
+}
+
+Gura_ImplementFunction(device)
+{
+	int id = args.IsNumber(0)? args.GetInt(0) : 0;
+	AutoPtr<Object_device> pObj(new Object_device(env));
+	if (!pObj->GetHandle().Open(id)) {
+		sig.SetError(ERR_IOError, "can't open MIDI device #%d", id);
+		return Value::Null;
+	}
+	return ReturnValue(env, sig, args, Value(pObj.release()));
+}
+
 // midi.test(num1:number, num2:number)
 Gura_DeclareFunction(test)
 {
@@ -19,7 +137,10 @@ Gura_DeclareFunction(test)
 
 Gura_ImplementFunction(test)
 {
-	return Value(args.GetNumber(0) + args.GetNumber(1));
+	::printf("%d\n", ::midiOutGetNumDevs());
+	::printf("%d\n", ::midiInGetNumDevs());
+	
+	return Value::Null;
 }
 
 //-----------------------------------------------------------------------------
@@ -27,7 +148,10 @@ Gura_ImplementFunction(test)
 //-----------------------------------------------------------------------------
 Gura_ModuleEntry()
 {
+	// class realization
+	Gura_RealizeUserClass(device, env.LookupClass(VTYPE_object));
 	// function assignment
+	Gura_AssignFunction(device);
 	Gura_AssignFunction(test);
 }
 
