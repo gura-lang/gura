67d83f20cd6dbd57cbdc13c1e3f0b2d0cb41ccd7
Yutaka Saito <ypsitau@nifty.com>
2013-05-29 17:25:45 +0900

    implementing midi module

--- src/Module_midi/MIDIHandle.cpp
+++ src/Module_midi/MIDIHandle.cpp
@@ -5,8 +5,6 @@ void MIDIHandle::MmlPlay(const char *mml)
 {
 	char ch = 0;
 	MmlParser parser(this);
-	//RawWrite(0x40 + ch, 100);
-	//RawWrite(0xc0 + ch, 0);
 	parser.Parse(mml);
 }
 
@@ -15,6 +13,7 @@ void MIDIHandle::MmlNote(MmlParser &parser, unsigned char note, int length)
 	char ch = 0;
 	RawWrite(0x90 + ch, note, 0x40);
 	Gura::OAL::Sleep(.01 * length);
+	RawWrite(0x90 + ch, note, 0x00);
 }
 
 void MIDIHandle::MmlRest(MmlParser &parser, int length)
@@ -28,7 +27,6 @@ void MIDIHandle::MmlVolume(MmlParser &parser, int volume)
 
 void MIDIHandle::MmlTone(MmlParser &parser, int tone)
 {
-	::printf("MmlTone\n");
 	char ch = 0;
 	RawWrite(0xc0 + ch, static_cast<unsigned char>(tone));
 }
