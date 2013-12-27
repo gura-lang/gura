073de7e01d5e68e33726ee2c43d3dd61fe319547
ypsitau <ypsitau@nifty.com>
2013-12-27 10:26:38 +0900

    removed unnecessary test script for markdown module

--- src/module-markdown/test.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-gura test-markdown.gura > result.txt
-if "%1" == "update" goto update
-diff -u result.sample.txt result.txt
-goto done
-:update
-copy result.txt result.sample.txt > nul
-:done
