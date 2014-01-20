6605aefbc2e7055a1357debcc40e3e3b6bb58f03
ypsitau <ypsitau@nifty.com>
2014-01-20 16:29:17 +0900

    renamed hash.hash -> hash.accumulator

--- src/module-hash/module-hash.cpp
+++ src/module-hash/module-hash.cpp
@@ -9,28 +9,24 @@ Gura_DeclareUserSymbol(digest);
 Gura_DeclareUserSymbol(hexdigest);
 
 //-----------------------------------------------------------------------------
-// Object_hash implementation
+// Object_accumulator implementation
 //-----------------------------------------------------------------------------
-Object_hash::Object_hash(Environment &env, HashBase *pHash, const char *name) :
-					Object_stream(Gura_UserClass(hash), pHash), _name(name)
+Object_accumulator::Object_accumulator(Environment &env, AccumulatorBase *pAccumulator, const char *name) :
+					Object_stream(Gura_UserClass(accumulator), pAccumulator), _name(name)
 {
 }
 
-Object_hash::Object_hash(Class *pClass, HashBase *pHash, const char *name) :
-					Object_stream(pClass, pHash), _name(name)
+Object_accumulator::Object_accumulator(Class *pClass, AccumulatorBase *pAccumulator, const char *name) :
+					Object_stream(pClass, pAccumulator), _name(name)
 {
 }
 
-Object_hash::~Object_hash()
-{
-}
-
-Object *Object_hash::Clone() const
+Object *Object_accumulator::Clone() const
 {
 	return NULL;
 }
 
-bool Object_hash::DoDirProp(Environment &env, Signal sig, SymbolSet &symbols)
+bool Object_accumulator::DoDirProp(Environment &env, Signal sig, SymbolSet &symbols)
 {
 	if (!Object::DoDirProp(env, sig, symbols)) return false;
 	symbols.insert(Gura_UserSymbol(digest));
@@ -39,15 +35,15 @@ bool Object_hash::DoDirProp(Environment &env, Signal sig, SymbolSet &symbols)
 	return true;
 }
 
-Value Object_hash::DoGetProp(Environment &env, Signal sig, const Symbol *pSymbol,
+Value Object_accumulator::DoGetProp(Environment &env, Signal sig, const Symbol *pSymbol,
 							const SymbolSet &attrs, bool &evaluatedFlag)
 {
 	if (pSymbol->IsIdentical(Gura_UserSymbol(digest))) {
 		evaluatedFlag = true;
-		return Value(new Object_binary(env, GetHash().GetDigest(), true));
+		return Value(new Object_binary(env, GetAccumulator().GetDigest(), true));
 	} else if (pSymbol->IsIdentical(Gura_UserSymbol(hexdigest))) {
 		evaluatedFlag = true;
-		const Binary &digest = GetHash().GetDigest();
+		const Binary &digest = GetAccumulator().GetDigest();
 		String str;
 		foreach_const (Binary, p, digest) {
 			char buff[8];
@@ -57,183 +53,183 @@ Value Object_hash::DoGetProp(Environment &env, Signal sig, const Symbol *pSymbol
 		return Value(str);
 	} else if (pSymbol->IsIdentical(Gura_Symbol(number))) {
 		evaluatedFlag = true;
-		return GetHash().GetValue();
+		return GetAccumulator().GetValue();
 	}
 	return Value::Null;
 }
 
-String Object_hash::ToString(bool exprFlag)
+String Object_accumulator::ToString(bool exprFlag)
 {
-	String str = "<hash:";
+	String str = "<hash.accumulator:";
 	str += _name;
 	str += ">";
 	return str;
 }
 
 //-----------------------------------------------------------------------------
-// Gura interfaces for Object_hash
+// Gura interfaces for Object_accumulator
 //-----------------------------------------------------------------------------
-// hash#init():reduce
-Gura_DeclareMethod(hash, init)
+// accumulator#init():reduce
+Gura_DeclareMethod(accumulator, init)
 {
 	SetMode(RSLTMODE_Reduce, FLAG_None);
 }
 
-Gura_ImplementMethod(hash, init)
+Gura_ImplementMethod(accumulator, init)
 {
-	HashBase &hash = Object_hash::GetThisObj(args)->GetHash();
-	hash.Init();
+	AccumulatorBase &accumulator = Object_accumulator::GetThisObj(args)->GetAccumulator();
+	accumulator.Init();
 	return args.GetThis();
 }
 
-// hash#update(stream:stream:r):reduce
-Gura_DeclareMethod(hash, update)
+// accumulator#update(stream:stream:r):reduce
+Gura_DeclareMethod(accumulator, update)
 {
 	SetMode(RSLTMODE_Reduce, FLAG_None);
 	DeclareArg(env, "stream", VTYPE_stream, OCCUR_Once, FLAG_Read);
 }
 
-Gura_ImplementMethod(hash, update)
+Gura_ImplementMethod(accumulator, update)
 {
-	HashBase &hash = Object_hash::GetThisObj(args)->GetHash();
-	args.GetStream(0).ReadToStream(env, sig, hash, 0x10000, false);
+	AccumulatorBase &accumulator = Object_accumulator::GetThisObj(args)->GetAccumulator();
+	args.GetStream(0).ReadToStream(env, sig, accumulator, 0x10000, false);
 	return args.GetThis();
 }
 
-// implementation of class Hash
-Gura_ImplementUserClass(hash)
+// implementation of class Accumulator
+Gura_ImplementUserClass(accumulator)
 {
-	Gura_AssignMethod(hash, init);
-	Gura_AssignMethod(hash, update);
+	Gura_AssignMethod(accumulator, init);
+	Gura_AssignMethod(accumulator, update);
 }
 
 //-----------------------------------------------------------------------------
-// HashBase implementation
+// AccumulatorBase implementation
 //-----------------------------------------------------------------------------
-const char *HashBase::GetIdentifier() const
+const char *AccumulatorBase::GetIdentifier() const
 {
 	return NULL;
 }
 
-size_t HashBase::DoRead(Signal sig, void *buff, size_t len)
+size_t AccumulatorBase::DoRead(Signal sig, void *buff, size_t len)
 {
 	return 0;
 }
 
-bool HashBase::DoSeek(Signal sig, long offset, size_t offsetPrev, SeekMode seekMode) { return false; }
-bool HashBase::DoFlush(Signal sig) { return true; }
-bool HashBase::DoClose(Signal sig) { return Stream::DoClose(sig); }
+bool AccumulatorBase::DoSeek(Signal sig, long offset, size_t offsetPrev, SeekMode seekMode) { return false; }
+bool AccumulatorBase::DoFlush(Signal sig) { return true; }
+bool AccumulatorBase::DoClose(Signal sig) { return Stream::DoClose(sig); }
 
-const Value &HashBase::GetValue()
+const Value &AccumulatorBase::GetValue()
 {
 	return _value;
 }
 
 //-----------------------------------------------------------------------------
-// Hash_MD5 implementation
+// Accumulator_MD5 implementation
 //-----------------------------------------------------------------------------
-Hash_MD5::Hash_MD5(Environment &env, Signal sig) : HashBase(env, sig)
+Accumulator_MD5::Accumulator_MD5(Environment &env, Signal sig) : AccumulatorBase(env, sig)
 {
 	_digest.clear();
 	::md5_init(&_state);
 }
 
-void Hash_MD5::Init()
+void Accumulator_MD5::Init()
 {
 	_digest.clear();
 	::md5_init(&_state);
 }
 
-const char *Hash_MD5::GetName() const
+const char *Accumulator_MD5::GetName() const
 {
-	return "<hash:md5>";
+	return "<hash.accumulator:md5>";
 }
 
-size_t Hash_MD5::DoWrite(Signal sig, const void *buff, size_t len)
+size_t Accumulator_MD5::DoWrite(Signal sig, const void *buff, size_t len)
 {
 	::md5_append(&_state, reinterpret_cast<const md5_byte_t *>(buff), static_cast<int>(len));
 	return len;
 }
 
-void Hash_MD5::Finish()
+void Accumulator_MD5::Finish()
 {
 	md5_byte_t digest[16];
 	::md5_finish(&_state, digest);
 	_digest = Binary(reinterpret_cast<char *>(digest), sizeof(digest));
 }
 
-const Binary &Hash_MD5::GetDigest()
+const Binary &Accumulator_MD5::GetDigest()
 {
 	if (_digest.empty()) Finish();
 	return _digest;
 }
 
 //-----------------------------------------------------------------------------
-// Hash_SHA1 implementation
+// Accumulator_SHA1 implementation
 //-----------------------------------------------------------------------------
-Hash_SHA1::Hash_SHA1(Environment &env, Signal sig) : HashBase(env, sig)
+Accumulator_SHA1::Accumulator_SHA1(Environment &env, Signal sig) : AccumulatorBase(env, sig)
 {
 	_digest.clear();
 	::sha1_starts(&_ctx);
 }
 
-void Hash_SHA1::Init()
+void Accumulator_SHA1::Init()
 {
 	_digest.clear();
 	::sha1_starts(&_ctx);
 }
 
-const char *Hash_SHA1::GetName() const
+const char *Accumulator_SHA1::GetName() const
 {
-	return "<hash:sha1>";
+	return "<hash.accumulator:sha1>";
 }
 
-size_t Hash_SHA1::DoWrite(Signal sig, const void *buff, size_t len)
+size_t Accumulator_SHA1::DoWrite(Signal sig, const void *buff, size_t len)
 {
 	::sha1_update(&_ctx, reinterpret_cast<uint8 *>(const_cast<void *>(buff)),
 													static_cast<uint32>(len));
 	return len;
 }
 
-void Hash_SHA1::Finish()
+void Accumulator_SHA1::Finish()
 {
 	uint8 digest[20];
 	::sha1_finish(&_ctx, digest);
 	_digest = Binary(reinterpret_cast<char *>(digest), sizeof(digest));
 }
 
-const Binary &Hash_SHA1::GetDigest()
+const Binary &Accumulator_SHA1::GetDigest()
 {
 	if (_digest.empty()) Finish();
 	return _digest;
 }
 
 //-----------------------------------------------------------------------------
-// Hash_CRC32 implementation
+// Accumulator_CRC32 implementation
 //-----------------------------------------------------------------------------
-Hash_CRC32::Hash_CRC32(Environment &env, Signal sig) : HashBase(env, sig)
+Accumulator_CRC32::Accumulator_CRC32(Environment &env, Signal sig) : AccumulatorBase(env, sig)
 {
 	_digest.clear();
 }
 
-void Hash_CRC32::Init()
+void Accumulator_CRC32::Init()
 {
 	_digest.clear();
 	_crc32.Initialize();
 }
 
-const char *Hash_CRC32::GetName() const
+const char *Accumulator_CRC32::GetName() const
 {
-	return "<hash:crc32>";
+	return "<accumulator:crc32>";
 }
 
-size_t Hash_CRC32::DoWrite(Signal sig, const void *buff, size_t len)
+size_t Accumulator_CRC32::DoWrite(Signal sig, const void *buff, size_t len)
 {
 	_crc32.Update(buff, len);
 	return len;
 }
 
-void Hash_CRC32::Finish()
+void Accumulator_CRC32::Finish()
 {
 	UChar digest[4];
 	ULong result = _crc32.GetResult();
@@ -245,13 +241,13 @@ void Hash_CRC32::Finish()
 	_digest = Binary(reinterpret_cast<char *>(digest), sizeof(digest));
 }
 
-const Binary &Hash_CRC32::GetDigest()
+const Binary &Accumulator_CRC32::GetDigest()
 {
 	if (_digest.empty()) Finish();
 	return _digest;
 }
 
-const Value &Hash_CRC32::GetValue()
+const Value &Accumulator_CRC32::GetValue()
 {
 	if (_digest.empty()) Finish();
 	return _value;
@@ -270,9 +266,9 @@ Gura_DeclareFunction(md5)
 
 Gura_ImplementFunction(md5)
 {
-	Object_hash *pObj = new Object_hash(env, new Hash_MD5(env, sig), "md5");
+	Object_accumulator *pObj = new Object_accumulator(env, new Accumulator_MD5(env, sig), "md5");
 	if (args.Is_stream(0)) {
-		args.GetStream(0).ReadToStream(env, sig, pObj->GetHash(), 0x10000, false);
+		args.GetStream(0).ReadToStream(env, sig, pObj->GetAccumulator(), 0x10000, false);
 	}
 	return ReturnValue(env, sig, args, Value(pObj));
 }
@@ -287,9 +283,9 @@ Gura_DeclareFunction(sha1)
 
 Gura_ImplementFunction(sha1)
 {
-	Object_hash *pObj = new Object_hash(env, new Hash_SHA1(env, sig), "sha1");
+	Object_accumulator *pObj = new Object_accumulator(env, new Accumulator_SHA1(env, sig), "sha1");
 	if (args.Is_stream(0)) {
-		args.GetStream(0).ReadToStream(env, sig, pObj->GetHash(), 0x10000, false);
+		args.GetStream(0).ReadToStream(env, sig, pObj->GetAccumulator(), 0x10000, false);
 	}
 	return ReturnValue(env, sig, args, Value(pObj));
 }
@@ -304,9 +300,9 @@ Gura_DeclareFunction(crc32)
 
 Gura_ImplementFunction(crc32)
 {
-	Object_hash *pObj = new Object_hash(env, new Hash_CRC32(env, sig), "crc32");
+	Object_accumulator *pObj = new Object_accumulator(env, new Accumulator_CRC32(env, sig), "crc32");
 	if (args.Is_stream(0)) {
-		args.GetStream(0).ReadToStream(env, sig, pObj->GetHash(), 0x10000, false);
+		args.GetStream(0).ReadToStream(env, sig, pObj->GetAccumulator(), 0x10000, false);
 	}
 	return ReturnValue(env, sig, args, Value(pObj));
 }
@@ -321,7 +317,7 @@ Gura_ModuleEntry()
 	Gura_AssignFunction(sha1);
 	Gura_AssignFunction(crc32);
 	// class realization
-	Gura_RealizeUserClass(hash, env.LookupClass(VTYPE_stream));
+	Gura_RealizeUserClass(accumulator, env.LookupClass(VTYPE_stream));
 }
 
 Gura_ModuleTerminate()
--- src/module-hash/module-hash.h
+++ src/module-hash/module-hash.h
@@ -13,14 +13,14 @@ extern "C" {
 Gura_BeginModuleHeader(hash)
 
 //-----------------------------------------------------------------------------
-// HashBase declaration
+// AccumulatorBase declaration
 //-----------------------------------------------------------------------------
-class HashBase : public Stream {
+class AccumulatorBase : public Stream {
 protected:
 	Value _value;
 	Binary _digest;
 public:
-	inline HashBase(Environment &env, Signal sig) : Stream(env, sig, ATTR_Infinite | ATTR_Writable) {}
+	inline AccumulatorBase(Environment &env, Signal sig) : Stream(env, sig, ATTR_Infinite | ATTR_Writable) {}
 	virtual const char *GetIdentifier() const;
 	virtual void Init() = 0;
 	virtual void Finish() = 0;
@@ -34,37 +34,36 @@ public:
 };
 
 //-----------------------------------------------------------------------------
-// Object_hash declaration
+// Object_accumulator declaration
 //-----------------------------------------------------------------------------
-Gura_DeclareUserClass(hash);
+Gura_DeclareUserClass(accumulator);
 
-class Object_hash : public Object_stream {
+class Object_accumulator : public Object_stream {
 public:
-	Gura_DeclareObjectAccessor(hash)
+	Gura_DeclareObjectAccessor(accumulator)
 private:
 	String _name;
 public:
-	Object_hash(Environment &env, HashBase *pHash, const char *name);
-	Object_hash(Class *pClass, HashBase *pHash, const char *name);
-	virtual ~Object_hash();
+	Object_accumulator(Environment &env, AccumulatorBase *pAccumulator, const char *name);
+	Object_accumulator(Class *pClass, AccumulatorBase *pAccumulator, const char *name);
 	virtual Object *Clone() const;
 	virtual bool DoDirProp(Environment &env, Signal sig, SymbolSet &symbols);
 	virtual Value DoGetProp(Environment &env, Signal sig, const Symbol *pSymbol,
 						const SymbolSet &attrs, bool &evaluatedFlag);
 	virtual String ToString(bool exprFlag);
-	inline HashBase &GetHash() {
-		return *dynamic_cast<HashBase *>(&GetStream());
+	inline AccumulatorBase &GetAccumulator() {
+		return *dynamic_cast<AccumulatorBase *>(&GetStream());
 	}
 };
 
 //-----------------------------------------------------------------------------
-// Hash_MD5 declaration
+// Accumulator_MD5 declaration
 //-----------------------------------------------------------------------------
-class Hash_MD5 : public HashBase {
+class Accumulator_MD5 : public AccumulatorBase {
 private:
 	md5_state_t _state;
 public:
-	Hash_MD5(Environment &env, Signal sig);
+	Accumulator_MD5(Environment &env, Signal sig);
 	virtual void Init();
 	virtual const char *GetName() const;
 	virtual size_t DoWrite(Signal sig, const void *buff, size_t len);
@@ -73,13 +72,13 @@ public:
 };
 
 //-----------------------------------------------------------------------------
-// Hash_SHA1 declaration
+// Accumulator_SHA1 declaration
 //-----------------------------------------------------------------------------
-class Hash_SHA1 : public HashBase {
+class Accumulator_SHA1 : public AccumulatorBase {
 private:
     sha1_context _ctx;
 public:
-	Hash_SHA1(Environment &env, Signal sig);
+	Accumulator_SHA1(Environment &env, Signal sig);
 	virtual void Init();
 	virtual const char *GetName() const;
 	virtual size_t DoWrite(Signal sig, const void *buff, size_t len);
@@ -88,13 +87,13 @@ public:
 };
 
 //-----------------------------------------------------------------------------
-// Hash_CRC32 declaration
+// Accumulator_CRC32 declaration
 //-----------------------------------------------------------------------------
-class Hash_CRC32 : public HashBase {
+class Accumulator_CRC32 : public AccumulatorBase {
 private:
 	CRC32 _crc32;
 public:
-	Hash_CRC32(Environment &env, Signal sig);
+	Accumulator_CRC32(Environment &env, Signal sig);
 	virtual void Init();
 	virtual const char *GetName() const;
 	virtual size_t DoWrite(Signal sig, const void *buff, size_t len);
