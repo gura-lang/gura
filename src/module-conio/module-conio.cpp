e3a1a8e85d0f9bc0df27b737add4840aedc28f1c
ypsitau <ypsitau@nifty.com>
2014-01-20 11:14:49 +0900

    added key-code variable in conio module

--- src/module-conio/module-conio.cpp
+++ src/module-conio/module-conio.cpp
@@ -1,6 +1,6 @@
-//-----------------------------------------------------------------------------
+//=============================================================================
 // Gura module: conio
-//-----------------------------------------------------------------------------
+//=============================================================================
 #include "stdafx.h"
 
 #define Gura_AssignValueOf(v) Gura_AssignValue(v, v)
@@ -8,7 +8,33 @@
 Gura_BeginModuleBody(conio)
 
 enum {
-	K_BACKSPACE	= 0x7f,
+	K_CTRL_A	= 0x01,
+	K_CTRL_B	= 0x02,
+	K_CTRL_C	= 0x03,
+	K_CTRL_D	= 0x04,
+	K_CTRL_E	= 0x05,
+	K_CTRL_F	= 0x06,
+	K_CTRL_G	= 0x07,
+	K_CTRL_H	= 0x08,
+	K_CTRL_I	= 0x09,
+	K_CTRL_J	= 0x0a,
+	K_CTRL_K	= 0x0b,
+	K_CTRL_L	= 0x0c,
+	K_CTRL_M	= 0x0d,
+	K_CTRL_N	= 0x0e,
+	K_CTRL_O	= 0x0f,
+	K_CTRL_P	= 0x10,
+	K_CTRL_Q	= 0x11,
+	K_CTRL_R	= 0x12,
+	K_CTRL_S	= 0x13,
+	K_CTRL_T	= 0x14,
+	K_CTRL_U	= 0x15,
+	K_CTRL_V	= 0x16,
+	K_CTRL_W	= 0x17,
+	K_CTRL_X	= 0x18,
+	K_CTRL_Y	= 0x19,
+	K_CTRL_Z	= 0x1a,
+	K_BACKSPACE	= 0x08,
 	K_TAB		= 0x09,
 	K_RETURN	= 0x0a,
 	K_ESCAPE	= 0x1b,
@@ -511,6 +537,32 @@ Gura_ModuleEntry()
 	Gura_AssignFunction(moveto);
 	Gura_AssignFunction(waitkey);
 	// value assignment
+	Gura_AssignValueOf(K_CTRL_A);
+	Gura_AssignValueOf(K_CTRL_B);
+	Gura_AssignValueOf(K_CTRL_C);
+	Gura_AssignValueOf(K_CTRL_D);
+	Gura_AssignValueOf(K_CTRL_E);
+	Gura_AssignValueOf(K_CTRL_F);
+	Gura_AssignValueOf(K_CTRL_G);
+	Gura_AssignValueOf(K_CTRL_H);
+	Gura_AssignValueOf(K_CTRL_I);
+	Gura_AssignValueOf(K_CTRL_J);
+	Gura_AssignValueOf(K_CTRL_K);
+	Gura_AssignValueOf(K_CTRL_L);
+	Gura_AssignValueOf(K_CTRL_M);
+	Gura_AssignValueOf(K_CTRL_N);
+	Gura_AssignValueOf(K_CTRL_O);
+	Gura_AssignValueOf(K_CTRL_P);
+	Gura_AssignValueOf(K_CTRL_Q);
+	Gura_AssignValueOf(K_CTRL_R);
+	Gura_AssignValueOf(K_CTRL_S);
+	Gura_AssignValueOf(K_CTRL_T);
+	Gura_AssignValueOf(K_CTRL_U);
+	Gura_AssignValueOf(K_CTRL_V);
+	Gura_AssignValueOf(K_CTRL_W);
+	Gura_AssignValueOf(K_CTRL_X);
+	Gura_AssignValueOf(K_CTRL_Y);
+	Gura_AssignValueOf(K_CTRL_Z);
 	Gura_AssignValueOf(K_BACKSPACE);
 	Gura_AssignValueOf(K_TAB);
 	Gura_AssignValueOf(K_RETURN);
