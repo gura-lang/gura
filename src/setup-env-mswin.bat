a23964988c44d9db3117083a3c20e71938d1a422
ypsitau <ypsitau@nifty.com>
2014-03-01 20:56:02 +0900

    modified repository

--- src/setup-env-mswin.bat
+++ src/setup-env-mswin.bat
@@ -4,12 +4,12 @@ rem Setup development environment
 rem ==========================================================================
 pushd ..
 if exist gura-doc rmdir /s /q gura-doc
-git clone https://github.com/ypsitau/gura-doc.git
+git clone https://github.com/gura-lang/gura-doc.git
 popd
 if exist ..\gura-guest\setup.bat goto update
 pushd ..
 if exist gura-guest rmdir /s /q gura-guest
-git clone https://github.com/ypsitau/gura-guest.git
+git clone https://github.com/gura-lang/gura-guest.git
 popd
 pushd ..\gura-guest
 goto setup
