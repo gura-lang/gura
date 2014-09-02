68dc37482f09ad11521a9d517d25a02eb3d767a5
ypsitau <ypsitau@nifty.com>
2014-09-02 17:38:25 +0900

    prepared naked source code for sdl2 module

---
+++ src/module-sdl2/module-sdl2.h
@@ -0,0 +1,13 @@
+//-----------------------------------------------------------------------------
+// Gura module: sdl2
+//-----------------------------------------------------------------------------
+#ifndef __GURA_MODULE_SDL2_H__
+#define __GURA_MODULE_SDL2_H__
+#include <gura.h>
+#include <SDL.h>
+
+Gura_BeginModuleHeader(sdl)
+
+Gura_EndModuleHeader(sdl)
+
+#endif
---
+++ src/module-sdl2/stdafx.h
@@ -0,0 +1,4 @@
+#ifndef __GURA_MODULE_SDL2_STDAFX_H__
+#define __GURA_MODULE_SDL2_STDAFX_H__
+#include "module-sdl2.h"
+#endif
