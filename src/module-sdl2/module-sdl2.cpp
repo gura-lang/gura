f9c8e856222d91fc5d7833ea0e3cf69f3ef7289f
ypsitau <ypsitau@nifty.com>
2014-09-02 18:07:20 +0900

    implementing sdl2 module

--- src/module-sdl2/module-sdl2.cpp
+++ src/module-sdl2/module-sdl2.cpp
@@ -6,8 +6,100 @@
 Gura_BeginModuleBody(sdl2)
 
 //-----------------------------------------------------------------------------
-// Module functions
+// SDL functions: Initialization and Shutdown
 //-----------------------------------------------------------------------------
+// sdl2.Init(flags:number)
+Gura_DeclareFunction(Init)
+{
+	SetMode(RSLTMODE_Normal, FLAG_None);
+	DeclareArg(env, "flags", VTYPE_number);
+	AddHelp(Gura_Symbol(en), Help::FMT_markdown,
+	"");
+}
+
+Gura_ImplementFunction(Init)
+{
+	Uint32 flags = args.GetULong(0);
+	int rtn = ::SDL_Init(flags);
+	return Value(rtn);
+}
+
+// sdl2.InitSubSystem(flags:number)
+Gura_DeclareFunction(InitSubSystem)
+{
+	SetMode(RSLTMODE_Normal, FLAG_None);
+	DeclareArg(env, "flags", VTYPE_number);
+	AddHelp(Gura_Symbol(en), Help::FMT_markdown,
+	"");
+}
+
+Gura_ImplementFunction(InitSubSystem)
+{
+	Uint32 flags = args.GetULong(0);
+	int rtn = ::SDL_InitSubSystem(flags);
+	return Value(rtn);
+}
+
+// sdl2.Quit():void
+Gura_DeclareFunction(Quit)
+{
+	SetMode(RSLTMODE_Void, FLAG_None);
+	AddHelp(Gura_Symbol(en), Help::FMT_markdown,
+	"");
+}
+
+Gura_ImplementFunction(Quit)
+{
+	::SDL_Quit();
+	return Value::Null;
+}
+
+// sdl2.QuitSubSystem(flags:number):void
+Gura_DeclareFunction(QuitSubSystem)
+{
+	SetMode(RSLTMODE_Void, FLAG_None);
+	DeclareArg(env, "flags", VTYPE_number);
+	AddHelp(Gura_Symbol(en), Help::FMT_markdown,
+	"");
+}
+
+Gura_ImplementFunction(QuitSubSystem)
+{
+	Uint32 flags = args.GetULong(0);
+	::SDL_QuitSubSystem(flags);
+	return Value::Null;
+}
+
+// sdl2.SetMainReady():void
+Gura_DeclareFunction(SetMainReady)
+{
+	SetMode(RSLTMODE_Void, FLAG_None);
+	AddHelp(Gura_Symbol(en), Help::FMT_markdown,
+	"");
+}
+
+Gura_ImplementFunction(SetMainReady)
+{
+	::SDL_SetMainReady();
+	return Value::Null;
+}
+
+// sdl2.WasInit(flags:number)
+Gura_DeclareFunction(WasInit)
+{
+	SetMode(RSLTMODE_Normal, FLAG_None);
+	DeclareArg(env, "flags", VTYPE_number);
+	AddHelp(Gura_Symbol(en), Help::FMT_markdown,
+	"");
+}
+
+Gura_ImplementFunction(WasInit)
+{
+	Uint32 flags = args.GetULong(0);
+	Uint32 rtn = ::SDL_WasInit(flags);
+	return Value(rtn);
+}
+
 // sdl2.test(num1:number, num2:number)
 Gura_DeclareFunction(test)
 {
@@ -17,7 +109,7 @@ Gura_DeclareFunction(test)
 Gura_ImplementFunction(test)
 {
 	::SDL_Init(SDL_INIT_VIDEO);
-	::SDL_Window *window = SDL_CreateWindow("Hey",
+	SDL_Window *window = ::SDL_CreateWindow("Hey",
 				SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
 	::SDL_Renderer *renderer = ::SDL_CreateRenderer(window, -1, 0);
 	SDL_Event ev;
@@ -40,6 +132,12 @@ Gura_ImplementFunction(test)
 Gura_ModuleEntry()
 {
 	// function assignment
+	// Initialization and Shutdown
+	Gura_AssignFunction(Init);
+	Gura_AssignFunction(InitSubSystem);
+	Gura_AssignFunction(Quit);
+	Gura_AssignFunction(QuitSubSystem);
+	Gura_AssignFunction(SetMainReady);
 	Gura_AssignFunction(test);
 	return true;
 }
