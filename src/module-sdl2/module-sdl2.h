d0bde20a42c73f2db4cf17fbc495c21649a54f6a
ypsitau <ypsitau@nifty.com>
2014-09-02 17:46:29 +0900

    implementing sdl2 module

--- src/module-sdl2/module-sdl2.h
+++ src/module-sdl2/module-sdl2.h
@@ -1,13 +1,11 @@
-//-----------------------------------------------------------------------------
+//=============================================================================
 // Gura module: sdl2
-//-----------------------------------------------------------------------------
+//=============================================================================
 #ifndef __GURA_MODULE_SDL2_H__
 #define __GURA_MODULE_SDL2_H__
 #include <gura.h>
-#include <SDL.h>
 
-Gura_BeginModuleHeader(sdl)
-
-Gura_EndModuleHeader(sdl)
+Gura_BeginModuleHeader(sdl2)
+Gura_EndModuleHeader(sdl2)
 
 #endif
