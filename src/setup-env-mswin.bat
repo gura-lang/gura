d2766c716024eb58c678ff7a0fdf86a6b628c81e
ypsitau <ypsitau@nifty.com>
2014-09-13 22:35:58 +0900

    eliminate gura-guest

--- src/setup-env-mswin.bat
+++ src/setup-env-mswin.bat
@@ -6,17 +6,6 @@ pushd ..
 if exist gura-doc rmdir /s /q gura-doc
 git clone https://github.com/gura-lang/gura-doc.git
 popd
-if exist ..\gura-guest\setup.bat goto update
-pushd ..
-if exist gura-guest rmdir /s /q gura-guest
-git clone https://github.com/gura-lang/gura-guest.git
-popd
-pushd ..\gura-guest
-goto setup
-:update
-pushd ..\gura-guest
-git pull
-goto setup
-:setup
-call setup.bat
+pushd ..\guests
+call prepare-for-mswin.bat
 popd
