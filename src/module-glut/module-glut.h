92798bb4419bd4f3c224272f4fe4e22f893c238d
ypsitau <ypsitau@nifty.com>
2014-11-23 19:46:48 +0900

    added empty project named module-glut

---
+++ src/module-glut/module-glut.h
@@ -0,0 +1,11 @@
+//=============================================================================
+// Gura module: glut
+//=============================================================================
+#ifndef __GURA_MODULE_GLUT_H__
+#define __GURA_MODULE_GLUT_H__
+#include <gura.h>
+
+Gura_BeginModuleHeader(glut)
+Gura_EndModuleHeader(glut)
+
+#endif
