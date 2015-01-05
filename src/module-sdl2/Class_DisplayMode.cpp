57f24c3960071f544d64024916b55d95753851fd
ypsitau <ypsitau@nifty.com>
2015-01-05 11:19:44 +0900

    implementing DisplayMode class

--- src/module-sdl2/Class_DisplayMode.cpp
+++ src/module-sdl2/Class_DisplayMode.cpp
@@ -50,6 +50,20 @@ Value Object_DisplayMode::DoSetProp(Environment &env, Signal sig,
 							  const Symbol *pSymbol, const Value &value,
 							  const SymbolSet &attrs, bool &evaluatedFlag)
 {
+	evaluatedFlag = true;
+	if (pSymbol->IsIdentical(Gura_UserSymbol(format))) {
+		_mode.format = value.GetLong();
+		return Value(_mode.format);
+	} else if (pSymbol->IsIdentical(Gura_UserSymbol(w))) {
+		_mode.w = value.GetInt();
+		return Value(_mode.w);
+	} else if (pSymbol->IsIdentical(Gura_UserSymbol(h))) {
+		_mode.h = value.GetInt();
+		return Value(_mode.h);
+	} else if (pSymbol->IsIdentical(Gura_UserSymbol(refresh_rate))) {
+		_mode.refresh_rate = value.GetInt();
+		return Value(_mode.refresh_rate);
+	}
 	evaluatedFlag = false;
 	return Value::Null;
 }
@@ -57,9 +71,26 @@ Value Object_DisplayMode::DoSetProp(Environment &env, Signal sig,
 //-----------------------------------------------------------------------------
 // Gura interfaces for DisplayMode
 //-----------------------------------------------------------------------------
+// sdl2.DisplayMode()
+Gura_DeclareFunction(DisplayMode)
+{
+	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
+	AddHelp(
+		Gura_Symbol(en), Help::FMT_markdown,
+		"");
+}
+
+Gura_ImplementFunction(DisplayMode)
+{
+	SDL_DisplayMode mode;
+	::memset(&mode, 0x00, sizeof(mode));
+	return Value(new Object_DisplayMode(mode));
+}
+
 // implementation of class DisplayMode
 Gura_ImplementUserClass(DisplayMode)
 {
+	Gura_AssignFunction(DisplayMode);
 }
 
 Gura_EndModuleScope(sdl2)
