14dd535acbe6c9ef5e50856f0a0feb473c380d54
ypsitau <ypsitau@nifty.com>
2015-01-21 19:32:50 +0900

    adds help information to base64 module

--- src/module-base64/module-base64.cpp
+++ src/module-base64/module-base64.cpp
@@ -8,14 +8,18 @@ Gura_BeginModuleBody(base64)
 //-----------------------------------------------------------------------------
 // Gura module functions: base64
 //-----------------------------------------------------------------------------
-// base64.decode(stream:stream:r)
+// base64.decode(stream:stream:r) {block?}
 Gura_DeclareFunction(decode)
 {
 	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
 	DeclareArg(env, "stream", VTYPE_stream, OCCUR_Once, FLAG_Read);
+	DeclareBlock(OCCUR_ZeroOrOnce);
 	AddHelp(
 		Gura_Symbol(en), Help::FMT_markdown,
-		"Reads text stream that is formatted in base64 and returns the decoded result in binary.");
+		"Reads text stream that is formatted in base64 and returns the decoded result in binary.\n"
+		"\n"
+		"If `block` is specified, it would be evaluated with block parameter `|data:binary|`,\n"
+		"where `data` is the created binary instance.\n");
 }
 
 Gura_ImplementFunction(decode)
@@ -26,18 +30,22 @@ Gura_ImplementFunction(decode)
 	AutoPtr<Stream> pStreamDst(new Stream_Binary(env, sig,
 									Object_binary::Reference(pObjBinary.get()), true));
 	if (!pStreamSrc->ReadToStream(env, sig, *pStreamDst)) return Value::Null;
-	return Value(pObjBinary.release());
+	return ReturnValue(env, sig, args, Value(pObjBinary.release()));
 }
 
-// base64.encode(stream:stream:r, linelen:number => 76)
+// base64.encode(stream:stream:r, linelen:number => 76) {block?}
 Gura_DeclareFunction(encode)
 {
 	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
 	DeclareArg(env, "stream", VTYPE_stream, OCCUR_Once, FLAG_Read);
 	DeclareArg(env, "linelen", VTYPE_number, OCCUR_Once, FLAG_Nil, new Expr_Value(76));
+	DeclareBlock(OCCUR_ZeroOrOnce);
 	AddHelp(
 		Gura_Symbol(en), Help::FMT_markdown,
-		"Encodes content of the stream into base64 format and returns the result in binary.");
+		"Encodes content of the stream into base64 format and returns the result in binary.\n"
+		"\n"
+		"If `block` is specified, it would be evaluated with block parameter `|data:binary|`,\n"
+		"where `data` is the created binary instance.\n");
 }
 
 Gura_ImplementFunction(encode)
@@ -48,38 +56,46 @@ Gura_ImplementFunction(encode)
 	AutoPtr<Stream> pStreamDst(new Stream_Base64Writer(env, sig,
 		new Stream_Binary(env, sig, Object_binary::Reference(pObjBinary.get()), true), nCharsPerLine));
 	if (!streamSrc.ReadToStream(env, sig, *pStreamDst)) return Value::Null;
-	return Value(pObjBinary.release());
+	return ReturnValue(env, sig, args, Value(pObjBinary.release()));
 }
 
-// base64.reader(stream:stream:r)
+// base64.reader(stream:stream:r) {block?}
 Gura_DeclareFunction(reader)
 {
 	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
 	DeclareArg(env, "stream", VTYPE_stream, OCCUR_Once, FLAG_Read);
+	DeclareBlock(OCCUR_ZeroOrOnce);
 	AddHelp(
 		Gura_Symbol(en), Help::FMT_markdown,
-		"Creates a stream instance that reads data formatted in base64 from `stream`.\n");
+		"Creates a stream instance that reads data formatted in base64 from `stream`.\n"
+		"\n"
+		"If `block` is specified, it would be evaluated with block parameter `|s:stream|`,\n"
+		"where `s` is the created stream instance.\n");
 }
 
 Gura_ImplementFunction(reader)
 {
 	Stream &stream = args.GetStream(0);
 	AutoPtr<Stream> pStream(new Stream_Base64Reader(env, sig, stream.Reference()));
-	return Value(new Object_stream(env, pStream.release()));
+	return ReturnValue(env, sig, args, Value(new Object_stream(env, pStream.release())));
 }
 
-// base64.writer(stream:stream:w, linelen:number => 76)
+// base64.writer(stream:stream:w, linelen:number => 76) {block?}
 Gura_DeclareFunction(writer)
 {
 	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
 	DeclareArg(env, "stream", VTYPE_stream, OCCUR_Once, FLAG_Write);
 	DeclareArg(env, "linelen", VTYPE_number, OCCUR_Once, FLAG_Nil, new Expr_Value(76));
+	DeclareBlock(OCCUR_ZeroOrOnce);
 	AddHelp(
 		Gura_Symbol(en), Help::FMT_markdown,
 		"Creates a stream instance that encodes data to base64 format and writes it to the `stream`.\n"
 		"\n"
 		"The number of characters per line is specified by an argument `linelen`.\n"
-		"If omitted, that is 76.\n");
+		"If omitted, that is 76.\n"
+		"\n"
+		"If `block` is specified, it would be evaluated with block parameter `|s:stream|`,\n"
+		"where `s` is the created stream instance.\n");
 }
 
 Gura_ImplementFunction(writer)
@@ -88,39 +104,47 @@ Gura_ImplementFunction(writer)
 	int nCharsPerLine = args.Is_number(1)? args.GetInt(1) : -1;
 	AutoPtr<Stream> pStream(new Stream_Base64Writer(env, sig,
 								stream.Reference(), nCharsPerLine));
-	return Value(new Object_stream(env, pStream.release()));
+	return ReturnValue(env, sig, args, Value(new Object_stream(env, pStream.release())));
 }
 
 //-----------------------------------------------------------------------------
 // Gura interfaces for stream class
 //-----------------------------------------------------------------------------
-// stream#reader@base64()
+// stream#reader@base64() {block?}
 Gura_DeclareMethodAlias(stream, reader_base64, "reader@base64")
 {
 	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
+	DeclareBlock(OCCUR_ZeroOrOnce);
 	AddHelp(
 		Gura_Symbol(en), Help::FMT_markdown,
-		"Creates a stream instance that reads data formatted in base64 from the target stream instance.\n");
+		"Creates a stream instance that reads data formatted in base64 from the target stream instance.\n"
+		"\n"
+		"If `block` is specified, it would be evaluated with block parameter `|s:stream|`,\n"
+		"where `s` is the created stream instance.\n");
 }
 
 Gura_ImplementMethod(stream, reader_base64)
 {
 	Stream &stream = Object_stream::GetThisObj(args)->GetStream();
 	AutoPtr<Stream> pStream(new Stream_Base64Reader(env, sig, stream.Reference()));
-	return Value(new Object_stream(env, pStream.release()));
+	return ReturnValue(env, sig, args, Value(new Object_stream(env, pStream.release())));
 }
 
-// stream#writer@base64(linelen:number => 76)
+// stream#writer@base64(linelen:number => 76) {block?}
 Gura_DeclareMethodAlias(stream, writer_base64, "writer@base64")
 {
 	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
 	DeclareArg(env, "linelen", VTYPE_number, OCCUR_Once, FLAG_Nil, new Expr_Value(76));
+	DeclareBlock(OCCUR_ZeroOrOnce);
 	AddHelp(
 		Gura_Symbol(en), Help::FMT_markdown,
 		"Creates a stream instance that encodes data to base64 format and writes it to the target stream instance.\n"
 		"\n"
 		"The number of characters per line is specified by an argument `linelen`.\n"
-		"If omitted, that is 76.\n");
+		"If omitted, that is 76.\n"
+		"\n"
+		"If `block` is specified, it would be evaluated with block parameter `|s:stream|`,\n"
+		"where `s` is the created stream instance.\n");
 }
 
 Gura_ImplementMethod(stream, writer_base64)
@@ -129,7 +153,7 @@ Gura_ImplementMethod(stream, writer_base64)
 	Stream &stream = Object_stream::GetThisObj(args)->GetStream();
 	AutoPtr<Stream> pStream(new Stream_Base64Writer(env, sig,
 								stream.Reference(), nCharsPerLine));
-	return Value(new Object_stream(env, pStream.release()));
+	return ReturnValue(env, sig, args, Value(new Object_stream(env, pStream.release())));
 }
 
 // Module entry
