63065a499fd1f1791d9d6fd992ad9abfe8018ba7
ypsitau <ypsitau@nifty.com>
2015-07-03 15:56:18 +0900

    prepared environment for module llvm

--- src/module-llvm/module-llvm.h
+++ src/module-llvm/module-llvm.h
@@ -3,22 +3,6 @@
 //-----------------------------------------------------------------------------
 #ifndef __GURA_MODULE_LLVM_H__
 #define __GURA_MODULE_LLVM_H__
-#include <llvm/LLVMContext.h>
-#include <llvm/Module.h>
-#include <llvm/Constants.h>
-#include <llvm/DerivedTypes.h>
-#include <llvm/Instructions.h>
-#include <llvm/ExecutionEngine/JIT.h>
-#include <llvm/ExecutionEngine/Interpreter.h>
-#include <llvm/ExecutionEngine/GenericValue.h>
-#include <llvm/Target/TargetSelect.h>
-#include <llvm/Support/ManagedStatic.h>
-#include <llvm/Support/raw_ostream.h>
-
-#include <llvm/Analysis/Verifier.h>
-#include <llvm/Support/IRBuilder.h>
-
-#undef GURA_ON_MSWIN
 #include <gura.h>
 
 Gura_BeginModuleHeader(llvm)
