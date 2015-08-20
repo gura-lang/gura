c0beaf4b508fc43a1377eae6bd94e96e6dfa3161
ypsitau <ypsitau@nifty.com>
2015-08-20 10:25:24 +0900

    implementing Monitor

--- include/gura/Monitor.h
+++ include/gura/Monitor.h
@@ -11,14 +11,32 @@ namespace Gura {
 // Monitor
 //-----------------------------------------------------------------------------
 class GURA_DLLDECLARE Monitor {
+public:
+	enum Command {
+		CMD_Run, CMD_Terminate,
+	};
 private:
 	int _cntRef;
 public:
 	Gura_DeclareReferenceAccessor(Monitor);
 public:
 	Monitor();
+	virtual ~Monitor();
 private:
 	inline Monitor(const Monitor &monitor) {}
+public:
+	virtual Command OnExprPre(Environment &env, const Expr *pExprNext);
+	virtual Command OnExprPost(Environment &env, const Expr *pExpr, Value &valueRtn);
+public:
+	inline static bool NotifyExprPre(Environment &env, const Expr *pExprNext) {
+		Monitor *pMonitor = env.GetMonitor();
+		return (pMonitor == nullptr) || (pMonitor->OnExprPre(env, pExprNext) != CMD_Terminate);
+	}
+	inline static bool NotifyExprPost(Environment &env, const Expr *pExpr, Value &valueRtn) {
+		Monitor *pMonitor = env.GetMonitor();
+		return (pMonitor == nullptr) ||
+			(pMonitor->OnExprPost(env, pExpr, valueRtn) != CMD_Terminate);
+	}
 };
 
 }
--- src/lib/Monitor.cpp
+++ src/lib/Monitor.cpp
@@ -8,8 +8,22 @@ namespace Gura {
 //-----------------------------------------------------------------------------
 // Monitor
 //-----------------------------------------------------------------------------
-Monitor::Monitor()
+Monitor::Monitor() : _cntRef(1)
 {
+}
+
+Monitor::~Monitor()
+{
+}
+
+Monitor::Command Monitor::OnExprPre(Environment &env, const Expr *pExprNext)
+{
+	return CMD_Run;
+}
+
+Monitor::Command Monitor::OnExprPost(Environment &env, const Expr *pExpr, Value &valueRtn)
+{
+	return CMD_Run;
 }
 
 }
