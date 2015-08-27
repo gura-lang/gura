4a7f438adf5b8ad71408868ac857a024b1d950fd
ypsitau <ypsitau@nifty.com>
2015-08-27 19:27:56 +0900

    updated batch file

--- build-mswin.bat
+++ build-mswin.bat
@@ -1,2 +1,2 @@
 @echo off
-msbuild gura.sln /clp:DisableConsoleColor /t:Build /p:Configuration="Release" /p:Platform=win32
+msbuild gura.sln /clp:DisableConsoleColor /t:Rebuild /p:Configuration="Release" /p:Platform=win32
