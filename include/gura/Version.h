eac5dc7184fe0421dbd289f748b014f312196b8b
ypsitau <ypsitau@nifty.com>
2015-12-03 11:32:09 +0900

    advanced version number to 0.7.0

--- include/gura/Version.h
+++ include/gura/Version.h
@@ -8,10 +8,10 @@
 #include "Common.h"
 
 #define GURA_VERSION_MAJOR	0
-#define GURA_VERSION_MINOR	6
-#define GURA_VERSION_PATCH	3
+#define GURA_VERSION_MINOR	7
+#define GURA_VERSION_PATCH	0
 
-#define GURA_VERSION	"0.6.3"
+#define GURA_VERSION	"0.7.0"
 
 #define GURA_COPYRIGHT	"Copyright (C) 2011-2015 ypsitau"
 
