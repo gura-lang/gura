91c92267cde57d9a7167abd153571e790a0b7317
ypsitau <ypsitau@nifty.com>
2016-01-31 16:15:13 +0900

    implementing stl module

--- src/module-stl/module-stl.cpp
+++ src/module-stl/module-stl.cpp
@@ -8,20 +8,76 @@ Gura_BeginModuleBody(stl)
 //-----------------------------------------------------------------------------
 // Module functions
 //-----------------------------------------------------------------------------
-// stl.test(num1:number, num2:number)
+// stl.test(stream:stream)
 Gura_DeclareFunction(test)
 {
 	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
-	DeclareArg(env, "num1", VTYPE_number);
-	DeclareArg(env, "num2", VTYPE_number);
+	DeclareArg(env, "stream", VTYPE_stream);
 	AddHelp(
 		Gura_Symbol(en), Help::FMT_markdown,
-		"This function adds two numbers and returns the result.\n");
+		"");
 }
 
 Gura_ImplementFunction(test)
 {
-	return Value(arg.GetNumber(0) + arg.GetNumber(1));
+	enum {
+		STAT_FileTop, STAT_LineTop, STAT_Token, STAT_SkipWhite,
+	} stat = STAT_FileTop;
+	String token;
+	Signal &sig = env.GetSignal();
+	Stream &stream = arg.GetStream(0);
+	for (;;) {
+		int chRaw = stream.GetChar(sig);
+		if (sig.IsSignalled()) break;
+		char ch = (chRaw < 0)? '\0' : static_cast<char>(static_cast<UChar>(chRaw));
+		Gura_BeginPushbackRegion();
+		switch (stat) {
+		case STAT_FileTop: {
+			if (ch == 's') {
+				Gura_Pushback();
+				stat = STAT_Token;
+			}
+			break;
+		}
+		case STAT_LineTop: {
+			if (ch == ' ' || ch == '\t') {
+				// nothing to do
+			} else if (ch == '\n' || ch == '\0') {
+				// nothing to do
+			} else {
+				Gura_Pushback();
+			}
+		}
+		case STAT_Token: {
+			if (ch == ' ' || ch == '\t') {
+				::printf("%s\n", token.c_str());
+				token.clear();
+				stat = STAT_SkipWhite;
+			} else if (ch == '\n' || ch == '\0') {
+				::printf("%s\n", token.c_str());
+				token.clear();
+				stat = STAT_LineTop;
+			} else {
+				token += ch;
+			}
+			break;
+		}
+		case STAT_SkipWhite: {
+			if (ch == ' ' || ch == '\t') {
+				// nothing to do
+			} else if (ch == '\n' || ch == '\0') {
+				stat = STAT_LineTop;
+			} else {
+				Gura_Pushback();
+				stat = STAT_Token;
+			}
+			break;
+		}
+		}
+		Gura_EndPushbackRegion();
+		if (ch == '\0') break;
+	}
+	return Value::Nil;
 }
 
 //-----------------------------------------------------------------------------
