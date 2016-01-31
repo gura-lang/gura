b085eea9a4781fc3f32a6a15d186b6800c4c04a8
ypsitau <ypsitau@nifty.com>
2016-01-31 15:50:04 +0900

    added project of stl module

---
+++ src/module-stl/module-stl.h
@@ -0,0 +1,11 @@
+//=============================================================================
+// Gura module: stl
+//=============================================================================
+#ifndef __GURA_MODULE_STL_H__
+#define __GURA_MODULE_STL_H__
+#include <gura.h>
+
+Gura_BeginModuleHeader(stl)
+Gura_EndModuleHeader(stl)
+
+#endif
