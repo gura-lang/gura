dcac65ca2a5e988303b317be92aa0aacf6ccdc8d
ypsitau <ypsitau@nifty.com>
2016-02-01 12:27:50 +0900

    implementing model.stl module

--- src/module-model_stl/module-model_stl.cpp
+++ src/module-model_stl/module-model_stl.cpp
@@ -4,27 +4,107 @@
 #include "stdafx.h"
 
 Gura_BeginModuleBody(model_stl)
+//-----------------------------------------------------------------------------
+// Facet
+//-----------------------------------------------------------------------------
+Facet::Facet(const Facet &facet) : _normal(facet._normal)
+{
+	for (size_t i = 0; i < ArraySizeOf(_vertexes); i++) {
+		_vertexes[i] = facet._vertexes[i];
+	}
+}
+
+String Facet::ToString() const
+{
+	String str;
+	str += _normal.ToString();
+	for (size_t i = 0; i < ArraySizeOf(_vertexes); i++) {
+		str += ";";
+		str += _vertexes[i].ToString();
+	}
+	return str;
+}
+
+//-----------------------------------------------------------------------------
+// Object_facet
+//-----------------------------------------------------------------------------
+Object_facet::Object_facet() : Object(Gura_UserClass(facet))
+{
+}
+
+Object_facet::Object_facet(const Object_facet &obj) :
+	Object(Gura_UserClass(facet)), _facet(obj._facet)
+{
+}
+
+Object_facet::~Object_facet()
+{
+}
+
+Object *Object_facet::Clone() const
+{
+	return new Object_facet(*this);
+}
+
+String Object_facet::ToString(bool exprFlag)
+{
+	return String("<model.stl.facet>");
+}
+
+//-----------------------------------------------------------------------------
+// Gura interfaces for facet
+//-----------------------------------------------------------------------------
+
+// implementation of class facet
+Gura_ImplementUserClass(facet)
+{
+}
+
+//-----------------------------------------------------------------------------
+// Iterator_facet
+//-----------------------------------------------------------------------------
+Iterator_facet::Iterator_facet(Stream *pStream) : Iterator(false), _pStream(pStream)
+{
+}
+
+Iterator_facet::~Iterator_facet()
+{
+}
+
+Iterator *Iterator_facet::GetSource()
+{
+	return nullptr;
+}
+
+bool Iterator_facet::DoNext(Environment &env, Value &value)
+{
+	return false;
+}
+
+String Iterator_facet::ToString() const
+{
+	String rtn;
+	rtn += "<iterator:model.stl.facet";
+	rtn += ">";
+	return rtn;
+}
+
+void Iterator_facet::GatherFollower(Environment::Frame *pFrame, EnvironmentSet &envSet)
+{
+}
 
 //-----------------------------------------------------------------------------
 // Tokenizer
 //-----------------------------------------------------------------------------
-class Tokenizer {
-public:
-	enum Stat {
-		STAT_FileTop, STAT_LineTop, STAT_Token, STAT_SkipWhite,
-	};
-private:
-	Stat _stat;
-	String _token;
-public:
-	inline Tokenizer() : _stat(STAT_FileTop) {}
-	const String &Tokenize(Environment &env, Stream &stream);
-};
-
-const String &Tokenizer::Tokenize(Environment &env, Stream &stream)
+TokenId Tokenizer::Tokenize(Environment &env, Stream &stream)
 {
 	Signal &sig = env.GetSignal();
-	_token.clear();
+	_field.clear();
+	if (_tokenIdPending != TOKEN_None) {
+		TokenId tokenId = _tokenIdPending;
+		_tokenIdPending = TOKEN_None;
+		return tokenId;
+	}
 	for (;;) {
 		int chRaw = stream.GetChar(sig);
 		if (sig.IsSignalled()) break;
@@ -34,8 +114,12 @@ const String &Tokenizer::Tokenize(Environment &env, Stream &stream)
 		case STAT_FileTop: {
 			if (ch == 's') {
 				Gura_Pushback();
-				_stat = STAT_Token;
+				_stat = STAT_Field;
 			}
+			break;
+		}
+		case STAT_FileEnd: {
+			// nothing to do
 			break;
 		}
 		case STAT_LineTop: {
@@ -45,30 +129,37 @@ const String &Tokenizer::Tokenize(Environment &env, Stream &stream)
 				// nothing to do
 			} else {
 				Gura_Pushback();
-				_stat = STAT_Token;
+				_stat = STAT_Field;
 			}
 			break;
 		}
-		case STAT_Token: {
+		case STAT_Field: {
 			if (ch == ' ' || ch == '\t') {
 				_stat = STAT_SkipWhite;
-				return _token;
-			} else if (ch == '\n' || ch == '\0') {
+				return TOKEN_Field;
+			} else if (ch == '\n') {
+				_tokenIdPending = TOKEN_EOL;
 				_stat = STAT_LineTop;
-				return _token;
+				return TOKEN_Field;
+			} else if (ch == '\0') {
+				_tokenIdPending = TOKEN_EOF;
+				_stat = STAT_FileEnd;
+				return TOKEN_Field;
 			} else {
-				_token += ch;
+				_field += ch;
 			}
 			break;
 		}
 		case STAT_SkipWhite: {
 			if (ch == ' ' || ch == '\t') {
 				// nothing to do
-			} else if (ch == '\n' || ch == '\0') {
+			} else if (ch == '\n') {
+				_stat = STAT_LineTop;
+			} else if (ch == '\0') {
 				_stat = STAT_LineTop;
 			} else {
 				Gura_Pushback();
-				_stat = STAT_Token;
+				_stat = STAT_Field;
 			}
 			break;
 		}
@@ -76,7 +167,7 @@ const String &Tokenizer::Tokenize(Environment &env, Stream &stream)
 		Gura_EndPushbackRegion();
 		if (ch == '\0') break;
 	}
-	return _token;
+	return TOKEN_EOF;
 }
 
 //-----------------------------------------------------------------------------
@@ -94,12 +185,184 @@ Gura_DeclareFunction(test)
 
 Gura_ImplementFunction(test)
 {
-	Tokenizer tokenizer;
+	enum {
+		STAT_solid, STAT_solid_name, STAT_solid_EOL,
+		STAT_facet, STAT_normal, STAT_normal_coords,
+		STAT_outer, STAT_loop,
+		STAT_vertex, STAT_vertex_coords,
+		STAT_endloop,
+		STAT_endfacet,
+	} _stat = STAT_solid;
+	size_t nCoords = 0;
+	size_t nVertexes = 0;
+	Vertex vertex;
+	Tokenizer _tokenizer;
 	Stream &stream = arg.GetStream(0);
+	AutoPtr<Object_facet> pObjFacet;
 	for (;;) {
-		const String &token = tokenizer.Tokenize(env, stream);
-		if (token.empty()) break;
-		::printf("%s\n", token.c_str());
+		TokenId tokenId = _tokenizer.Tokenize(env, stream);
+		switch (_stat) {
+		case STAT_solid: {
+			if (tokenId == TOKEN_Field) {
+				const String &field = _tokenizer.GetField();
+				if (field == "solid") {
+					_stat = STAT_solid_name;
+				} else {
+					// error
+				}
+			}
+			break;
+		}
+		case STAT_solid_name: {
+			if (tokenId == TOKEN_EOL) {
+				_stat = STAT_facet;
+			} else if (tokenId == TOKEN_Field) {
+				const String &field = _tokenizer.GetField();
+				_stat = STAT_solid_EOL;
+			}
+			break;
+		}
+		case STAT_solid_EOL: {
+			if (tokenId == TOKEN_EOL) {
+				_stat = STAT_facet;
+			}
+			break;
+		}
+		case STAT_facet: {
+			if (tokenId == TOKEN_Field) {
+				const String &field = _tokenizer.GetField();
+				if (field == "facet") {
+					pObjFacet.reset(new Object_facet());
+					_stat = STAT_normal;
+				} else {
+					// error
+				}
+			}
+			break;
+		}
+		case STAT_normal: {
+			if (tokenId == TOKEN_Field) {
+				const String &field = _tokenizer.GetField();
+				if (field == "normal") {
+					nCoords = 0;
+					_stat = STAT_normal_coords;
+				} else {
+					// error
+				}
+			}
+			break;
+		}
+		case STAT_normal_coords: {
+			if (tokenId == TOKEN_Field) {
+				const String &field = _tokenizer.GetField();
+				double num = 0;
+				char *p = nullptr;
+				num = ::strtod(field.c_str(), &p);
+				if (*p != '\0') {
+					// error
+				}
+				if (nCoords == 0) {
+					vertex.x = num;
+				} else if (nCoords == 1) {
+					vertex.y = num;
+				} else if (nCoords == 2) {
+					Facet &facet = pObjFacet->GetFacet();
+					vertex.z = num;
+					facet.SetNormal(vertex);
+					_stat = STAT_outer;
+				}
+				nCoords++;
+			}
+			break;
+		}
+		case STAT_outer: {
+			if (tokenId == TOKEN_Field) {
+				const String &field = _tokenizer.GetField();
+				if (field == "outer") {
+					_stat = STAT_loop;
+				} else {
+					// error
+				}
+			}
+			break;
+		}
+		case STAT_loop: {
+			if (tokenId == TOKEN_Field) {
+				const String &field = _tokenizer.GetField();
+				if (field == "loop") {
+					nVertexes = 0;
+					_stat = STAT_vertex;
+				} else {
+					// error
+				}
+			}
+			break;
+		}
+		case STAT_vertex: {
+			if (tokenId == TOKEN_Field) {
+				const String &field = _tokenizer.GetField();
+				if (field == "vertex") {
+					nCoords = 0;
+					_stat = STAT_vertex_coords;
+				} else {
+					// error
+				}
+			}
+			break;
+		}
+		case STAT_vertex_coords: {
+			if (tokenId == TOKEN_Field) {
+				const String &field = _tokenizer.GetField();
+				double num = 0;
+				char *p = nullptr;
+				num = ::strtod(field.c_str(), &p);
+				if (*p != '\0') {
+					// error
+				}
+				if (nCoords == 0) {
+					vertex.x = num;
+				} else if (nCoords == 1) {
+					vertex.y = num;
+				} else if (nCoords == 2) {
+					Facet &facet = pObjFacet->GetFacet();
+					vertex.z = num;
+					nCoords = 0;
+					facet.SetVertex(nVertexes, vertex);
+					if (nVertexes == 2) {
+						::printf("%s\n", facet.ToString().c_str());
+						// completed facet
+						_stat = STAT_endloop;
+					}
+					nVertexes++;
+				}
+				nCoords++;
+			}
+			break;
+		}
+		case STAT_endloop: {
+			if (tokenId == TOKEN_Field) {
+				const String &field = _tokenizer.GetField();
+				if (field == "endloop") {
+					_stat = STAT_endfacet;
+				} else {
+					// error
+				}
+			}
+			break;
+		}
+		case STAT_endfacet: {
+			if (tokenId == TOKEN_Field) {
+				const String &field = _tokenizer.GetField();
+				if (field == "endfacet") {
+					_stat = STAT_facet;
+				} else {
+					// error
+				}
+			}
+			break;
+		}
+		}
+		if (tokenId == TOKEN_EOF) break;
 	}
 	return Value::Nil;
 }
@@ -116,6 +379,11 @@ Gura_ModuleEntry()
 {
 	// function assignment
 	Gura_AssignFunction(test);
+	// class realization
+	Gura_RealizeUserClass(facet, env.LookupClass(VTYPE_object));
+	Gura_PrepareUserClass(facet);
+	// class reference assignment
+	Gura_AssignValue(facet, Value(Gura_UserClass(facet)->Reference()));
 	return true;
 }
 
--- src/module-model_stl/module-model_stl.h
+++ src/module-model_stl/module-model_stl.h
@@ -6,6 +6,90 @@
 #include <gura.h>
 
 Gura_BeginModuleHeader(model_stl)
+//-----------------------------------------------------------------------------
+// TokenId
+//-----------------------------------------------------------------------------
+enum TokenId {
+	TOKEN_None, TOKEN_EOL, TOKEN_EOF, TOKEN_Field,
+};
+
+//-----------------------------------------------------------------------------
+// Facet
+//-----------------------------------------------------------------------------
+class Facet {
+private:
+	Vertex _normal;
+	Vertex _vertexes[3];
+public:
+	inline Facet() {}
+	Facet(const Facet &facet);
+	inline void SetNormal(const Vertex &normal) { _normal = normal; }
+	inline void SetVertex(size_t idx, const Vertex &vertex) { _vertexes[idx] = vertex; }
+	inline const Vertex &GetNormal() const { return _normal; }
+	inline const Vertex &GetVertex(size_t idx) const { return _vertexes[idx]; }
+	String ToString() const;
+};
+
+//-----------------------------------------------------------------------------
+// Object_facet
+//-----------------------------------------------------------------------------
+Gura_DeclareUserClass(facet);
+
+class Object_facet : public Object {
+private:
+	Facet _facet;
+public:
+	Gura_DeclareObjectAccessor(facet)
+public:
+	Object_facet();
+	Object_facet(const Object_facet &obj);
+	virtual ~Object_facet();
+	virtual Object *Clone() const;
+	virtual String ToString(bool exprFlag);
+	inline Facet &GetFacet() { return _facet; }
+};
+
+//-----------------------------------------------------------------------------
+// Iterator_facet
+//-----------------------------------------------------------------------------
+class Iterator_facet : public Iterator {
+private:
+	AutoPtr<Stream> _pStream;
+public:
+	Iterator_facet(Stream *pStream);
+	virtual ~Iterator_facet();
+	virtual Iterator *GetSource();
+	virtual bool DoNext(Environment &env, Value &value);
+	virtual String ToString() const;
+	virtual void GatherFollower(Environment::Frame *pFrame, EnvironmentSet &envSet);
+};
+
+//-----------------------------------------------------------------------------
+// Solid
+//-----------------------------------------------------------------------------
+class Solid {
+private:
+	
+};
+
+//-----------------------------------------------------------------------------
+// Tokenizer
+//-----------------------------------------------------------------------------
+class Tokenizer {
+public:
+	enum Stat {
+		STAT_FileTop, STAT_FileEnd, STAT_LineTop, STAT_Field, STAT_SkipWhite,
+	};
+private:
+	Stat _stat;
+	String _field;
+	TokenId _tokenIdPending;
+public:
+	inline Tokenizer() : _stat(STAT_FileTop), _tokenIdPending(TOKEN_None) {}
+	TokenId Tokenize(Environment &env, Stream &stream);
+	inline const String &GetField() const { return _field; }
+};
+
 Gura_EndModuleHeader(model_stl)
 
 #endif
