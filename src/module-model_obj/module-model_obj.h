dd14c3da80c71fe9d92e4f86d14c7b9a47cbaa3c
ypsitau <ypsitau@nifty.com>
2016-02-04 19:43:53 +0900

    added project form model.obj module

---
+++ src/module-model_obj/module-model_obj.h
@@ -0,0 +1,11 @@
+//=============================================================================
+// Gura module: model.obj
+//=============================================================================
+#ifndef __GURA_MODULE_MODEL_OBJ_H__
+#define __GURA_MODULE_MODEL_OBJ_H__
+#include <gura.h>
+
+Gura_BeginModuleHeader(model_obj)
+Gura_EndModuleHeader(model_obj)
+
+#endif
---
+++ src/module-model_obj/stdafx.h
@@ -0,0 +1,4 @@
+#ifndef __GURA_MODULE_MODEL_OBJ_STDAFX_H__
+#define __GURA_MODULE_MODEL_OBJ_STDAFX_H__
+#include "module-model_obj.h"
+#endif
