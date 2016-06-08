841ecc8300b64f04cdd0bed7b116ca283cd8a679
ypsitau <ypsitau@nifty.com>
2016-06-08 20:56:14 +0900

    added skeleton of doxygen module

---
+++ src/module-doxygen/module-doxygen.h
@@ -0,0 +1,11 @@
+//=============================================================================
+// Gura module: doxygen
+//=============================================================================
+#ifndef __GURA_MODULE_DOXYGEN_H__
+#define __GURA_MODULE_DOXYGEN_H__
+#include <gura.h>
+
+Gura_BeginModuleHeader(doxygen)
+Gura_EndModuleHeader(doxygen)
+
+#endif
---
+++ src/module-doxygen/stdafx.h
@@ -0,0 +1,4 @@
+#ifndef __GURA_MODULE_DOXYGEN_STDAFX_H__
+#define __GURA_MODULE_DOXYGEN_STDAFX_H__
+#include "module-doxygen.h"
+#endif
