8f60fc0d681cc58848d5c3678158d85897218a5a
ypsitau <ypsitau@nifty.com>
2016-06-10 11:05:53 +0900

    implementing doxygen module

--- src/module-doxygen/module-doxygen.cpp
+++ src/module-doxygen/module-doxygen.cpp
@@ -5,114 +5,236 @@
 
 Gura_BeginModuleBody(doxygen)
 
-enum Stat {
-	STAT_LineTop,
-	STAT_Line,
-	STAT_LineCommentBgn,
-	STAT_LineCommentDoxy,
-	STAT_LineComment,
-	STAT_BlockCommentBgn,
-	STAT_BlockCommentDoxy,
-	STAT_BlockCommentDoxyEnd,
-	STAT_BlockComment,
-	STAT_BlockCommentEnd,
-	STAT_Slash,
+//-----------------------------------------------------------------------------
+// Parser
+//-----------------------------------------------------------------------------
+class Parser {
+public:
+	enum Stat {
+		STAT_Init,
+	};
+private:
+	Stat _stat;
+public:
+	inline Parser() : _stat(STAT_Init) {}
+	bool FeedChar(char ch);
 };
+
+bool Parser::FeedChar(char ch)
+{
+	::printf("%c", ch);
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+// Extractor
+//-----------------------------------------------------------------------------
+class Extractor {
+public:
+	enum Stat {
+		STAT_Indent,
+		STAT_Source,
+		STAT_LineCommentBgn,
+		STAT_LineDoxygen,
+		STAT_LineComment,
+		STAT_BlockCommentBgn,
+		STAT_BlockCommentBgn_Asterisk,
+		STAT_BlockDoxygen,
+		STAT_BlockDoxygen_Asterisk,
+		STAT_BlockDoxygen_Indent,
+		STAT_BlockDoxygen_IndentAsterisk,
+		STAT_BlockDoxygen_IndentAfterAsterisk,
+		STAT_BlockComment,
+		STAT_BlockComment_Asterisk,
+		STAT_Slash,
+	};
+private:
+	Stat _stat;
+	Parser _parser;
+public:
+	inline Extractor() : _stat(STAT_Indent) {}
+	bool FeedChar(char ch);
+};
+
+bool Extractor::FeedChar(char ch)
+{
+	Gura_BeginPushbackRegionEx(char, 16, ch);
+	switch (_stat) {
+	case STAT_Indent: {
+		if (ch == '\0') {
+			// nothing to do
+		} else if (ch == '\n') {
+			// nothing to do
+		} else if (ch == ' ' || ch == '\t') {
+			// nothing to do
+		} else {
+			_stat = STAT_Source;
+			Gura_PushbackEx(ch);
+		}
+		break;
+	}
+	case STAT_Source: {
+		if (ch == '/') {
+			_stat = STAT_Slash;
+		} else if (ch == '\n') {
+			_stat = STAT_Indent;
+		} else { // including '\0'
+			// nothing to do
+		}
+		break;
+	}
+	case STAT_Slash: {
+		if (ch == '\0') {
+			// nothing to do
+		} else if (ch == '/') {
+			_stat = STAT_LineCommentBgn;
+		} else if (ch == '*') {
+			_stat = STAT_BlockCommentBgn;
+		} else {
+			_stat = STAT_Source;
+			Gura_PushbackEx(ch);
+		}
+		break;
+	}
+	case STAT_LineCommentBgn: {
+		if (ch == '\0') {
+			// nothing to do
+		} else if (ch == '/' || ch == '!') {
+			_stat = STAT_LineDoxygen;
+		} else {
+			_stat = STAT_LineComment;
+		}
+		break;
+	}
+	case STAT_LineDoxygen: {
+		if (ch == '\n') {
+			// a line comment ends with newline.
+			_stat = STAT_Indent;
+		} else { // including '\0'
+			if (!_parser.FeedChar(ch)) return false;
+		}
+		break;
+	}
+	case STAT_LineComment: {
+		if (ch == '\n') {
+			// a line comment ends with newline.
+			_stat = STAT_Indent;
+		} else { // including '\0'
+			// nothing to do
+		}
+		break;
+	}
+	case STAT_BlockCommentBgn: {
+		if (ch == '\0') {
+			// nothing to do
+		} else if (ch == '*') {
+			_stat = STAT_BlockCommentBgn_Asterisk;
+		} else if (ch == '!') {
+			_stat = STAT_BlockDoxygen;
+		} else {
+			_stat = STAT_BlockComment;
+		}
+		break;
+	}
+	case STAT_BlockCommentBgn_Asterisk: {
+		if (ch == '/') {
+			_stat = STAT_Source;
+		} else {
+			Gura_PushbackEx(ch);
+			_stat = STAT_BlockDoxygen;
+		}
+		break;
+	}
+	case STAT_BlockDoxygen: {
+		if (ch == '\0') {
+			// nothing to do
+		} else if (ch == '*') {
+			_stat = STAT_BlockDoxygen_Asterisk;
+		} else if (ch == '\n') {
+			_stat = STAT_BlockDoxygen_Indent;
+		} else {
+			if (!_parser.FeedChar(ch)) return false;
+		}
+		break;
+	}
+	case STAT_BlockDoxygen_Asterisk: {
+		if (ch == '/') {
+			_stat = STAT_Source;
+		} else { // including '\0'
+			if (!_parser.FeedChar('*')) return false;
+			if (!_parser.FeedChar(ch)) return false;
+			_stat = STAT_BlockDoxygen;
+		}
+		break;
+	}
+	case STAT_BlockDoxygen_Indent: {
+		if (ch == '\0') {
+			// nothing to do
+		} else if (ch == '\n') {
+			// nothing to do
+		} else if (ch == ' ' || ch == '\t') {
+			// nothing to do
+		} else if (ch == '*') {
+			_stat = STAT_BlockDoxygen_IndentAsterisk;
+		} else {
+			Gura_PushbackEx(ch);
+			_stat = STAT_BlockDoxygen;
+		}
+		break;
+	}
+	case STAT_BlockDoxygen_IndentAsterisk: {
+		if (ch == '/') {
+			_stat = STAT_Source;
+		} else {
+			Gura_PushbackEx(ch);
+			_stat = STAT_BlockDoxygen_IndentAfterAsterisk;
+		}
+		break;
+	}
+	case STAT_BlockDoxygen_IndentAfterAsterisk: {
+		if (ch == '\0') {
+			// nothing to do
+		} else if (ch == '\n') {
+			// nothing to do
+		} else if (ch == ' ' || ch == '\t') {
+			// nothing to do
+		} else {
+			Gura_PushbackEx(ch);
+			_stat = STAT_BlockDoxygen;
+		}
+		break;
+	}
+	case STAT_BlockComment: {
+		if (ch == '*') {
+			_stat = STAT_BlockComment_Asterisk;
+		} else { // including '\0'
+			// nothing to do
+		}
+		break;
+	}
+	case STAT_BlockComment_Asterisk: {
+		if (ch == '\0') {
+			// nothing to do
+		} else if (ch == '/') {
+			_stat = STAT_Source;
+		} else {
+			_stat = STAT_BlockComment;
+		}
+		break;
+	}
+	}
+	Gura_EndPushbackRegionEx();
+	return true;
+}
 
 bool ParseStream(Signal &sig, SimpleStream &stream)
 {
-	Stat stat = STAT_LineTop;
+	Extractor extractor;
 	for (;;) {
 		int chRaw;
 		if ((chRaw = stream.GetChar(sig)) < 0) chRaw = 0;
 		char ch = static_cast<char>(static_cast<UChar>(chRaw));
-		::printf("%c", ch);
-		Gura_BeginPushbackRegionEx(char, 16, ch);
-		switch (stat) {
-		case STAT_LineTop: {
-			if (ch == '/') {
-				stat = STAT_Slash;
-			} else {
-				stat = STAT_Line;
-				Gura_PushbackEx(ch);
-			}
-			break;
-		}
-		case STAT_Line: {
-			
-			break;
-		}
-		case STAT_Slash: {
-			if (ch == '/') {
-				stat = STAT_LineCommentBgn;
-			} else if (ch == '*') {
-				stat = STAT_BlockCommentBgn;
-			} else {
-				stat = STAT_Line;
-				Gura_PushbackEx(ch);
-			}
-			break;
-		}
-		case STAT_LineCommentBgn: {
-			if (ch == '/' || ch == '!') {
-				stat = STAT_LineCommentDoxy;
-			} else {
-				stat = STAT_LineComment;
-			}
-		}
-		case STAT_LineCommentDoxy: {
-			if (ch == '\n') {
-				stat = STAT_LineTop;
-			}
-			break;
-		}
-		case STAT_LineComment: {
-			if (ch == '\n') {
-				stat = STAT_LineTop;
-			}
-			break;
-		}
-		case STAT_BlockCommentBgn: {
-			if (ch == '*' || ch == '!') {
-				stat = STAT_BlockCommentDoxy;
-			} else {
-				stat = STAT_BlockComment;
-			}
-		}
-		case STAT_BlockCommentDoxy: {
-			if (ch == '*') {
-				stat = STAT_BlockCommentDoxyEnd;
-			}
-			break;
-		}
-		case STAT_BlockComment: {
-			if (ch == '*') {
-				stat = STAT_BlockCommentEnd;
-			}
-			break;
-		}
-		case STAT_BlockCommentDoxyEnd: {
-			if (ch == '/') {
-				stat = STAT_Line;
-			} else {
-				Gura_PushbackEx(ch);
-				Gura_PushbackEx('*');
-				stat = STAT_BlockCommentDoxy;
-			}
-			break;
-		}
-		case STAT_BlockCommentEnd: {
-			if (ch == '/') {
-				stat = STAT_Line;
-			} else {
-				Gura_PushbackEx(ch);
-				Gura_PushbackEx('*');
-				stat = STAT_BlockComment;
-			}
-			break;
-		}
-		}
-		Gura_EndPushbackRegionEx();
+		extractor.FeedChar(ch);
 		if (ch == '\0') break;
 	}
 	return true;
@@ -121,10 +243,10 @@ bool ParseStream(Signal &sig, SimpleStream &stream)
 //-----------------------------------------------------------------------------
 // Module functions
 //-----------------------------------------------------------------------------
-// doxygen.test(stream:stream)
+// doxygen.test(stream:stream):void:map
 Gura_DeclareFunction(test)
 {
-	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
+	SetFuncAttr(VTYPE_any, RSLTMODE_Void, FLAG_Map);
 	DeclareArg(env, "stream", VTYPE_stream);
 	AddHelp(
 		Gura_Symbol(en), Help::FMT_markdown,
