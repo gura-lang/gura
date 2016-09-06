3eb8d8545660122afdbaadbf0d07a1394fb72cc2
ypsitau <ypsitau@nifty.com>
2016-09-06 19:45:05 +0900

    implementing lexer module

--- src/module-lexer/module-lexer.cpp
+++ src/module-lexer/module-lexer.cpp
@@ -8,6 +8,20 @@ Gura_BeginModuleBody(lexer)
 //-----------------------------------------------------------------------------
 // Gura module functions
 //-----------------------------------------------------------------------------
+// lexer.tokens(stream:stream):map
+Gura_DeclareFunction(tokens)
+{
+	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_Map);
+	DeclareArg(env, "stream", VTYPE_stream);
+	AddHelp(
+		Gura_Symbol(en), Help::FMT_markdown,
+		"");
+}
+
+Gura_ImplementFunction(tokens)
+{
+	return Value::Nil;
+}
 
 //-----------------------------------------------------------------------------
 // Module Entries
@@ -19,6 +33,7 @@ Gura_ModuleValidate()
 
 Gura_ModuleEntry()
 {
+	Gura_AssignFunction(tokens);
 	return true;
 }
 
