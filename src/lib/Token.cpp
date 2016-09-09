b877b8e0061a004a9023e9e4509dc45f1cbe8f9e
ypsitau <ypsitau@nifty.com>
2016-09-09 11:16:11 +0900

    refactored Token

--- src/lib/Token.cpp
+++ src/lib/Token.cpp
@@ -10,152 +10,79 @@ namespace Gura {
 //-----------------------------------------------------------------------------
 OpTypeToTokenInfoMap *Token::_pOpTypeToTokenInfoMap = nullptr;
 
-const TokenInfo TOKEN_Begin =
-{				 1, "Begin",			"[Bgn]",	OPTYPE_None		};	// B
-const TokenInfo TOKEN_Assign =
-{				 2, "Assign",			"=",		OPTYPE_None		};	// =
-const TokenInfo TOKEN_AssignAdd =
-{			 2, "AssignAdd",		"+=",		OPTYPE_None		};
-const TokenInfo TOKEN_AssignSub =
-{			 2, "AssignSub",		"-=",		OPTYPE_None		};
-const TokenInfo TOKEN_AssignMul =
-{			 2, "AssignMul",		"*=",		OPTYPE_None		};
-const TokenInfo TOKEN_AssignDiv =
-{			 2, "AssignDiv",		"/=",		OPTYPE_None		};
-const TokenInfo TOKEN_AssignMod =
-{			 2, "AssignMod",		"%=",		OPTYPE_None		};
-const TokenInfo TOKEN_AssignPow =
-{			 2, "AssignPow",		"**=",		OPTYPE_None		};
-const TokenInfo TOKEN_AssignOr =
-{				 2, "AssignOr",			"|=",		OPTYPE_None		};
-const TokenInfo TOKEN_AssignAnd =
-{			 2, "AssignAnd",		"&=",		OPTYPE_None		};
-const TokenInfo TOKEN_AssignXor =
-{			 2, "AssignXor",		"^=",		OPTYPE_None		};
-const TokenInfo TOKEN_AssignShl =
-{			 2, "AssignShl",		"<<=",		OPTYPE_None		};
-const TokenInfo TOKEN_AssignShr =
-{			 2, "AssignShr",		">>=",		OPTYPE_None		};
-const TokenInfo TOKEN_Pair =
-{					 2, "Pair",				"=>",		OPTYPE_Pair		};
-const TokenInfo TOKEN_OrOr =
-{					 3, "OrOr",				"||",		OPTYPE_OrOr		};	// ||
-const TokenInfo TOKEN_AndAnd =
-{				 4, "AndAnd",			"&&",		OPTYPE_AndAnd	};	// &&
-const TokenInfo TOKEN_Not =
-{					 5, "Not",				"!",		OPTYPE_Not		};	// !
-const TokenInfo TOKEN_Contains =
-{				 6, "Contains",			"in",		OPTYPE_Contains	};	// in
-const TokenInfo TOKEN_Lt =
-{					 7, "Lt",				"<",		OPTYPE_Lt		};	// <
-const TokenInfo TOKEN_Gt =
-{					 7, "Gt",				">",		OPTYPE_Gt		};
-const TokenInfo TOKEN_Le =
-{		 			 7, "Le",				"<=",		OPTYPE_Le		};
-const TokenInfo TOKEN_Ge =
-{					 7, "Ge",				">=",		OPTYPE_Ge		};
-const TokenInfo TOKEN_Cmp =
-{					 7, "Cmp",				"<=>",		OPTYPE_Cmp		};
-const TokenInfo TOKEN_Eq =
-{					 7, "Eq",				"==",		OPTYPE_Eq		};
-const TokenInfo TOKEN_Ne =
-{					 7, "Ne",				"!=",		OPTYPE_Ne		};
-const TokenInfo TOKEN_Seq =
-{				 	 8, "Seq",				"..",		OPTYPE_Seq		};	// ..
-const TokenInfo TOKEN_Or =
-{					 9, "Or",				"|",		OPTYPE_Or		};	// |
-const TokenInfo TOKEN_Xor =
-{					10, "Xor",				"^",		OPTYPE_Xor		};	// ^
-const TokenInfo TOKEN_And =
-{					11, "And",				"&",		OPTYPE_And		};	// &
-const TokenInfo TOKEN_Shl =
-{					12, "Shl",				"<<",		OPTYPE_Shl		};	// <<
-const TokenInfo TOKEN_Shr =
-{					12, "Shr",				">>",		OPTYPE_Shr		};
-const TokenInfo TOKEN_Add =
-{					13, "Add",				"+",		OPTYPE_Add		};	// +
-const TokenInfo TOKEN_Sub =
-{					13, "Sub",				"-",		OPTYPE_Sub		};
-const TokenInfo TOKEN_Mul =
-{					14, "Mul",				"*",		OPTYPE_Mul		};	// *
-const TokenInfo TOKEN_Div =
-{					14, "Div",				"/",		OPTYPE_Div		};
-const TokenInfo TOKEN_Mod =
-{					14, "Mod",				"%",		OPTYPE_Mod		};
-const TokenInfo TOKEN_ModMod =
-{				14, "ModMod",			"%%",		OPTYPE_None		};
-const TokenInfo TOKEN_Question =
-{				14, "Question",			"?",		OPTYPE_None		};
-const TokenInfo TOKEN_Inv =
-{					15, "Inv",				"~",		OPTYPE_Inv		};	// ~
-const TokenInfo TOKEN_Pow =
-{					16, "Pow",				"**",		OPTYPE_Pow		};	// **
-const TokenInfo TOKEN_Quote =
-{				17, "Quote",			"`",		OPTYPE_None		};	// `
-const TokenInfo TOKEN_Colon =
-{				18, "Colon",			":",		OPTYPE_None		};	// :
-const TokenInfo TOKEN_ColonAfterSuffix =
-{		19, "ColonAfterSuffix",	":",		OPTYPE_None		};	// *:
-const TokenInfo TOKEN_Dot =
-{					20, "Dot",				".",		OPTYPE_None		};	// .
-const TokenInfo TOKEN_ColonColon =
-{			20, "ColonColon",		"::",		OPTYPE_None		};
-const TokenInfo TOKEN_ColonAsterisk =
-{		20, "ColonAsterisk",	":*",		OPTYPE_None		};
-const TokenInfo TOKEN_ColonAnd =
-{				20, "ColonAnd",			":&",		OPTYPE_None		};
-const TokenInfo TOKEN_LParenthesis =
-{			21, "LParenthesis",		"(",		OPTYPE_None		};	// (
-const TokenInfo TOKEN_RParenthesis =
-{			22, "RParenthesis",		")",		OPTYPE_None		};	// )
-const TokenInfo TOKEN_LBrace =
-{				21, "LBrace",			"{",		OPTYPE_None		};
-const TokenInfo TOKEN_RBrace =
-{				22, "RBrace",			"}",		OPTYPE_None		};
-const TokenInfo TOKEN_LBlockParam =
-{			21, "LBlockParam",		"|",		OPTYPE_None		};
-const TokenInfo TOKEN_RBlockParam =
-{			22, "RBlockParam",		"|",		OPTYPE_None		};
-const TokenInfo TOKEN_LBracket =
-{				23, "LBracket",			"[",		OPTYPE_None		};	// [
-const TokenInfo TOKEN_RBracket =
-{				24, "RBracket",			"]",		OPTYPE_None		};	// ]
-const TokenInfo TOKEN_Comma =
-{				25, "Comma",			",",		OPTYPE_None		};	// ,
-const TokenInfo TOKEN_Semicolon =
-{			25, "Semicolon",		";",		OPTYPE_None		};
-const TokenInfo TOKEN_EOL =
-{					26, "EOL",				"[EOL]",	OPTYPE_None		};	// \n
-const TokenInfo TOKEN_Number =
-{				27, "Number",			"[Num]",	OPTYPE_None		};	// V
-const TokenInfo TOKEN_NumberSuffixed =
-{		27, "NumberSuffixed",	"[NmS]",	OPTYPE_None		};
-const TokenInfo TOKEN_String =
-{				27, "String",			"[Str]",	OPTYPE_None		};
-const TokenInfo TOKEN_StringSuffixed =
-{		27, "StringSuffixed",	"[StS]",	OPTYPE_None		};
-const TokenInfo TOKEN_Binary =
-{				27, "Binary",			"[Bin]",	OPTYPE_None		};
-const TokenInfo TOKEN_EmbedString =
-{			27, "EmbedString",		"[EmS]",	OPTYPE_None		};
-const TokenInfo TOKEN_Symbol =
-{				28, "Symbol",			"[Sym]",	OPTYPE_None		};	// S
-const TokenInfo TOKEN_EOF =
-{					29, "EOF",				"[EOF]",	OPTYPE_None		};	// E
-const TokenInfo TOKEN_Expr =
-{					 0, "Expr",				"[Exp]",	OPTYPE_None		};
-const TokenInfo TOKEN_White =
-{				 0, "White",			"[Wht]",	OPTYPE_None		};
-const TokenInfo TOKEN_CommentLine =
-{			 0, "CommentLine",		"[CmL]",	OPTYPE_None		};
-const TokenInfo TOKEN_CommentBlock =
-{			 0, "CommentBlock",		"[CmB]",	OPTYPE_None		};
-const TokenInfo TOKEN_DoubleChars =
-{			 0, "DoubleChars",		"[DbC]",	OPTYPE_None		};
-const TokenInfo TOKEN_TripleChars =
-{			 0, "TripleChars",		"[TrC]",	OPTYPE_None		};
-const TokenInfo TOKEN_Unknown =
-{				 0, "Unknown",			"[unk]",	OPTYPE_None		};
+const TokenInfo TOKEN_Begin =			{  1, "Begin",			"[Bgn]",	OPTYPE_None		};	// B
+const TokenInfo TOKEN_Assign =			{  2, "Assign",			"=",		OPTYPE_None		};	// =
+const TokenInfo TOKEN_AssignAdd =		{  2, "AssignAdd",		"+=",		OPTYPE_None		};
+const TokenInfo TOKEN_AssignSub =		{  2, "AssignSub",		"-=",		OPTYPE_None		};
+const TokenInfo TOKEN_AssignMul =		{  2, "AssignMul",		"*=",		OPTYPE_None		};
+const TokenInfo TOKEN_AssignDiv =		{  2, "AssignDiv",		"/=",		OPTYPE_None		};
+const TokenInfo TOKEN_AssignMod =		{  2, "AssignMod",		"%=",		OPTYPE_None		};
+const TokenInfo TOKEN_AssignPow =		{  2, "AssignPow",		"**=",		OPTYPE_None		};
+const TokenInfo TOKEN_AssignOr =		{  2, "AssignOr",		"|=",		OPTYPE_None		};
+const TokenInfo TOKEN_AssignAnd =		{  2, "AssignAnd",		"&=",		OPTYPE_None		};
+const TokenInfo TOKEN_AssignXor =		{  2, "AssignXor",		"^=",		OPTYPE_None		};
+const TokenInfo TOKEN_AssignShl =		{  2, "AssignShl",		"<<=",		OPTYPE_None		};
+const TokenInfo TOKEN_AssignShr =		{  2, "AssignShr",		">>=",		OPTYPE_None		};
+const TokenInfo TOKEN_Pair =			{  2, "Pair",			"=>",		OPTYPE_Pair		};
+const TokenInfo TOKEN_OrOr =			{  3, "OrOr",			"||",		OPTYPE_OrOr		};	// ||
+const TokenInfo TOKEN_AndAnd =			{  4, "AndAnd",			"&&",		OPTYPE_AndAnd	};	// &&
+const TokenInfo TOKEN_Not =				{  5, "Not",			"!",		OPTYPE_Not		};	// !
+const TokenInfo TOKEN_Contains =		{  6, "Contains",		"in",		OPTYPE_Contains	};	// in
+const TokenInfo TOKEN_Lt =				{  7, "Lt",				"<",		OPTYPE_Lt		};	// <
+const TokenInfo TOKEN_Gt =				{  7, "Gt",				">",		OPTYPE_Gt		};
+const TokenInfo TOKEN_Le =				{  7, "Le",				"<=",		OPTYPE_Le		};
+const TokenInfo TOKEN_Ge =				{  7, "Ge",				">=",		OPTYPE_Ge		};
+const TokenInfo TOKEN_Cmp =				{  7, "Cmp",			"<=>",		OPTYPE_Cmp		};
+const TokenInfo TOKEN_Eq =				{  7, "Eq",				"==",		OPTYPE_Eq		};
+const TokenInfo TOKEN_Ne =				{  7, "Ne",				"!=",		OPTYPE_Ne		};
+const TokenInfo TOKEN_Seq =				{  8, "Seq",			"..",		OPTYPE_Seq		};	// ..
+const TokenInfo TOKEN_Or =				{  9, "Or",				"|",		OPTYPE_Or		};	// |
+const TokenInfo TOKEN_Xor =				{ 10, "Xor",			"^",		OPTYPE_Xor		};	// ^
+const TokenInfo TOKEN_And =				{ 11, "And",			"&",		OPTYPE_And		};	// &
+const TokenInfo TOKEN_Shl =				{ 12, "Shl",			"<<",		OPTYPE_Shl		};	// <<
+const TokenInfo TOKEN_Shr =				{ 12, "Shr",			">>",		OPTYPE_Shr		};
+const TokenInfo TOKEN_Add =				{ 13, "Add",			"+",		OPTYPE_Add		};	// +
+const TokenInfo TOKEN_Sub =				{ 13, "Sub",			"-",		OPTYPE_Sub		};
+const TokenInfo TOKEN_Mul =				{ 14, "Mul",			"*",		OPTYPE_Mul		};	// *
+const TokenInfo TOKEN_Div =				{ 14, "Div",			"/",		OPTYPE_Div		};
+const TokenInfo TOKEN_Mod =				{ 14, "Mod",			"%",		OPTYPE_Mod		};
+const TokenInfo TOKEN_ModMod =			{ 14, "ModMod",			"%%",		OPTYPE_None		};
+const TokenInfo TOKEN_Question =		{ 14, "Question",		"?",		OPTYPE_None		};
+const TokenInfo TOKEN_Inv =				{ 15, "Inv",			"~",		OPTYPE_Inv		};	// ~
+const TokenInfo TOKEN_Pow =				{ 16, "Pow",			"**",		OPTYPE_Pow		};	// **
+const TokenInfo TOKEN_Quote =			{ 17, "Quote",			"`",		OPTYPE_None		};	// `
+const TokenInfo TOKEN_Colon =			{ 18, "Colon",			":",		OPTYPE_None		};	// :
+const TokenInfo TOKEN_ColonAfterSuffix ={ 19, "ColonAfterSuffix",":",		OPTYPE_None		};	// *:
+const TokenInfo TOKEN_Dot =				{ 20, "Dot",			".",		OPTYPE_None		};	// .
+const TokenInfo TOKEN_ColonColon =		{ 20, "ColonColon",		"::",		OPTYPE_None		};
+const TokenInfo TOKEN_ColonAsterisk =	{ 20, "ColonAsterisk",	":*",		OPTYPE_None		};
+const TokenInfo TOKEN_ColonAnd =		{ 20, "ColonAnd",		":&",		OPTYPE_None		};
+const TokenInfo TOKEN_LParenthesis =	{ 21, "LParenthesis",	"(",		OPTYPE_None		};	// (
+const TokenInfo TOKEN_RParenthesis =	{ 22, "RParenthesis",	")",		OPTYPE_None		};	// )
+const TokenInfo TOKEN_LBrace =			{ 21, "LBrace",			"{ ",		OPTYPE_None		};
+const TokenInfo TOKEN_RBrace =			{ 22, "RBrace",			"}",		OPTYPE_None		};
+const TokenInfo TOKEN_LBlockParam =		{ 21, "LBlockParam",	"|",		OPTYPE_None		};
+const TokenInfo TOKEN_RBlockParam =		{ 22, "RBlockParam",	"|",		OPTYPE_None		};
+const TokenInfo TOKEN_LBracket =		{ 23, "LBracket",		"[",		OPTYPE_None		};	// [
+const TokenInfo TOKEN_RBracket =		{ 24, "RBracket",		"]",		OPTYPE_None		};	// ]
+const TokenInfo TOKEN_Comma =			{ 25, "Comma",			",",		OPTYPE_None		};	// ,
+const TokenInfo TOKEN_Semicolon =		{ 25, "Semicolon",		";",		OPTYPE_None		};
+const TokenInfo TOKEN_EOL =				{ 26, "EOL",			"[EOL]",	OPTYPE_None		};	// \n
+const TokenInfo TOKEN_Number =			{ 27, "Number",			"[Num]",	OPTYPE_None		};	// V
+const TokenInfo TOKEN_NumberSuffixed =	{ 27, "NumberSuffixed",	"[NmS]",	OPTYPE_None		};
+const TokenInfo TOKEN_String =			{ 27, "String",			"[Str]",	OPTYPE_None		};
+const TokenInfo TOKEN_StringSuffixed =	{ 27, "StringSuffixed",	"[StS]",	OPTYPE_None		};
+const TokenInfo TOKEN_Binary =			{ 27, "Binary",			"[Bin]",	OPTYPE_None		};
+const TokenInfo TOKEN_EmbedString =		{ 27, "EmbedString",	"[EmS]",	OPTYPE_None		};
+const TokenInfo TOKEN_Symbol =			{ 28, "Symbol",			"[Sym]",	OPTYPE_None		};	// S
+const TokenInfo TOKEN_EOF =				{ 29, "EOF",			"[EOF]",	OPTYPE_None		};	// E
+const TokenInfo TOKEN_Expr =			{  0, "Expr",			"[Exp]",	OPTYPE_None		};
+const TokenInfo TOKEN_White =			{  0, "White",			"[Wht]",	OPTYPE_None		};
+const TokenInfo TOKEN_CommentLine =		{  0, "CommentLine",	"[CmL]",	OPTYPE_None		};
+const TokenInfo TOKEN_CommentBlock =	{  0, "CommentBlock",	"[CmB]",	OPTYPE_None		};
+const TokenInfo TOKEN_DoubleChars =		{  0, "DoubleChars",	"[DbC]",	OPTYPE_None		};
+const TokenInfo TOKEN_TripleChars =		{  0, "TripleChars",	"[TrC]",	OPTYPE_None		};
+const TokenInfo TOKEN_Unknown =			{  0, "Unknown",		"[unk]",	OPTYPE_None		};
 
 const TokenInfo *const Token::_tokenInfoTbl[] = {
 	&TOKEN_Begin,
@@ -211,8 +138,8 @@ const TokenInfo *const Token::_tokenInfoTbl[] = {
 	&TOKEN_RBrace,			// close token
 	&TOKEN_LBracket,		// open token
 	&TOKEN_RBracket,		// close token
-	&TOKEN_LBlockParam,	// open token
-	&TOKEN_RBlockParam,	// close token
+	&TOKEN_LBlockParam,		// open token
+	&TOKEN_RBlockParam,		// close token
 	&TOKEN_Comma,
 	&TOKEN_Semicolon,
 	&TOKEN_EOL,
@@ -226,10 +153,10 @@ const TokenInfo *const Token::_tokenInfoTbl[] = {
 	&TOKEN_EOF,
 	&TOKEN_Expr,
 	&TOKEN_White,			// for watcher
-	&TOKEN_CommentLine,	// for watcher
+	&TOKEN_CommentLine,		// for watcher
 	&TOKEN_CommentBlock,	// for watcher
-	&TOKEN_DoubleChars,	// only used in tokenizing process
-	&TOKEN_TripleChars,	// only used in tokenizing process
+	&TOKEN_DoubleChars,		// only used in tokenizing process
+	&TOKEN_TripleChars,		// only used in tokenizing process
 	&TOKEN_Unknown,
 };
 
