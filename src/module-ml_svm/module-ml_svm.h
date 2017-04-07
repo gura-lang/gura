ee374736fd1d98f9cc5a788cef4116ee414fe4dc
ypsitau <ypsitau@nifty.com>
2017-04-07 19:07:47 +0900

    added workspace for ml.svm module

---
+++ src/module-ml_svm/module-ml_svm.h
@@ -0,0 +1,11 @@
+//=============================================================================
+// Gura module: ml.svm
+//=============================================================================
+#ifndef __GURA_MODULE_ML_SVM_H__
+#define __GURA_MODULE_ML_SVM_H__
+#include <gura.h>
+
+Gura_BeginModuleHeader(ml_svm)
+Gura_EndModuleHeader(ml_svm)
+
+#endif
