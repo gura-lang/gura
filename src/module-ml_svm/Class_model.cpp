773de55781032d4bfe51f093ad585e77bfbc4de5
ypsitau <ypsitau@nifty.com>
2017-04-12 20:07:06 +0900

    implementing ml.svm and ml.linear modules

--- src/module-ml_svm/Class_model.cpp
+++ src/module-ml_svm/Class_model.cpp
@@ -42,9 +42,8 @@ Gura_ImplementMethod(model, predict)
 	return Value(label);
 }
 
-#if 0
-// ml.linear.model.get_nr_feature()
-Gura_DeclareMethod(model, get_nr_feature)
+// ml.svm.model.get_svm_type()
+Gura_DeclareMethod(model, get_svm_type)
 {
 	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
 	AddHelp(
@@ -52,13 +51,13 @@ Gura_DeclareMethod(model, get_nr_feature)
 		"");
 }
 
-Gura_ImplementMethod(model, get_nr_feature)
+Gura_ImplementMethod(model, get_svm_type)
 {
-	struct model *pModel = Object_model::GetObjectThis(arg)->GetEntity();
-	return Value(::get_nr_feature(pModel));
+	struct svm_model *pModel = Object_model::GetObjectThis(arg)->GetEntity();
+	return Value(::svm_get_svm_type(pModel));
 }
 
-// ml.linear.model.get_nr_class()
+// ml.svm.model.get_nr_class()
 Gura_DeclareMethod(model, get_nr_class)
 {
 	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
@@ -69,11 +68,11 @@ Gura_DeclareMethod(model, get_nr_class)
 
 Gura_ImplementMethod(model, get_nr_class)
 {
-	struct model *pModel = Object_model::GetObjectThis(arg)->GetEntity();
-	return Value(::get_nr_class(pModel));
+	struct svm_model *pModel = Object_model::GetObjectThis(arg)->GetEntity();
+	return Value(::svm_get_nr_class(pModel));
 }
 
-// ml.linear.model.get_labels()
+// ml.svm.model.get_labels()
 Gura_DeclareMethod(model, get_labels)
 {
 	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
@@ -84,10 +83,10 @@ Gura_DeclareMethod(model, get_labels)
 
 Gura_ImplementMethod(model, get_labels)
 {
-	struct model *pModel = Object_model::GetObjectThis(arg)->GetEntity();
-	int nClasses = ::get_nr_class(pModel);
+	struct svm_model *pModel = Object_model::GetObjectThis(arg)->GetEntity();
+	int nClasses = ::svm_get_nr_class(pModel);
 	std::unique_ptr<int[]> labels(new int[nClasses]);
-	::get_labels(pModel, labels.get());
+	::svm_get_labels(pModel, labels.get());
 	Value rtn;
 	Object_list *pObjList = rtn.InitAsList(env);
 	for (int i = 0; i < nClasses; i++) {
@@ -96,43 +95,6 @@ Gura_ImplementMethod(model, get_labels)
 	return rtn;
 }
 
-// ml.linear.model.get_decfun_coef(feat_idx:number, label_idx:number)
-Gura_DeclareMethod(model, get_decfun_coef)
-{
-	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
-	DeclareArg(env, "feat_idx", VTYPE_number);
-	DeclareArg(env, "label_idx", VTYPE_number);
-	AddHelp(
-		Gura_Symbol(en),
-		"");
-}
-
-Gura_ImplementMethod(model, get_decfun_coef)
-{
-	struct model *pModel = Object_model::GetObjectThis(arg)->GetEntity();
-	int feat_idx = arg.GetInt(0);
-	int label_idx = arg.GetInt(1);
-	return Value(::get_decfun_coef(pModel, feat_idx, label_idx));
-}
-
-// ml.linear.model.get_decfun_bias(label_idx:number)
-Gura_DeclareMethod(model, get_decfun_bias)
-{
-	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
-	DeclareArg(env, "label_idx", VTYPE_number);
-	AddHelp(
-		Gura_Symbol(en),
-		"");
-}
-
-Gura_ImplementMethod(model, get_decfun_bias)
-{
-	struct model *pModel = Object_model::GetObjectThis(arg)->GetEntity();
-	int label_idx = arg.GetInt(0);
-	return Value(::get_decfun_bias(pModel, label_idx));
-}
-#endif
-
 //-----------------------------------------------------------------------------
 // Implementation of class
 //-----------------------------------------------------------------------------
@@ -140,13 +102,9 @@ Gura_ImplementUserClass(model)
 {
 	// Assignment of methods
 	Gura_AssignMethod(model, predict);
-#if 0
-	Gura_AssignMethod(model, get_nr_feature);
+	Gura_AssignMethod(model, get_svm_type);
 	Gura_AssignMethod(model, get_nr_class);
 	Gura_AssignMethod(model, get_labels);
-	Gura_AssignMethod(model, get_decfun_coef);
-	Gura_AssignMethod(model, get_decfun_bias);
-#endif
 	// Assignment of value
 	Gura_AssignValue(model, Value(Reference()));
 }
