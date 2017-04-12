0bdebe33ac4889affd54417eabc4db5c40a499fb
ypsitau <ypsitau@nifty.com>
2017-04-12 21:42:31 +0900

    implementing ml.svm module

---
+++ src/module-ml_svm/Class_sample.cpp
@@ -0,0 +1,93 @@
+#include "stdafx.h"
+#include "Class_sample.h"
+
+Gura_BeginModuleScope(ml_svm)
+
+//-----------------------------------------------------------------------------
+// Object_sample implementation
+//-----------------------------------------------------------------------------
+Object_sample::Object_sample(Sample *pSample) :
+					Object(Gura_UserClass(sample)), _pSample(pSample)
+{
+}
+
+String Object_sample::ToString(bool exprFlag)
+{
+	return String("<ml.svm.sample>");
+}
+
+//-----------------------------------------------------------------------------
+// Implementation of properties
+//-----------------------------------------------------------------------------
+// ml.svm.sample#label
+Gura_DeclareProperty_R(sample, label)
+{
+	SetPropAttr(VTYPE_number);
+	AddHelp(
+		Gura_Symbol(en),
+		"");
+}
+
+Gura_ImplementPropertyGetter(sample, label)
+{
+	Sample *pSample = Object_sample::GetObject(valueThis)->GetEntity();
+	return Value(pSample->GetLabel());
+}
+
+// ml.svm.sample#feature
+Gura_DeclareProperty_R(sample, feature)
+{
+	SetPropAttr(VTYPE_feature);
+	AddHelp(
+		Gura_Symbol(en),
+		"");
+}
+
+Gura_ImplementPropertyGetter(sample, feature)
+{
+	Sample *pSample = Object_sample::GetObject(valueThis)->GetEntity();
+	return Value(new Object_feature(pSample->GetFeature()->Reference()));
+}
+
+//-----------------------------------------------------------------------------
+// Implementation of functions
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Implementation of class
+//-----------------------------------------------------------------------------
+Gura_ImplementUserClass(sample)
+{
+	// Assignment of properties
+	Gura_AssignProperty(sample, label);
+	Gura_AssignProperty(sample, feature);
+	// Assignment of function
+	//Gura_AssignFunction(sample);
+}
+
+//-----------------------------------------------------------------------------
+// Iterator_sample
+//-----------------------------------------------------------------------------
+Iterator *Iterator_sample::GetSource()
+{
+	return nullptr;
+}
+
+bool Iterator_sample::DoNext(Environment &env, Value &value)
+{
+	if (_idx >= _pSampleOwner->size()) return false;
+	value = Value(new Object_sample((*_pSampleOwner)[_idx]->Reference()));
+	_idx++;
+	return true;
+}
+
+String Iterator_sample::ToString() const
+{
+	return "ml.svm.sample";
+}
+
+void Iterator_sample::GatherFollower(Environment::Frame *pFrame, EnvironmentSet &envSet)
+{
+}
+
+Gura_EndModuleScope(ml_svm)
---
+++ src/module-ml_svm/Class_sample.h
@@ -0,0 +1,42 @@
+#ifndef __ML_SVM_CLASS_SAMPLE_H__
+#define __ML_SVM_CLASS_SAMPLE_H__
+#include <gura.h>
+#include "Sample.h"
+
+Gura_BeginModuleScope(ml_svm)
+
+//-----------------------------------------------------------------------------
+// Object_sample declaration
+//-----------------------------------------------------------------------------
+Gura_DeclareUserClass(sample);
+
+class Object_sample : public Object {
+private:
+	AutoPtr<Sample> _pSample;
+public:
+	Gura_DeclareObjectAccessor(sample)
+public:
+	Object_sample(Sample *pSample);
+	virtual String ToString(bool exprFlag);
+	inline Sample *GetEntity() { return _pSample.get(); }
+};
+
+//-----------------------------------------------------------------------------
+// Iterator_sample
+//-----------------------------------------------------------------------------
+class GURA_DLLDECLARE Iterator_sample : public Iterator {
+private:
+	AutoPtr<SampleOwner> _pSampleOwner;
+	size_t _idx;
+public:
+	inline Iterator_sample(SampleOwner *pSampleOwner) :
+		Iterator(Finite), _pSampleOwner(pSampleOwner), _idx(0) {}
+	virtual Iterator *GetSource();
+	virtual bool DoNext(Environment &env, Value &value);
+	virtual String ToString() const;
+	virtual void GatherFollower(Environment::Frame *pFrame, EnvironmentSet &envSet);
+};
+
+Gura_EndModuleScope(ml_svm)
+
+#endif
