01c6c8286f9d2c129c61e1f321e0940d140f9e62
ypsitau <ypsitau@nifty.com>
2017-06-02 22:02:43 +0900

    implementing fftw module

--- src/module-fftw/module-fftw.cpp
+++ src/module-fftw/module-fftw.cpp
@@ -2,6 +2,7 @@
 // Gura module: fftw
 //=============================================================================
 #include "stdafx.h"
+#include "fftw3.h"
 
 Gura_BeginModuleBody(fftw)
 
@@ -21,7 +22,17 @@ Gura_DeclareFunction(test)
 
 Gura_ImplementFunction(test)
 {
-	return Value(arg.GetNumber(0) + arg.GetNumber(1));
+	int N = 256;
+	fftw_complex *in = (fftw_complex*)::fftw_malloc(sizeof(fftw_complex) * N);
+	fftw_complex *out = (fftw_complex*)::fftw_malloc(sizeof(fftw_complex) * N);
+	fftw_plan p = ::fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+
+	::fftw_execute(p); /* repeat as needed */
+
+	::fftw_destroy_plan(p);
+	::fftw_free(in);
+	::fftw_free(out);
+	return Value::Nil;
 }
 
 //-----------------------------------------------------------------------------
