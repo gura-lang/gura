91602e61d7d47c3cb37206098466c3d64aaa6ac1
ypsitau <ypsitau@nifty.com>
2017-06-02 21:34:44 +0900

    added skeleton code for fftw module

---
+++ src/module-fftw/module-fftw.h
@@ -0,0 +1,11 @@
+//=============================================================================
+// Gura module: fftw
+//=============================================================================
+#ifndef __GURA_MODULE_FFTW_H__
+#define __GURA_MODULE_FFTW_H__
+#include <gura.h>
+
+Gura_BeginModuleHeader(fftw)
+Gura_EndModuleHeader(fftw)
+
+#endif
---
+++ src/module-fftw/stdafx.h
@@ -0,0 +1,4 @@
+#ifndef __GURA_MODULE_FFTW_STDAFX_H__
+#define __GURA_MODULE_FFTW_STDAFX_H__
+#include "module-fftw.h"
+#endif
