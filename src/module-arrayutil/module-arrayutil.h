ec6b0b93efd3bf0a9c36b6e7de1f0a0f84e34c71
ypsitau <ypsitau@nifty.com>
2017-06-21 16:12:00 +0900

    added skeleton for arrayutil module

---
+++ src/module-arrayutil/module-arrayutil.h
@@ -0,0 +1,11 @@
+//=============================================================================
+// Gura module: arrayutil
+//=============================================================================
+#ifndef __GURA_MODULE_ARRAYUTIL_H__
+#define __GURA_MODULE_ARRAYUTIL_H__
+#include <gura.h>
+
+Gura_BeginModuleHeader(arrayutil)
+Gura_EndModuleHeader(arrayutil)
+
+#endif
