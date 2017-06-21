fd0f9a436e44e1e2f126cf2aa5fe604e57da4ad2
ypsitau <ypsitau@nifty.com>
2017-06-21 16:25:52 +0900

    implementing arrayutil module

--- src/module-arrayutil/Operator.cpp
+++ src/module-arrayutil/Operator.cpp
@@ -2,8 +2,104 @@
 
 Gura_BeginModuleScope(arrayutil)
 
+//-----------------------------------------------------------------------------
+// [+A] ... UnaryOperator(Pos, A)
+//-----------------------------------------------------------------------------
+Gura_ImplementUnaryOperator(Pos, array)
+{
+	return Array::ApplyUnaryFuncOnValue(env, Array::unaryFuncPack_Pos, value);
+}
+
+//-----------------------------------------------------------------------------
+// [-A] ... UnaryOperator(Neg, A)
+//-----------------------------------------------------------------------------
+Gura_ImplementUnaryOperator(Neg, array)
+{
+	return Array::ApplyUnaryFuncOnValue(env, Array::unaryFuncPack_Neg, value);
+}
+
+//-----------------------------------------------------------------------------
+// [A + B] ... BinaryOperator(Add, A, B)
+//-----------------------------------------------------------------------------
+Gura_ImplementBinaryOperator(Add, array, array)
+{
+	return Array::ApplyBinaryFuncOnValue_array_array(
+		env, Array::binaryFuncPack_Add, valueLeft, valueRight);
+}
+
+Gura_ImplementBinaryOperator(Add, array, number)
+{
+	return Array::ApplyBinaryFuncOnValue_array_number(
+		env, Array::binaryFuncPack_Add, valueLeft, valueRight);
+}
+
+Gura_ImplementBinaryOperator(Add, number, array)
+{
+	return Array::ApplyBinaryFuncOnValue_number_array(
+		env, Array::binaryFuncPack_Add, valueLeft, valueRight);
+}
+
+Gura_ImplementBinaryOperator(Add, array, complex)
+{
+	return Array::ApplyBinaryFuncOnValue_array_complex(
+		env, Array::binaryFuncPack_Add, valueLeft, valueRight);
+}
+
+Gura_ImplementBinaryOperator(Add, complex, array)
+{
+	return Array::ApplyBinaryFuncOnValue_complex_array(
+		env, Array::binaryFuncPack_Add, valueLeft, valueRight);
+}
+
+//-----------------------------------------------------------------------------
+// [A - B] ... BinaryOperator(Sub, A, B)
+//-----------------------------------------------------------------------------
+Gura_ImplementBinaryOperator(Sub, array, array)
+{
+	return Array::ApplyBinaryFuncOnValue_array_array(
+		env, Array::binaryFuncPack_Sub, valueLeft, valueRight);
+}
+
+Gura_ImplementBinaryOperator(Sub, array, number)
+{
+	return Array::ApplyBinaryFuncOnValue_array_number(
+		env, Array::binaryFuncPack_Sub, valueLeft, valueRight);
+}
+
+Gura_ImplementBinaryOperator(Sub, number, array)
+{
+	return Array::ApplyBinaryFuncOnValue_number_array(
+		env, Array::binaryFuncPack_Sub, valueLeft, valueRight);
+}
+
+Gura_ImplementBinaryOperator(Sub, array, complex)
+{
+	return Array::ApplyBinaryFuncOnValue_array_complex(
+		env, Array::binaryFuncPack_Sub, valueLeft, valueRight);
+}
+
+Gura_ImplementBinaryOperator(Sub, complex, array)
+{
+	return Array::ApplyBinaryFuncOnValue_complex_array(
+		env, Array::binaryFuncPack_Sub, valueLeft, valueRight);
+}
+
 void AssignOperators(Environment &env)
 {
+	// unary operators
+	Gura_AssignUnaryOperator(Pos, array);
+	Gura_AssignUnaryOperator(Neg, array);
+	// binary operators
+	Gura_AssignBinaryOperator(Add, array, array);
+	Gura_AssignBinaryOperator(Add, array, number);
+	Gura_AssignBinaryOperator(Add, number, array);
+	Gura_AssignBinaryOperator(Add, array, complex);
+	Gura_AssignBinaryOperator(Add, complex, array);
+	Gura_AssignBinaryOperator(Sub, array, array);
+	Gura_AssignBinaryOperator(Sub, array, number);
+	Gura_AssignBinaryOperator(Sub, number, array);
+	Gura_AssignBinaryOperator(Sub, array, complex);
+	Gura_AssignBinaryOperator(Sub, complex, array);
 }
 
 Gura_EndModuleScope(arrayutil)
--- src/module-arrayutil/module-arrayutil.cpp
+++ src/module-arrayutil/module-arrayutil.cpp
@@ -25,88 +25,6 @@ Gura_ImplementFunction(test)
 }
 
 //-----------------------------------------------------------------------------
-// [+A] ... UnaryOperator(Pos, A)
-//-----------------------------------------------------------------------------
-Gura_ImplementUnaryOperator(Pos, array)
-{
-	return Array::ApplyUnaryFuncOnValue(env, Array::unaryFuncPack_Pos, value);
-}
-
-//-----------------------------------------------------------------------------
-// [-A] ... UnaryOperator(Neg, A)
-//-----------------------------------------------------------------------------
-Gura_ImplementUnaryOperator(Neg, array)
-{
-	return Array::ApplyUnaryFuncOnValue(env, Array::unaryFuncPack_Neg, value);
-}
-
-//-----------------------------------------------------------------------------
-// [A + B] ... BinaryOperator(Add, A, B)
-//-----------------------------------------------------------------------------
-Gura_ImplementBinaryOperator(Add, array, array)
-{
-	return Array::ApplyBinaryFuncOnValue_array_array(
-		env, Array::binaryFuncPack_Add, valueLeft, valueRight);
-}
-
-Gura_ImplementBinaryOperator(Add, array, number)
-{
-	return Array::ApplyBinaryFuncOnValue_array_number(
-		env, Array::binaryFuncPack_Add, valueLeft, valueRight);
-}
-
-Gura_ImplementBinaryOperator(Add, number, array)
-{
-	return Array::ApplyBinaryFuncOnValue_number_array(
-		env, Array::binaryFuncPack_Add, valueLeft, valueRight);
-}
-
-Gura_ImplementBinaryOperator(Add, array, complex)
-{
-	return Array::ApplyBinaryFuncOnValue_array_complex(
-		env, Array::binaryFuncPack_Add, valueLeft, valueRight);
-}
-
-Gura_ImplementBinaryOperator(Add, complex, array)
-{
-	return Array::ApplyBinaryFuncOnValue_complex_array(
-		env, Array::binaryFuncPack_Add, valueLeft, valueRight);
-}
-
-//-----------------------------------------------------------------------------
-// [A - B] ... BinaryOperator(Sub, A, B)
-//-----------------------------------------------------------------------------
-Gura_ImplementBinaryOperator(Sub, array, array)
-{
-	return Array::ApplyBinaryFuncOnValue_array_array(
-		env, Array::binaryFuncPack_Sub, valueLeft, valueRight);
-}
-
-Gura_ImplementBinaryOperator(Sub, array, number)
-{
-	return Array::ApplyBinaryFuncOnValue_array_number(
-		env, Array::binaryFuncPack_Sub, valueLeft, valueRight);
-}
-
-Gura_ImplementBinaryOperator(Sub, number, array)
-{
-	return Array::ApplyBinaryFuncOnValue_number_array(
-		env, Array::binaryFuncPack_Sub, valueLeft, valueRight);
-}
-
-Gura_ImplementBinaryOperator(Sub, array, complex)
-{
-	return Array::ApplyBinaryFuncOnValue_array_complex(
-		env, Array::binaryFuncPack_Sub, valueLeft, valueRight);
-}
-
-Gura_ImplementBinaryOperator(Sub, complex, array)
-{
-	return Array::ApplyBinaryFuncOnValue_complex_array(
-		env, Array::binaryFuncPack_Sub, valueLeft, valueRight);
-}
-
-//-----------------------------------------------------------------------------
 // Module Entries
 //-----------------------------------------------------------------------------
 Gura_ModuleValidate()
@@ -118,20 +36,7 @@ Gura_ModuleEntry()
 {
 	// function assignment
 	Gura_AssignFunction(test);
-	// unary operators
-	Gura_AssignUnaryOperator(Pos, array);
-	Gura_AssignUnaryOperator(Neg, array);
-	// binary operators
-	Gura_AssignBinaryOperator(Add, array, array);
-	Gura_AssignBinaryOperator(Add, array, number);
-	Gura_AssignBinaryOperator(Add, number, array);
-	Gura_AssignBinaryOperator(Add, array, complex);
-	Gura_AssignBinaryOperator(Add, complex, array);
-	Gura_AssignBinaryOperator(Sub, array, array);
-	Gura_AssignBinaryOperator(Sub, array, number);
-	Gura_AssignBinaryOperator(Sub, number, array);
-	Gura_AssignBinaryOperator(Sub, array, complex);
-	Gura_AssignBinaryOperator(Sub, complex, array);
+	AssignOperators(env);
 	return true;
 }
 
