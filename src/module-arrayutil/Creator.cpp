86e97b46f6a26d76af6ed560476d0a0b264b1c66
ypsitau <ypsitau@nifty.com>
2017-06-25 19:45:22 +0900

    implementing arrayutil module

--- src/module-arrayutil/Creator.cpp
+++ src/module-arrayutil/Creator.cpp
@@ -3,6 +3,15 @@
 Gura_BeginModuleScope(arrayutil)
 
 //-----------------------------------------------------------------------------
+// utilities
+//-----------------------------------------------------------------------------
+void SetError_CreationError(Environment &env, Array::ElemType elemType)
+{
+	env.SetError(ERR_ValueError, "can't create an array of %s",
+				 Array::GetElemTypeName(elemType));
+}
+
+//-----------------------------------------------------------------------------
 // Implementation of array creators
 //-----------------------------------------------------------------------------
 // array.identity(n:number, elemtype?:symbol):static:map {block?}
@@ -23,14 +32,31 @@ Gura_DeclareClassMethod(array, identity)
 		);
 }
 
+template<typename T_Elem> Array *FuncTmpl_identity(size_t n)
+{
+	AutoPtr<ArrayT<T_Elem> > pArrayT(new ArrayT<T_Elem>(n, n));
+	pArrayT->FillZero();
+	T_Elem *p = pArrayT->GetPointer();
+	size_t stride = n + 1;
+	for (size_t i = 0; i < n; i++, p += stride) *p = 1;
+	return pArrayT.release();
+}
+
+
 Gura_ImplementClassMethod(array, identity)
 {
+	typedef Array *(*FuncT)(size_t n);
+	DeclareFunctionTable1D(FuncT, funcTbl, FuncTmpl_identity);
 	Array::ElemType elemType = arg.IsValid(1)?
 		Array::SymbolToElemTypeWithError(env, arg.GetSymbol(1)) : Array::ETYPE_Double;
 	if (env.IsSignalled()) return Value::Nil;
-	AutoPtr<Array> pArray(ArrayT<Double>::CreateIdentity(arg.GetSizeT(0)));
-	Value value(new Object_array(env, pArray.release()));
-	return ReturnValue(env, arg, value);
+	FuncT func = funcTbl[elemType];
+	if (func == nullptr) {
+		SetError_CreationError(env, elemType);
+		return Value::Nil;
+	}
+	AutoPtr<Array> pArray((*func)(arg.GetSizeT(0)));
+	return ReturnValue(env, arg, new Object_array(env, pArray.release()));
 }
 
 // array.interval(begin:number, end:number, samples:number, elemtype?:symbol):static:map:[open,open_l,open_r] {block?}
@@ -64,11 +90,30 @@ Gura_DeclareClassMethod(array, interval)
 		);
 }
 
+template<typename T_Elem> Array *FuncTmpl_interval(
+	Double numBegin, Double numEnd, int numSamples, Double numDenom, int iFactor)
+{
+	AutoPtr<ArrayT<T_Elem> > pArrayT(new ArrayT<T_Elem>(numSamples));
+	T_Elem *p = pArrayT->GetPointer();
+	for (int i = 0; i < numSamples; i++, iFactor++) {
+		p[i] = static_cast<T_Elem>((numEnd - numBegin) * iFactor / numDenom + numBegin);
+	}
+	return pArrayT.release();
+}
+
 Gura_ImplementClassMethod(array, interval)
 {
+	typedef Array *(*FuncT)(Double numBegin, Double numEnd, int numSamples,
+							Double numDenom, int iFactor);
+	DeclareFunctionTable1D(FuncT, funcTbl, FuncTmpl_interval);
 	Array::ElemType elemType = arg.IsValid(3)?
 		Array::SymbolToElemTypeWithError(env, arg.GetSymbol(3)) : Array::ETYPE_Double;
 	if (env.IsSignalled()) return Value::Nil;
+	FuncT func = funcTbl[elemType];
+	if (func == nullptr) {
+		SetError_CreationError(env, elemType);
+		return Value::Nil;
+	}
 	Number numBegin = arg.GetNumber(0);
 	Number numEnd = arg.GetNumber(1);
 	int numSamples = arg.GetInt(2);
@@ -91,10 +136,8 @@ Gura_ImplementClassMethod(array, interval)
 		numDenom = numSamples;
 		iFactor = 0;
 	}
-	AutoPtr<Array> pArray(ArrayT<Double>::CreateInterval(
-										 numBegin, numEnd, numSamples, numDenom, iFactor));
-	Value value(new Object_array(env, pArray.release()));
-	return ReturnValue(env, arg, value);
+	AutoPtr<Array> pArray((*func)(numBegin, numEnd, numSamples, numDenom, iFactor));
+	return ReturnValue(env, arg, Value(new Object_array(env, pArray.release())));
 }
 
 // array.ones(dims[]:number, elemtype?:symbol):static:map {block?}
@@ -115,13 +158,29 @@ Gura_DeclareClassMethod(array, ones)
 		);
 }
 
+template<typename T_Elem> Array *FuncTmpl_ones(const ValueList &valList)
+{
+	AutoPtr<ArrayT<T_Elem> > pArrayT(new ArrayT<T_Elem>());
+	pArrayT->SetDimensions(valList);
+	pArrayT->AllocMemory();
+	pArrayT->Fill(1);
+	return pArrayT.release();
+}
+
 Gura_ImplementClassMethod(array, ones)
 {
+	typedef Array *(*FuncT)(const ValueList &valList);
+	DeclareFunctionTable1D(FuncT, funcTbl, FuncTmpl_ones);
 	Array::ElemType elemType = arg.IsValid(1)?
 		Array::SymbolToElemTypeWithError(env, arg.GetSymbol(1)) : Array::ETYPE_Double;
 	if (env.IsSignalled()) return Value::Nil;
-	Value value(new Object_array(env, ArrayT<Double>::CreateOnes(arg.GetList(0))));
-	return ReturnValue(env, arg, value);
+	FuncT func = funcTbl[elemType];
+	if (func == nullptr) {
+		SetError_CreationError(env, elemType);
+		return Value::Nil;
+	}
+	AutoPtr<Array> pArray((*func)(arg.GetList(0)));
+	return ReturnValue(env, arg, Value(new Object_array(env, pArray.release())));
 }
 
 // array.rands(dims[]:number, range?:number, elemtype?:symbol):static:map {block?}
@@ -138,14 +197,30 @@ Gura_DeclareClassMethod(array, rands)
 		);
 }
 
+template<typename T_Elem> Array *FuncTmpl_rands(const ValueList &valList, UInt range)
+{
+	AutoPtr<ArrayT<T_Elem> > pArrayT(new ArrayT<T_Elem>());
+	pArrayT->SetDimensions(valList);
+	pArrayT->AllocMemory();
+	pArrayT->FillRand(range);
+	return pArrayT.release();
+}
+
 Gura_ImplementClassMethod(array, rands)
 {
+	typedef Array *(*FuncT)(const ValueList &valList, UInt range);
+	DeclareFunctionTable1D(FuncT, funcTbl, FuncTmpl_rands);
 	Array::ElemType elemType = arg.IsValid(2)?
 		Array::SymbolToElemTypeWithError(env, arg.GetSymbol(2)) : Array::ETYPE_Double;
 	if (env.IsSignalled()) return Value::Nil;
+	FuncT func = funcTbl[elemType];
+	if (func == nullptr) {
+		SetError_CreationError(env, elemType);
+		return Value::Nil;
+	}
 	UInt range = arg.IsValid(1)? arg.GetUInt(1) : 0;
-	Value value(new Object_array(env, ArrayT<Double>::CreateRands(arg.GetList(0), range)));
-	return ReturnValue(env, arg, value);
+	AutoPtr<Array> pArray((*func)(arg.GetList(0), range));
+	return ReturnValue(env, arg, Value(new Object_array(env, pArray.release())));
 }
 
 // array.rands@normal(dims[]:number, mu?:number, sigma?:number, elemtype?:symbol):static:map {block?}
@@ -164,16 +239,31 @@ Gura_DeclareClassMethodAlias(array, rands_at_normal, "rands@normal")
 		);
 }
 
+template<typename T_Elem> Array *FuncTmpl_rands_at_normal(const ValueList &valList, double mu, double sigma)
+{
+	AutoPtr<ArrayT<T_Elem> > pArrayT(new ArrayT<T_Elem>());
+	pArrayT->SetDimensions(valList);
+	pArrayT->AllocMemory();
+	pArrayT->FillRandNormal(mu, sigma);
+	return pArrayT.release();
+}
+
 Gura_ImplementClassMethod(array, rands_at_normal)
 {
+	typedef Array *(*FuncT)(const ValueList &valList, double mu, double sigma);
+	DeclareFunctionTable1D(FuncT, funcTbl, FuncTmpl_rands_at_normal);
 	Array::ElemType elemType = arg.IsValid(3)?
 		Array::SymbolToElemTypeWithError(env, arg.GetSymbol(3)) : Array::ETYPE_Double;
 	if (env.IsSignalled()) return Value::Nil;
+	FuncT func = funcTbl[elemType];
+	if (func == nullptr) {
+		SetError_CreationError(env, elemType);
+		return Value::Nil;
+	}
 	double mu = arg.IsValid(1)? arg.GetDouble(1) : 0;
 	double sigma = arg.IsValid(2)? arg.GetDouble(2) : 1;
-	Value value(new Object_array(env, ArrayT<Double>::CreateRandsNormal(
-									 arg.GetList(0), mu, sigma)));
-	return ReturnValue(env, arg, value);
+	AutoPtr<Array> pArray((*func)(arg.GetList(0), mu, sigma));
+	return ReturnValue(env, arg, Value(new Object_array(env, pArray.release())));
 }
 
 // array.range(num:number, num_end?:number, step?:number, elemtype?:symbol):static:map {block?}
@@ -206,11 +296,37 @@ Gura_DeclareClassMethod(array, range)
 		);
 }
 
+template<typename T_Elem> Array *FuncTmpl_range(Double numBegin, Double numEnd, Double numStep)
+{
+	int numSamples = 0;
+	if (numEnd > numBegin) {
+		Double numRange = numEnd - numBegin;
+		numSamples = static_cast<int>((numRange - 1) / numStep) + 1;
+	} else {
+		Double numRange = numBegin - numEnd;
+		numSamples = static_cast<int>(-(numRange - 1) / numStep) + 1;
+	}
+	if (numSamples < 0) numSamples = 0;
+	AutoPtr<ArrayT<T_Elem> > pArrayT(new ArrayT<T_Elem>(numSamples));
+	T_Elem *p = pArrayT->GetPointer();
+	for (size_t i = 0; i < numSamples; i++) {
+		p[i] = static_cast<T_Elem>(numBegin + numStep * i);
+	}
+	return pArrayT.release();
+}
+
 Gura_ImplementClassMethod(array, range)
 {
+	typedef Array *(*FuncT)(Double numBegin, Double numEnd, Double numStep);
+	DeclareFunctionTable1D(FuncT, funcTbl, FuncTmpl_range);
 	Array::ElemType elemType = arg.IsValid(3)?
 		Array::SymbolToElemTypeWithError(env, arg.GetSymbol(3)) : Array::ETYPE_Double;
 	if (env.IsSignalled()) return Value::Nil;
+	FuncT func = funcTbl[elemType];
+	if (func == nullptr) {
+		SetError_CreationError(env, elemType);
+		return Value::Nil;
+	}
 	Double numBegin = 0.;
 	Double numEnd = 0.;
 	Double numStep = 1.;
@@ -235,8 +351,8 @@ Gura_ImplementClassMethod(array, range)
 	} else {
 		numEnd = arg.GetDouble(0);
 	}
-	AutoPtr<ArrayT<Double> > pArrayT(ArrayT<Double>::CreateRange(numBegin, numEnd, numStep));
-	return ReturnValue(env, arg, Value(new Object_array(env, pArrayT.release())));
+	AutoPtr<Array> pArray((*func)(numBegin, numEnd, numStep));
+	return ReturnValue(env, arg, Value(new Object_array(env, pArray.release())));
 }
 
 // array.rotation(angle:number, xtrans?:number, ytrans?:number, elemtype?:symbol):static:map:[deg] {block?}
@@ -261,23 +377,53 @@ Gura_DeclareClassMethod(array, rotation)
 		);
 }
 
+template<typename T_Elem> Array *FuncTmpl_rotation(double rad, bool transFlag, double xTrans, double yTrans)
+{
+	int sizeMat = transFlag? 3 : 2;
+	T_Elem numCos = static_cast<T_Elem>(::cos(rad));
+	T_Elem numSin = static_cast<T_Elem>(::sin(rad));
+	AutoPtr<ArrayT<T_Elem> > pArrayT(new ArrayT<T_Elem>(sizeMat, sizeMat));
+	T_Elem *p = pArrayT->GetPointer();
+	// row-1
+	*p++ = numCos;
+	*p++ = -numSin;
+	if (transFlag) *p++ = static_cast<T_Elem>(xTrans);
+	// row-2
+	*p++ = numSin;
+	*p++ = numCos;
+	if (transFlag) {
+		*p++ = static_cast<T_Elem>(yTrans);
+		// row-3
+		*p++ = 0;
+		*p++ = 0;
+		*p++ = 1;
+	}
+	return pArrayT.release();
+}
+
 Gura_ImplementClassMethod(array, rotation)
 {
+	typedef Array *(*FuncT)(double rad, bool transFlag, double xTrans, double yTrans);
+	DeclareFunctionTable1D(FuncT, funcTbl, FuncTmpl_rotation);
 	Array::ElemType elemType = arg.IsValid(3)?
 		Array::SymbolToElemTypeWithError(env, arg.GetSymbol(3)) : Array::ETYPE_Double;
 	if (env.IsSignalled()) return Value::Nil;
+	FuncT func = funcTbl[elemType];
+	if (func == nullptr) {
+		SetError_CreationError(env, elemType);
+		return Value::Nil;
+	}
 	double angle = arg.GetDouble(0);
 	if (arg.IsSet(Gura_Symbol(deg))) angle = DegToRad(angle);
 	AutoPtr<Array> pArray;
 	if (arg.IsValid(1) || arg.IsValid(2)) {
-		Double xTrans = arg.IsValid(1)? static_cast<Double>(arg.GetDouble(1)) : 0;
-		Double yTrans = arg.IsValid(2)? static_cast<Double>(arg.GetDouble(2)) : 0;
-		pArray.reset(ArrayT<Double>::CreateRotation(angle, true, xTrans, yTrans));
+		Double xTrans = arg.IsValid(1)? arg.GetDouble(1) : 0;
+		Double yTrans = arg.IsValid(2)? arg.GetDouble(2) : 0;
+		pArray.reset((*func)(angle, true, xTrans, yTrans));
 	} else {
-		pArray.reset(ArrayT<Double>::CreateRotation(angle, false, 0, 0));
+		pArray.reset((*func)(angle, false, 0, 0));
 	}
-	Value value(new Object_array(env, pArray.release()));
-	return ReturnValue(env, arg, value);
+	return ReturnValue(env, arg, Value(new Object_array(env, pArray.release())));
 }
 
 // array.rotation@x(angle:number, xtrans?:number, ytrans?:number, ztrans?:number, elemtype?:symbol):static:map:[deg] {block?}
@@ -303,11 +449,54 @@ Gura_DeclareClassMethodAlias(array, rotation_at_x, "rotation@x")
 		);
 }
 
+template<typename T_Elem> Array *FuncTmpl_rotation_at_x(Double rad, bool transFlag, Double xTrans, Double yTrans, Double zTrans)
+{
+	int sizeMat = transFlag? 4 : 3;
+	T_Elem numCos = static_cast<T_Elem>(::cos(rad));
+	T_Elem numSin = static_cast<T_Elem>(::sin(rad));
+	AutoPtr<ArrayT<T_Elem> > pArrayT(new ArrayT<T_Elem>(sizeMat, sizeMat));
+	T_Elem *p = pArrayT->GetPointer();
+	// row-1
+	*p++ = 1;
+	*p++ = 0;
+	*p++ = 0;
+	if (transFlag) {
+		*p++ = static_cast<T_Elem>(xTrans);
+	}
+	// row-2
+	*p++ = 0;
+	*p++ = numCos;
+	*p++ = -numSin;
+	if (transFlag) {
+		*p++ = static_cast<T_Elem>(yTrans);
+	}
+	// row-3
+	*p++ = 0;
+	*p++ = numSin;
+	*p++ = numCos;
+	if (transFlag) {
+		*p++ = static_cast<T_Elem>(zTrans);
+		// row-4
+		*p++ = 0;
+		*p++ = 0;
+		*p++ = 0;
+		*p++ = 1;
+	}
+	return pArrayT.release();
+}
+
 Gura_ImplementClassMethod(array, rotation_at_x)
 {
+	typedef Array *(*FuncT)(Double rad, bool transFlag, Double xTrans, Double yTrans, Double zTrans);
+	DeclareFunctionTable1D(FuncT, funcTbl, FuncTmpl_rotation_at_x);
 	Array::ElemType elemType = arg.IsValid(4)?
 		Array::SymbolToElemTypeWithError(env, arg.GetSymbol(4)) : Array::ETYPE_Double;
 	if (env.IsSignalled()) return Value::Nil;
+	FuncT func = funcTbl[elemType];
+	if (func == nullptr) {
+		SetError_CreationError(env, elemType);
+		return Value::Nil;
+	}
 	double angle = arg.GetDouble(0);
 	if (arg.IsSet(Gura_Symbol(deg))) angle = DegToRad(angle);
 	AutoPtr<Array> pArray;
@@ -315,12 +504,11 @@ Gura_ImplementClassMethod(array, rotation_at_x)
 		Double xTrans = arg.IsValid(1)? static_cast<Double>(arg.GetDouble(1)) : 0;
 		Double yTrans = arg.IsValid(2)? static_cast<Double>(arg.GetDouble(2)) : 0;
 		Double zTrans = arg.IsValid(3)? static_cast<Double>(arg.GetDouble(3)) : 0;
-		pArray.reset(ArrayT<Double>::CreateRotationX(angle, true, xTrans, yTrans, zTrans));
+		pArray.reset((*func)(angle, true, xTrans, yTrans, zTrans));
 	} else {
-		pArray.reset(ArrayT<Double>::CreateRotationX(angle, false, 0, 0, 0));
+		pArray.reset((*func)(angle, false, 0, 0, 0));
 	}
-	Value value(new Object_array(env, pArray.release()));
-	return ReturnValue(env, arg, value);
+	return ReturnValue(env, arg, Value(new Object_array(env, pArray.release())));
 }
 
 // array.rotation@y(angle:number, xtrans?:number, ytrans?:number, ztrans?:number, elemtype?:symbol):static:map:[deg] {block?}
@@ -346,11 +534,56 @@ Gura_DeclareClassMethodAlias(array, rotation_at_y, "rotation@y")
 		);
 }
 
+template<typename T_Elem> Array *FuncTmpl_rotation_at_y(Double rad, bool transFlag, Double xTrans, Double yTrans, Double zTrans)
+{
+	typedef Array *(*FuncT)(Double rad, bool transFlag, Double xTrans, Double yTrans, Double zTrans);
+	DeclareFunctionTable1D(FuncT, funcTbl, FuncTmpl_rotation_at_y);
+	int sizeMat = transFlag? 4 : 3;
+	T_Elem numCos = static_cast<T_Elem>(::cos(rad));
+	T_Elem numSin = static_cast<T_Elem>(::sin(rad));
+	AutoPtr<ArrayT<T_Elem> > pArrayT(new ArrayT<T_Elem>(sizeMat, sizeMat));
+	T_Elem *p = pArrayT->GetPointer();
+	// row-1
+	*p++ = numCos;
+	*p++ = 0;
+	*p++ = numSin;
+	if (transFlag) {
+		*p++ = static_cast<T_Elem>(xTrans);
+	}
+	// row-2
+	*p++ = 0;
+	*p++ = 1;
+	*p++ = 0;
+	if (transFlag) {
+		*p++ = static_cast<T_Elem>(yTrans);
+	}
+	// row-3
+	*p++ = -numSin;
+	*p++ = 0;
+	*p++ = numCos;
+	if (transFlag) {
+		*p++ = static_cast<T_Elem>(zTrans);
+		// row-4
+		*p++ = 0;
+		*p++ = 0;
+		*p++ = 0;
+		*p++ = 1;
+	}
+	return pArrayT.release();
+}
+
 Gura_ImplementClassMethod(array, rotation_at_y)
 {
+	typedef Array *(*FuncT)(Double rad, bool transFlag, Double xTrans, Double yTrans, Double zTrans);
+	DeclareFunctionTable1D(FuncT, funcTbl, FuncTmpl_rotation_at_y);
 	Array::ElemType elemType = arg.IsValid(4)?
 		Array::SymbolToElemTypeWithError(env, arg.GetSymbol(4)) : Array::ETYPE_Double;
 	if (env.IsSignalled()) return Value::Nil;
+	FuncT func = funcTbl[elemType];
+	if (func == nullptr) {
+		SetError_CreationError(env, elemType);
+		return Value::Nil;
+	}
 	double angle = arg.GetDouble(0);
 	if (arg.IsSet(Gura_Symbol(deg))) angle = DegToRad(angle);
 	AutoPtr<Array> pArray;
@@ -358,12 +591,11 @@ Gura_ImplementClassMethod(array, rotation_at_y)
 		Double xTrans = arg.IsValid(1)? static_cast<Double>(arg.GetDouble(1)) : 0;
 		Double yTrans = arg.IsValid(2)? static_cast<Double>(arg.GetDouble(2)) : 0;
 		Double zTrans = arg.IsValid(3)? static_cast<Double>(arg.GetDouble(3)) : 0;
-		pArray.reset(ArrayT<Double>::CreateRotationY(angle, true, xTrans, yTrans, zTrans));
+		pArray.reset((*func)(angle, true, xTrans, yTrans, zTrans));
 	} else {
-		pArray.reset(ArrayT<Double>::CreateRotationY(angle, false, 0, 0, 0));
+		pArray.reset((*func)(angle, false, 0, 0, 0));
 	}
-	Value value(new Object_array(env, pArray.release()));
-	return ReturnValue(env, arg, value);
+	return ReturnValue(env, arg, Value(new Object_array(env, pArray.release())));
 }
 
 // array.rotation@z(angle:number, xtrans?:number, ytrans?:number, ztrans?:number, elemtype?:symbol):static:map:[deg] {block?}
@@ -389,11 +621,54 @@ Gura_DeclareClassMethodAlias(array, rotation_at_z, "rotation@z")
 		);
 }
 
+template<typename T_Elem> Array *FuncTmpl_rotation_at_z(Double rad, bool transFlag, Double xTrans, Double yTrans, Double zTrans)
+{
+	int sizeMat = transFlag? 4 : 3;
+	T_Elem numCos = static_cast<T_Elem>(::cos(rad));
+	T_Elem numSin = static_cast<T_Elem>(::sin(rad));
+	AutoPtr<ArrayT<T_Elem> > pArrayT(new ArrayT<T_Elem>(sizeMat, sizeMat));
+	T_Elem *p = pArrayT->GetPointer();
+	// row-1
+	*p++ = numCos;
+	*p++ = -numSin;
+	*p++ = 0;
+	if (transFlag) {
+		*p++ = static_cast<T_Elem>(xTrans);
+	}
+	// row-2
+	*p++ = numSin;
+	*p++ = numCos;
+	*p++ = 0;
+	if (transFlag) {
+		*p++ = static_cast<T_Elem>(yTrans);
+	}
+	// row-3
+	*p++ = 0;
+	*p++ = 0;
+	*p++ = 1;
+	if (transFlag) {
+		*p++ = static_cast<T_Elem>(zTrans);
+		// row-4
+		*p++ = 0;
+		*p++ = 0;
+		*p++ = 0;
+		*p++ = 1;
+	}
+	return pArrayT.release();
+}
+
 Gura_ImplementClassMethod(array, rotation_at_z)
 {
+	typedef Array *(*FuncT)(Double rad, bool transFlag, Double xTrans, Double yTrans, Double zTrans);
+	DeclareFunctionTable1D(FuncT, funcTbl, FuncTmpl_rotation_at_z);
 	Array::ElemType elemType = arg.IsValid(4)?
 		Array::SymbolToElemTypeWithError(env, arg.GetSymbol(4)) : Array::ETYPE_Double;
 	if (env.IsSignalled()) return Value::Nil;
+	FuncT func = funcTbl[elemType];
+	if (func == nullptr) {
+		SetError_CreationError(env, elemType);
+		return Value::Nil;
+	}
 	double angle = arg.GetDouble(0);
 	if (arg.IsSet(Gura_Symbol(deg))) angle = DegToRad(angle);
 	AutoPtr<Array> pArray;
@@ -401,12 +676,11 @@ Gura_ImplementClassMethod(array, rotation_at_z)
 		Double xTrans = arg.IsValid(1)? static_cast<Double>(arg.GetDouble(1)) : 0;
 		Double yTrans = arg.IsValid(2)? static_cast<Double>(arg.GetDouble(2)) : 0;
 		Double zTrans = arg.IsValid(3)? static_cast<Double>(arg.GetDouble(3)) : 0;
-		pArray.reset(ArrayT<Double>::CreateRotationZ(angle, true, xTrans, yTrans, zTrans));
+		pArray.reset((*func)(angle, true, xTrans, yTrans, zTrans));
 	} else {
-		pArray.reset(ArrayT<Double>::CreateRotationZ(angle, false, 0, 0, 0));
+		pArray.reset((*func)(angle, false, 0, 0, 0));
 	}
-	Value value(new Object_array(env, pArray.release()));
-	return ReturnValue(env, arg, value);
+	return ReturnValue(env, arg, Value(new Object_array(env, pArray.release())));
 }
 
 // array.scaling(xscale:number, yscale:number, zscale?:number, elemtype?:symbol):static:map {block?}
--- src/module-arrayutil/module-arrayutil.h
+++ src/module-arrayutil/module-arrayutil.h
@@ -8,6 +8,25 @@
 #include "Creator.h"
 #include "Method.h"
 
+#define DeclareFunctionTable1D(FuncT, funcTbl, FuncTmpl) \
+const static FuncT funcTbl[Array::ETYPE_Max] = { \
+	nullptr, \
+	&FuncTmpl<Boolean>, \
+	&FuncTmpl<Int8>, \
+	&FuncTmpl<UInt8>, \
+	&FuncTmpl<Int16>, \
+	&FuncTmpl<UInt16>, \
+	&FuncTmpl<Int32>, \
+	&FuncTmpl<UInt32>, \
+	&FuncTmpl<Int64>, \
+	&FuncTmpl<UInt64>, \
+	&FuncTmpl<Half>, \
+	&FuncTmpl<Float>, \
+	&FuncTmpl<Double>, \
+	&FuncTmpl<Complex>, \
+	nullptr, \
+}
+
 Gura_BeginModuleHeader(arrayutil)
 Gura_EndModuleHeader(arrayutil)
 
