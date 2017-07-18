997840ca67b141151ffe0dc0984a1bb31f27f456
ypsitau <ypsitau@nifty.com>
2017-07-18 22:46:55 +0900

    implementing ceres module

--- src/module-ceres/module-ceres.cpp
+++ src/module-ceres/module-ceres.cpp
@@ -5,22 +5,8 @@
 #include "ceres/ceres.h"
 #include "glog/logging.h"
 
-using ceres::AutoDiffCostFunction;
-using ceres::CostFunction;
-using ceres::Problem;
-using ceres::Solver;
-using ceres::Solve;
-
-// A templated cost functor that implements the residual r = 10 -
-// x. The method operator() is templated so that we can then use an
-// automatic differentiation wrapper around it to generate its
-// derivatives.
-struct CostFunctor {
-  template <typename T> bool operator()(const T* const x, T* residual) const {
-    residual[0] = T(10.0) - x[0];
-    return true;
-  }
-};
+int helloworld();
+int curve_fitting();
 
 Gura_BeginModuleBody(ceres)
 
@@ -38,31 +24,8 @@ Gura_DeclareFunction(test)
 
 Gura_ImplementFunction(test)
 {
-	google::InitGoogleLogging("hello");
-
-	// The variable to solve for with its initial value. It will be
-	// mutated in place by the solver.
-	double x = 0.5;
-	const double initial_x = x;
-	
-	// Build the problem.
-	Problem problem;
-	
-	// Set up the only cost function (also known as residual). This uses
-	// auto-differentiation to obtain the derivative (jacobian).
-	CostFunction* cost_function =
-		new AutoDiffCostFunction<CostFunctor, 1, 1>(new CostFunctor);
-	problem.AddResidualBlock(cost_function, NULL, &x);
-	
-	// Run the solver!
-	Solver::Options options;
-	options.minimizer_progress_to_stdout = true;
-	Solver::Summary summary;
-	Solve(options, &problem, &summary);
-	
-	std::cout << summary.BriefReport() << "\n";
-	std::cout << "x : " << initial_x
-			  << " -> " << x << "\n";
+	//curve_fitting();
+	helloworld();
 	return Value::Nil;
 }
 
