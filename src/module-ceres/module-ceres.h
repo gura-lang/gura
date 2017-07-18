ff6832907c15626e7784b41c9f3c55dcc35622ca
ypsitau <ypsitau@nifty.com>
2017-07-18 12:38:27 +0900

    added skeleton for ceres module

---
+++ src/module-ceres/module-ceres.h
@@ -0,0 +1,11 @@
+//=============================================================================
+// Gura module: ceres
+//=============================================================================
+#ifndef __GURA_MODULE_CERES_H__
+#define __GURA_MODULE_CERES_H__
+#include <gura.h>
+
+Gura_BeginModuleHeader(ceres)
+Gura_EndModuleHeader(ceres)
+
+#endif
