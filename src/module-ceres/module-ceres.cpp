0f9c85e39d6fffdb3d7a6b98caba0fc082c93856
ypsitau <ypsitau@nifty.com>
2017-07-18 22:37:39 +0900

    implementing ceres module

--- src/module-ceres/module-ceres.cpp
+++ src/module-ceres/module-ceres.cpp
@@ -2,26 +2,68 @@
 // Gura module: ceres
 //=============================================================================
 #include "stdafx.h"
+#include "ceres/ceres.h"
+#include "glog/logging.h"
+
+using ceres::AutoDiffCostFunction;
+using ceres::CostFunction;
+using ceres::Problem;
+using ceres::Solver;
+using ceres::Solve;
+
+// A templated cost functor that implements the residual r = 10 -
+// x. The method operator() is templated so that we can then use an
+// automatic differentiation wrapper around it to generate its
+// derivatives.
+struct CostFunctor {
+  template <typename T> bool operator()(const T* const x, T* residual) const {
+    residual[0] = T(10.0) - x[0];
+    return true;
+  }
+};
 
 Gura_BeginModuleBody(ceres)
 
 //-----------------------------------------------------------------------------
 // Module functions
 //-----------------------------------------------------------------------------
-// ceres.test(num1:number, num2:number)
+// ceres.test()
 Gura_DeclareFunction(test)
 {
 	SetFuncAttr(VTYPE_any, RSLTMODE_Normal, FLAG_None);
-	DeclareArg(env, "num1", VTYPE_number);
-	DeclareArg(env, "num2", VTYPE_number);
 	AddHelp(
 		Gura_Symbol(en),
-		"This function adds two numbers and returns the result.\n");
+		"");
 }
 
 Gura_ImplementFunction(test)
 {
-	return Value(arg.GetNumber(0) + arg.GetNumber(1));
+	google::InitGoogleLogging("hello");
+
+	// The variable to solve for with its initial value. It will be
+	// mutated in place by the solver.
+	double x = 0.5;
+	const double initial_x = x;
+	
+	// Build the problem.
+	Problem problem;
+	
+	// Set up the only cost function (also known as residual). This uses
+	// auto-differentiation to obtain the derivative (jacobian).
+	CostFunction* cost_function =
+		new AutoDiffCostFunction<CostFunctor, 1, 1>(new CostFunctor);
+	problem.AddResidualBlock(cost_function, NULL, &x);
+	
+	// Run the solver!
+	Solver::Options options;
+	options.minimizer_progress_to_stdout = true;
+	Solver::Summary summary;
+	Solve(options, &problem, &summary);
+	
+	std::cout << summary.BriefReport() << "\n";
+	std::cout << "x : " << initial_x
+			  << " -> " << x << "\n";
+	return Value::Nil;
 }
 
 //-----------------------------------------------------------------------------
