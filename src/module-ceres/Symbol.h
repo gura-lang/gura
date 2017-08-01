21959035c5cea2a367de3c4b5eeead48fed12923
ypsitau <ypsitau@nifty.com>
2017-08-01 11:37:14 +0900

    implementing ceres module

---
+++ src/module-ceres/Symbol.h
@@ -0,0 +1,14 @@
+//=============================================================================
+// Symbol.h
+//=============================================================================
+#ifndef __GURA_MODULE_CERES_SYMBOL_H__
+#define __GURA_MODULE_CERES_SYMBOL_H__
+#include <gura.h>
+
+Gura_BeginModuleHeader(ceres)
+
+Gura_DeclareUserSymbol(Evaluate);
+
+Gura_EndModuleHeader(ceres)
+
+#endif
