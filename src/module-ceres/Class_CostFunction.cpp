2327f5a26cedb0693ed71cc21e1d4c987146e7ec
ypsitau <ypsitau@nifty.com>
2017-08-02 11:16:14 +0900

    implementing ceres module

--- src/module-ceres/Class_CostFunction.cpp
+++ src/module-ceres/Class_CostFunction.cpp
@@ -106,7 +106,7 @@ bool CostFunctionCustom::Evaluate(double const *const *parameters,
 	}
 	ValueList::const_iterator pValue = valList.begin();
 	do {
-		const Array *pArray_residuals = Object_array::GetObject(valList[0])->GetArray();
+		const Array *pArray_residuals = Object_array::GetObject(*pValue)->GetArray();
 		if (pArray_residuals->GetElemType() != Array::ETYPE_Double) {
 			env.SetError(ERR_ValueError, "type of elements in the returned array must be double");
 			return false;
@@ -117,28 +117,22 @@ bool CostFunctionCustom::Evaluate(double const *const *parameters,
 			return false;
 		}
 		::memcpy(residuals, pArrayT_residuals->GetPointer(), sizeof(Double) * pArrayT_residuals->GetElemNum());
+		pValue++;
 	} while (0);
 	if (jacobians != nullptr) {
-		const Array *pArray_jacobians = Object_array::GetObject(valList[1])->GetArray();
-		if (pArray_jacobians->GetElemType() != Array::ETYPE_Double) {
-			env.SetError(ERR_ValueError, "type of elements in the returned array must be double");
-			return false;
-		}
-		const ArrayT<Double> *pArrayT_jacobians = dynamic_cast<const ArrayT<Double> *>(pArray_jacobians);
-		const Array::Dimensions &dims = pArrayT_jacobians->GetDimensions();
-		if (dims.size() != 2) {
-			env.SetError(ERR_ValueError, "returned array must be two-dimension");
-			return false;
-		}
 		size_t i = 0;
-		const double *pElem = pArrayT_jacobians->GetPointer();
 		foreach_const (std::vector<ceres::int32>, pSize, parameter_block_sizes()) {
-			if (i >= dims[0].GetSize()) break;
-			ceres::int32 size = *pSize;
 			double *jacobian = jacobians[i++];
-			size_t sizeMin = ChooseMin(dims[1].GetSize(), static_cast<size_t>(size));
-			::memcpy(jacobian, pElem, sizeMin * sizeof(double));
-			pElem += dims[0].GetStride();
+			if (pValue == valList.end()) break;
+			const Array *pArray_jacobian = Object_array::GetObject(*pValue)->GetArray();
+			if (pArray_jacobian->GetElemType() != Array::ETYPE_Double) {
+				env.SetError(ERR_ValueError, "type of elements in the returned array must be double");
+				return false;
+			}
+			const ArrayT<Double> *pArrayT_jacobian = dynamic_cast<const ArrayT<Double> *>(pArray_jacobian);
+			size_t sizeMin = ChooseMin(pArrayT_jacobian->GetElemNum(), static_cast<size_t>(*pSize));
+			::memcpy(jacobian, pArrayT_jacobian->GetPointer(), sizeMin * sizeof(double));
+			pValue++;
 		}
 	}
 	return true;
