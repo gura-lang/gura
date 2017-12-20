41f9a6c1dce5ecbe010e9cafe5f7612d6815532c
ypsitau <ypsitau@nifty.com>
2017-12-20 21:45:15 +0900

    implementing gear@conv3d

--- src/lib/Class_gear_at_conv3d.cpp
+++ src/lib/Class_gear_at_conv3d.cpp
@@ -112,7 +112,29 @@ Gura_DeclareFunctionAlias(gear_at_conv3d, "gear@conv3d")
 	SetClassToConstruct(env.LookupClass(VTYPE_gear_at_conv3d));
 	AddHelp(
 		Gura_Symbol(en),
-		"Creates a `gear@conv3d` instance.\n");
+		"Creates a `gear@conv3d` instance.\n"
+		"\n"
+		"The given `array` instance shoud have one of the following shapes:\n"
+		"\n"
+		"- `[plane_size, row_size, col_size]` .. 3-dimensions\n"
+		"- `[plane_size, row_size, col_size, channel_num]` .. 4-dimensions and `channel_pos` is `` `last``\n"
+		"- `[channel_num, plane_size, row_size, col_size]` .. 4-dimensions and `channel_pos` is `` `first``\n"
+		"- `[filter_num, plane_size, row_size, col_size]` .. 4-dimensions and `channel_pos` is `` `none``\n"
+		"- `[filter_num, plane_size, row_size, col_size, channel_num]` .. 5-dimensions and `channel_pos` is `` `last``\n"
+		"- `[filter_num, channel_num, plane_size, row_size, col_size]` .. 5-dimensions and `channel_pos` is `` `first``\n"
+		"\n"
+		"where `plane_size`, `row_size` and `col_size` are the size of the filter's kernel,\n"
+		"`channel_num` is the number of channels and `filter_num` is the number of filters.\n"
+		"\n"
+		"The `strides` is a list of strides for a sliding window in plane, row and column. Default is `[1, 1, 1]`.\n"
+		"\n"
+		"The `padding` is a padding style and takes `` `valid`` or `` `same``. Default is `` `same``.\n"
+		"When `valid` is specified, there is no padding. When `same` is specified, zero values are padded so that\n"
+		"the result array has the size of the division of the original size by `strides`.\n"
+		"\n"
+		"The `channel_pos` is a channel position and takes `` `none``, `` `first`` or `` `last``.\n"
+		"If not specified, `` `none` for an array without channel dimension\n"
+		"and `` `last`` for others are to be set.\n");
 }
 
 Gura_ImplementFunction(gear_at_conv3d)
@@ -158,6 +180,115 @@ Gura_ImplementFunction(gear_at_conv3d)
 }
 
 //-----------------------------------------------------------------------------
+// Implementation of properties
+//-----------------------------------------------------------------------------
+// gear@conv3d#array
+Gura_DeclareProperty_R(gear_at_conv3d, array)
+{
+	SetPropAttr(VTYPE_array);
+	AddHelp(
+		Gura_Symbol(en),
+		""
+		);
+}
+
+Gura_ImplementPropertyGetter(gear_at_conv3d, array)
+{
+	const Gear_Conv3d *pGear = Object_gear_at_conv3d::GetObject(valueThis)->GetGear();
+	return Value(new Object_array(env, pGear->GetArrayGear()->Reference()));
+}
+
+// gear@conv3d#channel_num
+Gura_DeclareProperty_R(gear_at_conv3d, channel_num)
+{
+	SetPropAttr(VTYPE_number, FLAG_Nil);
+	AddHelp(
+		Gura_Symbol(en),
+		"");
+}
+
+Gura_ImplementPropertyGetter(gear_at_conv3d, channel_num)
+{
+	const Gear_Conv3d *pGear = Object_gear_at_conv3d::GetObject(valueThis)->GetGear();
+	return pGear->HasChannelDim()? Value(pGear->GetChannelNum()) : Value::Nil;
+}
+
+// gear@conv3d#channel_pos
+Gura_DeclareProperty_R(gear_at_conv3d, channel_pos)
+{
+	SetPropAttr(VTYPE_symbol);
+	AddHelp(
+		Gura_Symbol(en),
+		"");
+}
+
+Gura_ImplementPropertyGetter(gear_at_conv3d, channel_pos)
+{
+	const Gear_Conv3d *pGear = Object_gear_at_conv3d::GetObject(valueThis)->GetGear();
+	return Value(Array::ChannelPosToSymbol(pGear->GetChannelPos()));
+}
+
+// gear@conv1d#filter_num
+Gura_DeclareProperty_R(gear_at_conv3d, filter_num)
+{
+	SetPropAttr(VTYPE_number, FLAG_Nil);
+	AddHelp(
+		Gura_Symbol(en),
+		"");
+}
+
+Gura_ImplementPropertyGetter(gear_at_conv3d, filter_num)
+{
+	const Gear_Conv3d *pGear = Object_gear_at_conv3d::GetObject(valueThis)->GetGear();
+	return pGear->HasFilterDim()? Value(pGear->GetFilterNum()) : Value::Nil;
+}
+
+// gear@conv3d#padding
+Gura_DeclareProperty_R(gear_at_conv3d, padding)
+{
+	SetPropAttr(VTYPE_symbol);
+	AddHelp(
+		Gura_Symbol(en),
+		"");
+}
+
+Gura_ImplementPropertyGetter(gear_at_conv3d, padding)
+{
+	const Gear_Conv3d *pGear = Object_gear_at_conv3d::GetObject(valueThis)->GetGear();
+	return Value(Gear::PaddingTypeToSymbol(pGear->GetPaddingType()));
+}
+
+// gear@conv3d#size
+Gura_DeclareProperty_R(gear_at_conv3d, size)
+{
+	SetPropAttr(VTYPE_number, FLAG_ListVar);
+	AddHelp(
+		Gura_Symbol(en),
+		"");
+}
+
+Gura_ImplementPropertyGetter(gear_at_conv3d, size)
+{
+	const Gear_Conv3d *pGear = Object_gear_at_conv3d::GetObject(valueThis)->GetGear();
+	return Value::CreateList(env, Value(pGear->GetSizePlane()), Value(pGear->GetSizeRow()), Value(pGear->GetSizeCol()));
+}
+
+// gear@conv3d#strides
+Gura_DeclareProperty_R(gear_at_conv3d, strides)
+{
+	SetPropAttr(VTYPE_number, FLAG_ListVar);
+	AddHelp(
+		Gura_Symbol(en),
+		"");
+}
+
+Gura_ImplementPropertyGetter(gear_at_conv3d, strides)
+{
+	const Gear_Conv3d *pGear = Object_gear_at_conv3d::GetObject(valueThis)->GetGear();
+	return Value::CreateList(env, Value(pGear->GetStridesRow()), Value(pGear->GetStridesCol()));
+}
+
+//-----------------------------------------------------------------------------
 // Implementation of class
 //-----------------------------------------------------------------------------
 Class_gear_at_conv3d::Class_gear_at_conv3d(Environment *pEnvOuter) :
@@ -169,6 +300,14 @@ void Class_gear_at_conv3d::DoPrepare(Environment &env)
 {
 	// Assignment of function
 	Gura_AssignFunction(gear_at_conv3d);
+	// Assignment of properties
+	Gura_AssignProperty(gear_at_conv3d, array);
+	Gura_AssignProperty(gear_at_conv3d, channel_num);
+	Gura_AssignProperty(gear_at_conv3d, channel_pos);
+	Gura_AssignProperty(gear_at_conv3d, filter_num);
+	Gura_AssignProperty(gear_at_conv3d, padding);
+	Gura_AssignProperty(gear_at_conv3d, size);
+	Gura_AssignProperty(gear_at_conv3d, strides);
 	// Assignment of NodeGear creator for Trainer
 	Trainer::RegisterNodeGearCreator(VTYPE_gear_at_conv3d, new NodeGear_Conv3d::CreatorEx());
 	// help document
