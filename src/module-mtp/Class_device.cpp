2d780dc46f4361c65dcbfada2299fe1dfaad816d
ypsitau <ypsitau@nifty.com>
2018-06-15 20:37:11 +0900

    implementing mtp module

--- src/module-mtp/Class_device.cpp
+++ src/module-mtp/Class_device.cpp
@@ -96,29 +96,43 @@ void Device::LookupStorages(Object_list *pObjList) const
 
 Directory *Device::GenerateDirectory(Signal &sig, uint32_t storageId, const char *pathName) const
 {
+	Directory *pParent = nullptr;
 #if 0
 	uint32_t itemIdParent = LIBMTP_FILES_AND_FOLDERS_ROOT;
-	LIBMTP_file_t *fileInfo = ::LIBMTP_Get_Files_And_Folders(_mtpDevice, storageId, itemIdParent);
-	if (fileInfo == nullptr) {
-		::LIBMTP_Dump_Errorstack(_mtpDevice);
-		::LIBMTP_Clear_Errorstack(_mtpDevice);
-		sig.SetError(ERR_LibraryError, "failed to get file information from MTP");
-		return nullptr;
-	}	
-	while (fileInfo != nullptr) {
-		if (fileInfo->filetype == LIBMTP_FILETYPE_FOLDER) {
-			printf("[%s]\n", fileInfo->filename);
-			//ListFiles(mtpDevice, storageId, fileInfo->item_id, indentLevel + 1);
-		} else {
-			printf("%s\n", fileInfo->filename);
+	const char *p = pathName;
+	if (IsFileSeparator(*p)) p++;
+	String field;
+	for ( ; ; p++) {
+		char ch = *p;
+		if (!IsFileSeparator(ch) && ch != '\0') {
+			field += ch;
+			continue;
 		}
-		LIBMTP_file_t *fileInfoNext = fileInfo->next;
-		::LIBMTP_destroy_file_t(fileInfo);
-		fileInfo = fileInfoNext;
+		if (!field.empty()) {
+			LIBMTP_file_t *fileInfo = ::LIBMTP_Get_Files_And_Folders(_mtpDevice, storageId, itemIdParent);
+			if (fileInfo == nullptr) {
+				::LIBMTP_Dump_Errorstack(_mtpDevice);
+				::LIBMTP_Clear_Errorstack(_mtpDevice);
+				sig.SetError(ERR_LibraryError, "failed to get file information from MTP");
+				return nullptr;
+			}	
+			while (fileInfo != nullptr) {
+				if (fileInfo->filetype == LIBMTP_FILETYPE_FOLDER) {
+					printf("[%s]\n", fileInfo->filename);
+					//ListFiles(mtpDevice, storageId, fileInfo->item_id, indentLevel + 1);
+				} else {
+					//printf("%s\n", fileInfo->filename);
+				}
+				LIBMTP_file_t *fileInfoNext = fileInfo->next;
+				::LIBMTP_destroy_file_t(fileInfo);
+				fileInfo = fileInfoNext;
+			}
+		}
+		if (ch == '\0') break;
 	}
 #endif
 	return new Directory_MTP(
-		nullptr, "/", Directory::TYPE_Container,
+		pParent, "/", Directory::TYPE_Container,
 		Reference(), storageId, LIBMTP_FILES_AND_FOLDERS_ROOT,
 		new Stat("", 0, DateTime(), LIBMTP_FILETYPE_FOLDER));
 }
