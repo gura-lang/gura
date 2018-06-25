9c03650df51d8eb0364fd6e96b53797420b5f91f
ypsitau <ypsitau@nifty.com>
2018-06-25 12:17:13 +0900

    updating prepare-for-mswin for vs2017

--- guest/prepare-for-mswin.bat
+++ guest/prepare-for-mswin.bat
@@ -1,37 +1,78 @@
 @echo off
+rem ---------------------------------------------------------------------------
+rem Main entry
+rem ---------------------------------------------------------------------------
+:main
 set GUESTURL=http://www.gura-lang.org/guest
 set BASEDIR=%~dp0
 set UNZIP="%BASEDIR%buildtools-mswin\7za920\7za.exe"
 set GNUMAKE="%BASEDIR%buildtools-mswin\UnxUtils\make.exe"
 set CURL="%BASEDIR%buildtools-mswin\curl\curl.exe"
 set FAILEDLIST=
-rem ---------------------------------------------------------------------------
-set VCVERSION=14.0
-set VCVARSALL="C:\Program Files\Microsoft Visual Studio %VCVERSION%\VC\vcvarsall.bat"
-if not exist %VCVARSALL% set VCVARSALL="D:\Program Files\Microsoft Visual Studio %VCVERSION%\VC\vcvarsall.bat"
-if not exist %VCVARSALL% set VCVARSALL="E:\Program Files\Microsoft Visual Studio %VCVERSION%\VC\vcvarsall.bat"
-if not exist %VCVARSALL% set VCVARSALL="F:\Program Files\Microsoft Visual Studio %VCVERSION%\VC\vcvarsall.bat"
-if not exist %VCVARSALL% set VCVARSALL="G:\Program Files\Microsoft Visual Studio %VCVERSION%\VC\vcvarsall.bat"
-if not exist %VCVARSALL% set VCVARSALL="C:\Program Files (x86)\Microsoft Visual Studio %VCVERSION%\VC\vcvarsall.bat"
-if not exist %VCVARSALL% set VCVARSALL="D:\Program Files (x86)\Microsoft Visual Studio %VCVERSION%\VC\vcvarsall.bat"
-if not exist %VCVARSALL% set VCVARSALL="E:\Program Files (x86)\Microsoft Visual Studio %VCVERSION%\VC\vcvarsall.bat"
-if not exist %VCVARSALL% set VCVARSALL="F:\Program Files (x86)\Microsoft Visual Studio %VCVERSION%\VC\vcvarsall.bat"
-if not exist %VCVARSALL% set VCVARSALL="G:\Program Files (x86)\Microsoft Visual Studio %VCVERSION%\VC\vcvarsall.bat"
-if not exist %VCVARSALL% goto err_vcvarsall_not_found
-call %VCVARSALL%
+set VCVERSION=2017
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\vsdevcmd.bat" -arch=x86
 rem Add include path containing Win32.mak in case vs2015 doesn't include SDK
 rem that provides the file.
 set INCLUDE=%BASEDIR%include;%INCLUDE%
+
+if not "%1" == "" (
+	call :build_%1
+	exit /b
+)
+
 rem ---------------------------------------------------------------------------
-rem goto skip_curl
+call :download_buildtools
+call :download_packages
+
+call :build_eigen
+call :build_sqlite
+call :build_zlib
+call :build_bzip2
+call :build_jpegsrc
+call :build_lpng
+call :build_tiff
+call :build_yaml
+call :build_onig
+call :build_expat
+call :build_tcltk
+call :build_wx
+call :build_pixman
+call :build_freeglut
+call :build_glew
+call :build_freetype
+call :build_cairo
+call :build_sdl
+call :build_sdl2
+call :build_curl
+call :build_mpir
+call :build_fftw
+call :build_liblinear
+call :build_libsvm
+
+if not "%FAILEDLIST%" == "" (
+	echo ======================================================================
+	echo Error occured:%FAILEDLIST%
+	echo ======================================================================
+)
+pause
+exit /b
+
 rem ---------------------------------------------------------------------------
+rem Download buildtools
+rem ---------------------------------------------------------------------------
+:download_buildtools
 if not exist buildtools-mswin git clone https://github.com/gura-lang/buildtools-mswin.git
 %UNZIP% x -y -obuildtools-mswin\curl buildtools-mswin\curl_737_1.zip
 %CURL% %GUESTURL%/buildtools-mswin/UnxUpdates.zip -o buildtools-mswin\UnxUpdates.zip
 %CURL% %GUESTURL%/buildtools-mswin/wix38-binaries.zip -o buildtools-mswin\wix38-binaries.zip
 %UNZIP% x -y -obuildtools-mswin\UnxUtils buildtools-mswin\UnxUpdates.zip
 %UNZIP% x -y -obuildtools-mswin\wix38-binaries buildtools-mswin\wix38-binaries.zip
+exit /b
+
 rem ---------------------------------------------------------------------------
+rem Download packages
+rem ---------------------------------------------------------------------------
+:download_packages
 %CURL% -O %GUESTURL%/bzip2-1.0.6.tar.gz
 %CURL% -O %GUESTURL%/cairo-1.12.18.tar.xz
 %CURL% -O %GUESTURL%/cairo-1.12.18-gurapatch.zip
@@ -72,20 +113,14 @@ rem ---------------------------------------------------------------------------
 %CURL% -O %GUESTURL%/yaml-0.1.5.tar.gz
 %CURL% -O %GUESTURL%/yaml-0.1.5-gurapatch-vs2015.zip
 %CURL% -O %GUESTURL%/zlib127.zip
+exit /b
+
 rem ---------------------------------------------------------------------------
-:skip_curl
+rem Build packages
 rem ---------------------------------------------------------------------------
-%UNZIP% x -y eigen-eigen-5a0156e40feb.zip
-move eigen-eigen-5a0156e40feb/Eigen include/Eigen
+
 rem ---------------------------------------------------------------------------
-%UNZIP% x -y -osqlite-amalgamation sqlite-amalgamation-201409011821.zip
-rem ---------------------------------------------------------------------------
-%UNZIP% x -y zlib127.zip
-pushd zlib-1.2.7
-nmake -f win32\Makefile.msc
-if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% zlib
-popd
-rem ---------------------------------------------------------------------------
+:build_bzip2
 %UNZIP% x -y bzip2-1.0.6.tar.gz
 %UNZIP% x -y bzip2-1.0.6.tar
 del bzip2-1.0.6.tar
@@ -93,7 +128,91 @@ pushd bzip2-1.0.6
 nmake -f makefile.msc
 if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% bzip2
 popd
+exit /b
+
 rem ---------------------------------------------------------------------------
+:build_cairo
+%UNZIP% x -y cairo-1.12.18.tar.xz
+%UNZIP% x -y cairo-1.12.18.tar
+%UNZIP% x -y cairo-1.12.18-gurapatch.zip
+del cairo-1.12.18.tar
+pushd cairo-1.12.18\src
+%GNUMAKE% -f Makefile.win32 CFG=release
+if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% cairo
+popd
+exit /b
+
+rem ---------------------------------------------------------------------------
+:build_curl
+mkdir deps
+mkdir deps\lib
+mkdir deps\include
+mkdir deps\bin
+copy zlib-1.2.7\*.h deps\include
+copy zlib-1.2.7\zlib.lib deps\lib\zlib_a.lib
+%UNZIP% x -y curl-7.38.0.zip
+pushd curl-7.38.0\winbuild
+nmake -f Makefile.vc mode=static WITH_ZLIB=static
+if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% curl
+popd
+exit /b
+
+rem ---------------------------------------------------------------------------
+:build_eigen
+%UNZIP% x -y eigen-eigen-5a0156e40feb.zip
+move eigen-eigen-5a0156e40feb/Eigen include/Eigen
+exit /b
+
+rem ---------------------------------------------------------------------------
+:build_expat
+%UNZIP% x -y expat-2.1.0.tar.gz
+%UNZIP% x -y expat-2.1.0.tar
+%UNZIP% x -y expat-2.1.0-gurapatch-vs2015.zip
+del expat-2.1.0.tar
+msbuild expat-2.1.0\lib\expat_static.vcxproj /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=win32
+if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% expat
+exit /b
+
+rem ---------------------------------------------------------------------------
+:build_fftw
+%UNZIP% x -y fftw-3.3.6-pl2.tar.gz
+%UNZIP% x -y fftw-3.3.6-pl2.tar
+%UNZIP% x -y fftw-3.3.6-pl2-gurapatch-vs2015.zip
+del fftw-3.3.6-pl2.tar
+msbuild fftw-3.3.6-pl2\msw\fftw-3.3-libs.sln /clp:DisableConsoleColor /t:Build /p:Configuration="Static-Release" /p:Platform=win32
+if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% fftw
+exit /b
+
+rem ---------------------------------------------------------------------------
+:build_freeglut
+%UNZIP% x -y freeglut-2.8.1.tar.gz
+%UNZIP% x -y freeglut-2.8.1.tar
+%UNZIP% x -y freeglut-2.8.1-gurapatch.zip
+del freeglut-2.8.1.tar
+msbuild freeglut-2.8.1\VisualStudio\2015\freeglut.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Release_Static /p:Platform=win32
+if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% freeglut
+exit /b
+
+rem ---------------------------------------------------------------------------
+:build_freetype
+%UNZIP% x -y freetype-2.5.3.tar.bz2
+%UNZIP% x -y freetype-2.5.3.tar
+%UNZIP% x -y freetype-2.5.3-gurapatch.zip
+del freetype-2.5.3.tar
+msbuild freetype-2.5.3\builds\windows\vc2015\freetype.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=win32
+if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% freetype
+exit /b
+
+rem ---------------------------------------------------------------------------
+:build_glew
+%UNZIP% x -y glew-1.13.0.zip
+%UNZIP% x -y glew-1.13.0-gurapatch.zip
+msbuild glew-1.13.0\build\vc14\glew.sln /clp:DisableConsoleColor /t:Build /p:Configuration="Release" /p:Platform=win32
+if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% glew
+exit /b
+
+rem ---------------------------------------------------------------------------
+:build_jpegsrc
 %UNZIP% x -y jpegsrc.v9a.tar.gz
 %UNZIP% x -y jpegsrc.v9a.tar
 del jpegsrc.v9a.tar
@@ -102,28 +221,54 @@ copy jconfig.vc jconfig.h
 nmake -f makefile.vc nodebug=1
 if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% jpegsrc
 popd
+exit /b
+
 rem ---------------------------------------------------------------------------
+:build_liblinear
+%UNZIP% x -y liblinear-2.11.zip
+%UNZIP% x -y liblinear-2.11-gurapatch.zip
+pushd liblinear-2.11
+nmake -f Makefile.win clean all
+if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% liblinear
+popd
+copy liblinear-2.11\windows\liblinear.dll dylib
+exit /b
+
+rem ---------------------------------------------------------------------------
+:build_libsvm
+%UNZIP% x -y libsvm-3.22.zip
+%UNZIP% x -y libsvm-3.22-gurapatch.zip
+pushd libsvm-3.22
+nmake -f Makefile.win clean all
+if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% libsvm
+popd
+copy libsvm-3.22\windows\libsvm.dll dylib
+exit /b
+
+rem ---------------------------------------------------------------------------
+:build_lpng
 rem You cannot build source code in libpng-x.x.x.tar.gz properly under Windows.
 %UNZIP% x -y lpng1520.zip
 %UNZIP% x -y lpng1520-gurapatch-vs2015.zip
 msbuild lpng1520\projects\vstudio\vstudio.sln /clp:DisableConsoleColor /t:Build /p:Configuration="Release Library" /p:Platform=win32
 if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% lpng
+exit /b
+
 rem ---------------------------------------------------------------------------
-rem You cannot build source code in tiff-3.8.2.tar.gz properly under Windows.
-%UNZIP% x -y tiff-3.8.2.zip
-%UNZIP% x -y tiff-3.8.2-gurapatch.zip
-pushd tiff-3.8.2
-nmake -f Makefile.vc lib
-if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% tiff
-popd
+:build_mpir
+%UNZIP% x -y mpir-2.7.2.tar.bz2
+%UNZIP% x -y mpir-2.7.2.tar
+del mpir-2.7.2.tar
+msbuild mpir-2.7.2\build.vc14\lib_mpir_gc\lib_mpir_gc.vcxproj /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=win32
+if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% mpir_gc
+msbuild mpir-2.7.2\build.vc14\lib_mpir_cxx\lib_mpir_cxx.vcxproj /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=win32
+if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% mpir_cxx
+rem copy mpir-2.7.2\build.vc14\lib_mpir_gc\win32\Release\mpir.lib mpir-2.7.2\lib\win32\Release
+rem copy mpir-2.7.2\build.vc14\lib_mpir_cxx\win32\Release\mpirxx.lib mpir-2.7.2\lib\win32\Release
+exit /b
+
 rem ---------------------------------------------------------------------------
-%UNZIP% x -y yaml-0.1.5.tar.gz
-%UNZIP% x -y yaml-0.1.5.tar
-%UNZIP% x -y yaml-0.1.5-gurapatch-vs2015.zip
-del yaml-0.1.5.tar
-msbuild yaml-0.1.5\win32\vs2015\yaml.vcxproj /clp:DisableConsoleColor /t:Build /p:Configuration="Release" /p:Platform=win32
-if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% yaml
-rem ---------------------------------------------------------------------------
+:build_onig
 %UNZIP% x -y onig-5.9.5.tar.gz
 %UNZIP% x -y onig-5.9.5.tar
 del onig-5.9.5.tar
@@ -133,14 +278,46 @@ copy win32\config.h config.h
 nmake
 if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% onig
 popd
+exit /b
+
 rem ---------------------------------------------------------------------------
-%UNZIP% x -y expat-2.1.0.tar.gz
-%UNZIP% x -y expat-2.1.0.tar
-%UNZIP% x -y expat-2.1.0-gurapatch-vs2015.zip
-del expat-2.1.0.tar
-msbuild expat-2.1.0\lib\expat_static.vcxproj /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=win32
-if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% expat
+:build_pixman
+%UNZIP% x -y pixman-0.32.6.tar.gz
+%UNZIP% x -y pixman-0.32.6.tar
+%UNZIP% x -y pixman-0.32.6-gurapatch.zip
+del pixman-0.32.6.tar
+pushd pixman-0.32.6\pixman
+if not exist release mkdir release
+%GNUMAKE% -f Makefile.win32 CFG=release
+if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% pixman
+popd
+exit /b
+
 rem ---------------------------------------------------------------------------
+:build_sdl
+%UNZIP% x -y SDL-1.2.15.zip
+%UNZIP% x -y SDL-1.2.15-gurapatch-vs2015.zip
+msbuild SDL-1.2.15\VisualC\SDL.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=win32
+if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% sdl
+copy SDL-1.2.15\VisualC\SDL\Release\SDL.dll dylib
+exit /b
+
+rem ---------------------------------------------------------------------------
+:build_sdl2
+%UNZIP% x -y SDL2-2.0.4.zip
+%UNZIP% x -y SDL2-2.0.4-gurapatch.zip
+msbuild SDL2-2.0.4\VisualC\SDL.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=win32
+if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% sdl2
+copy SDL2-2.0.4\VisualC\Win32\Release\SDL2.dll dylib
+exit /b
+
+rem ---------------------------------------------------------------------------
+:build_sqlite
+%UNZIP% x -y -osqlite-amalgamation sqlite-amalgamation-201409011821.zip
+exit /b
+
+rem ---------------------------------------------------------------------------
+:build_tcltk
 %UNZIP% x -y tcl8519-src.zip -otcl
 %UNZIP% x -y tk8519-src.zip -otcl
 pushd tcl\tcl8.5.19\win
@@ -155,115 +332,41 @@ if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% tk
 nmake -f makefile.vc install INSTALLDIR=..\..
 if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% tk
 popd
+exit /b
+
 rem ---------------------------------------------------------------------------
+:build_tiff
+rem You cannot build source code in tiff-3.8.2.tar.gz properly under Windows.
+%UNZIP% x -y tiff-3.8.2.zip
+%UNZIP% x -y tiff-3.8.2-gurapatch.zip
+pushd tiff-3.8.2
+nmake -f Makefile.vc lib
+if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% tiff
+popd
+exit /b
+
+rem ---------------------------------------------------------------------------
+:build_wx
 rem Building wxWidgets library using /m option doesn't produce correct results.
 %UNZIP% x -y -owxWidgets-3.1.0 wxWidgets-3.1.0.7z
 msbuild wxWidgets-3.1.0\build\msw\wx_vc14.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=win32
-if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% wxWidgets
+if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% wx
+exit /b
+
 rem ---------------------------------------------------------------------------
-%UNZIP% x -y pixman-0.32.6.tar.gz
-%UNZIP% x -y pixman-0.32.6.tar
-%UNZIP% x -y pixman-0.32.6-gurapatch.zip
-del pixman-0.32.6.tar
-pushd pixman-0.32.6\pixman
-if not exist release mkdir release
-%GNUMAKE% -f Makefile.win32 CFG=release
-if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% pixman
-popd
+:build_yaml
+%UNZIP% x -y yaml-0.1.5.tar.gz
+%UNZIP% x -y yaml-0.1.5.tar
+%UNZIP% x -y yaml-0.1.5-gurapatch-vs2015.zip
+del yaml-0.1.5.tar
+msbuild yaml-0.1.5\win32\vs2015\yaml.vcxproj /clp:DisableConsoleColor /t:Build /p:Configuration="Release" /p:Platform=win32
+if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% yaml
+
 rem ---------------------------------------------------------------------------
-%UNZIP% x -y freeglut-2.8.1.tar.gz
-%UNZIP% x -y freeglut-2.8.1.tar
-%UNZIP% x -y freeglut-2.8.1-gurapatch.zip
-del freeglut-2.8.1.tar
-msbuild freeglut-2.8.1\VisualStudio\2015\freeglut.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Release_Static /p:Platform=win32
-if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% freeglut
-rem ---------------------------------------------------------------------------
-%UNZIP% x -y glew-1.13.0.zip
-%UNZIP% x -y glew-1.13.0-gurapatch.zip
-msbuild glew-1.13.0\build\vc14\glew.sln /clp:DisableConsoleColor /t:Build /p:Configuration="Release" /p:Platform=win32
-if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% glew
-rem ---------------------------------------------------------------------------
-%UNZIP% x -y freetype-2.5.3.tar.bz2
-%UNZIP% x -y freetype-2.5.3.tar
-%UNZIP% x -y freetype-2.5.3-gurapatch.zip
-del freetype-2.5.3.tar
-msbuild freetype-2.5.3\builds\windows\vc2015\freetype.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=win32
-if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% freetype
-rem ---------------------------------------------------------------------------
-%UNZIP% x -y cairo-1.12.18.tar.xz
-%UNZIP% x -y cairo-1.12.18.tar
-%UNZIP% x -y cairo-1.12.18-gurapatch.zip
-del cairo-1.12.18.tar
-pushd cairo-1.12.18\src
-%GNUMAKE% -f Makefile.win32 CFG=release
-if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% cairo
-popd
-rem ---------------------------------------------------------------------------
-%UNZIP% x -y SDL-1.2.15.zip
-%UNZIP% x -y SDL-1.2.15-gurapatch-vs2015.zip
-msbuild SDL-1.2.15\VisualC\SDL.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=win32
-if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% SDL
-copy SDL-1.2.15\VisualC\SDL\Release\SDL.dll dylib
-rem ---------------------------------------------------------------------------
-%UNZIP% x -y SDL2-2.0.4.zip
-%UNZIP% x -y SDL2-2.0.4-gurapatch.zip
-msbuild SDL2-2.0.4\VisualC\SDL.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=win32
-if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% SDL2
-copy SDL2-2.0.4\VisualC\Win32\Release\SDL2.dll dylib
-rem ---------------------------------------------------------------------------
-mkdir deps
-mkdir deps\lib
-mkdir deps\include
-mkdir deps\bin
-copy zlib-1.2.7\*.h deps\include
-copy zlib-1.2.7\zlib.lib deps\lib\zlib_a.lib
-%UNZIP% x -y curl-7.38.0.zip
-pushd curl-7.38.0\winbuild
-nmake -f Makefile.vc mode=static WITH_ZLIB=static
+:build_zlib
+%UNZIP% x -y zlib127.zip
+pushd zlib-1.2.7
+nmake -f win32\Makefile.msc
 if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% zlib
 popd
-rem ---------------------------------------------------------------------------
-%UNZIP% x -y mpir-2.7.2.tar.bz2
-%UNZIP% x -y mpir-2.7.2.tar
-del mpir-2.7.2.tar
-msbuild mpir-2.7.2\build.vc14\lib_mpir_gc\lib_mpir_gc.vcxproj /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=win32
-if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% mpir_gc
-msbuild mpir-2.7.2\build.vc14\lib_mpir_cxx\lib_mpir_cxx.vcxproj /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=win32
-if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% mpir_cxx
-rem copy mpir-2.7.2\build.vc14\lib_mpir_gc\win32\Release\mpir.lib mpir-2.7.2\lib\win32\Release
-rem copy mpir-2.7.2\build.vc14\lib_mpir_cxx\win32\Release\mpirxx.lib mpir-2.7.2\lib\win32\Release
-rem ---------------------------------------------------------------------------
-%UNZIP% x -y fftw-3.3.6-pl2.tar.gz
-%UNZIP% x -y fftw-3.3.6-pl2.tar
-%UNZIP% x -y fftw-3.3.6-pl2-gurapatch-vs2015.zip
-del fftw-3.3.6-pl2.tar
-msbuild fftw-3.3.6-pl2\msw\fftw-3.3-libs.sln /clp:DisableConsoleColor /t:Build /p:Configuration="Static-Release" /p:Platform=win32
-if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% fftw
-rem ---------------------------------------------------------------------------
-%UNZIP% x -y liblinear-2.11.zip
-%UNZIP% x -y liblinear-2.11-gurapatch.zip
-pushd liblinear-2.11
-nmake -f Makefile.win clean all
-if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% liblinear
-popd
-copy liblinear-2.11\windows\liblinear.dll dylib
-rem ---------------------------------------------------------------------------
-%UNZIP% x -y libsvm-3.22.zip
-%UNZIP% x -y libsvm-3.22-gurapatch.zip
-pushd libsvm-3.22
-nmake -f Makefile.win clean all
-if ERRORLEVEL 1 set FAILEDLIST=%FAILEDLIST% libsvm
-popd
-copy libsvm-3.22\windows\libsvm.dll dylib
-rem ---------------------------------------------------------------------------
-goto done
-:err_vcvarsall_not_found
-echo cannot find vcvarsall.bat. edit setup.bat and modify VCVARSALL variable.
-goto end
-:done
-if "%FAILEDLIST%"=="" goto end
-echo ======================================================================
-echo Error occured:%FAILEDLIST%
-echo ======================================================================
-:end
-pause
+exit /b
