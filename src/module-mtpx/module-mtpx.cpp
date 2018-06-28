701e56a0836cc721ab8a28c3ede60b19638db664
ypsitau <ypsitau@nifty.com>
2018-06-28 12:45:07 +0900

    implementing mtpx module

--- src/module-mtpx/module-mtpx.cpp
+++ src/module-mtpx/module-mtpx.cpp
@@ -5,6 +5,16 @@
 
 Gura_BeginModuleBody(mtpx)
 
+String BSTRToString(const OLECHAR *bstr)
+{
+	// cnt includes null-terminater
+	int cnt = ::WideCharToMultiByte(CP_UTF8, 0, bstr, -1, nullptr, 0, nullptr, nullptr);
+	char *psz = new char [cnt + 1];
+	::WideCharToMultiByte(CP_UTF8, 0, bstr, -1, psz, cnt, nullptr, nullptr);
+	psz[cnt] = '\0';
+	return String(psz);
+}
+
 //-----------------------------------------------------------------------------
 // Implementation of function
 //-----------------------------------------------------------------------------
@@ -20,23 +30,81 @@ Gura_DeclareFunction(test)
 Gura_ImplementFunction(test)
 {
 	HRESULT hr;
-	ComPtr<IPortableDeviceValues> pPortableDeviceValues;
-	ComPtr<IPortableDeviceManager> pPortableDeviceManager;
-	ComPtr<IPortableDevice> pPortableDevice = nullptr;
-	
-	hr = ::CoCreateInstance(CLSID_PortableDeviceValues,
-				nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pPortableDeviceValues));
-	if (FAILED(hr)) {
-		env.SetError(ERR_RuntimeError, "failed to get interface of PortableDeviceValues");
-		return Value::Nil;
-	}
-
-	hr = ::CoCreateInstance(CLSID_PortableDeviceManager,
-				nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pPortableDeviceManager));
-	if (FAILED(hr)) {
-		env.SetError(ERR_RuntimeError, "failed to get interface of PortableDeviceManager");
-		return Value::Nil;
-	}
+	//ComPtr<IPortableDevice> pPortableDevice;
+	do {
+		ComPtr<IPortableDeviceValues> pPortableDeviceValues;
+		hr = ::CoCreateInstance(CLSID_PortableDeviceValues,
+					nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pPortableDeviceValues));
+		if (FAILED(hr)) {
+			env.SetError(ERR_RuntimeError, "failed to get interface of PortableDeviceValues");
+			goto error_done;
+		}
+		hr = pPortableDeviceValues->SetStringValue(WPD_CLIENT_NAME, L"Gura mtp Module");
+		if (FAILED(hr)) goto error_done;
+		hr = pPortableDeviceValues->SetUnsignedIntegerValue(WPD_CLIENT_MAJOR_VERSION, GURA_VERSION_MAJOR);
+		if (FAILED(hr)) goto error_done;
+		hr = pPortableDeviceValues->SetUnsignedIntegerValue(WPD_CLIENT_MINOR_VERSION, GURA_VERSION_MINOR);
+		if (FAILED(hr)) goto error_done;
+		hr = pPortableDeviceValues->SetUnsignedIntegerValue(WPD_CLIENT_REVISION, GURA_VERSION_PATCH);
+		if (FAILED(hr)) goto error_done;
+	} while (0);
+	do {
+		ComPtr<IPortableDeviceManager> pPortableDeviceManager;
+		hr = ::CoCreateInstance(CLSID_PortableDeviceManager,
+					nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pPortableDeviceManager));
+		if (FAILED(hr)) {
+			env.SetError(ERR_RuntimeError, "failed to get interface of PortableDeviceManager");
+			goto error_done;
+		}
+		DWORD nDeviceIDs = 0;
+		hr = pPortableDeviceManager->GetDevices(nullptr, &nDeviceIDs);
+		if (FAILED(hr)) goto error_done;
+		std::unique_ptr<LPWSTR []> deviceIDs(new LPWSTR[nDeviceIDs]);
+		hr = pPortableDeviceManager->GetDevices(deviceIDs.get(), &nDeviceIDs);
+		if (FAILED(hr)) goto error_done;
+		for (DWORD i = 0; i < nDeviceIDs; i++) {
+			LPWSTR deviceID = deviceIDs[i];
+			String friendlyName;
+			String manufacturer;
+			String description;
+			do {
+				DWORD len = 0;
+				hr = pPortableDeviceManager->GetDeviceFriendlyName(deviceID, nullptr, &len);
+				if (FAILED(hr)) goto error_done;
+				std::unique_ptr<WCHAR []> str(new WCHAR[len + 1]);
+				hr = pPortableDeviceManager->GetDeviceFriendlyName(deviceID, str.get(), &len);
+				if (FAILED(hr)) goto error_done;
+				friendlyName = BSTRToString(str.get());
+			} while (0);
+			do {
+				DWORD len = 0;
+				hr = pPortableDeviceManager->GetDeviceManufacturer(deviceID, nullptr, &len);
+				if (FAILED(hr)) goto error_done;
+				std::unique_ptr<WCHAR []> str(new WCHAR[len + 1]);
+				hr = pPortableDeviceManager->GetDeviceManufacturer(deviceID, str.get(), &len);
+				if (FAILED(hr)) goto error_done;
+				manufacturer = BSTRToString(str.get());
+			} while (0);
+			do {
+				DWORD len = 0;
+				hr = pPortableDeviceManager->GetDeviceDescription(deviceID, nullptr, &len);
+				if (FAILED(hr)) goto error_done;
+				std::unique_ptr<WCHAR []> str(new WCHAR[len + 1]);
+				hr = pPortableDeviceManager->GetDeviceDescription(deviceID, str.get(), &len);
+				if (FAILED(hr)) goto error_done;
+				description = BSTRToString(str.get());
+			} while (0);
+			::printf("friendlyName: %s\n", friendlyName.c_str());
+			::printf("manufacturer: %s\n", manufacturer.c_str());
+			::printf("description:  %s\n", description.c_str());
+		}
+		for (DWORD i = 0; i < nDeviceIDs; i++) {
+			::CoTaskMemFree(deviceIDs[i]);
+		}
+	} while (0);
+	return Value::Nil;
+error_done:
+	env.SetError(ERR_RuntimeError, "error while calling COM function");
 	return Value::Nil;
 }
 
